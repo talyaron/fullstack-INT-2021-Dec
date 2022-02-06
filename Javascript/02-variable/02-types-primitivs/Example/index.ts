e712f514798abd5395232514b176587e0926ac0a
Tal Yaron <tal.yaron@gmail.com>
2022-02-06 19:51:44 +0200

    cont

--- Javascript/02-variable/02-types-primitivs/Example/index.ts
+++ Javascript/02-variable/02-types-primitivs/Example/index.ts
@@ -3,9 +3,15 @@ var x: number = 4;
 x = x + 6;
 
 var y: string = "aaaa";
-y = "eeee3";
+y = "2";
+y = `4556`;
 
-var p:boolean = true;
+var p: boolean = true;
 
-var z:any = 4;
-z = 'tret';
+var z: any = 4;
+z = "tret";
+
+var und = undefined;
+var nl = null;
+console.log(und+5); //NaN
+console.log(nl+5); //5
