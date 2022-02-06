39b10c5c40e7dd3a397c9fe515efa0034943fddd
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-02-06 20:22:21 +0200

    onlije 1

---
+++ Javascript/02-variable/03-concatinte/OnlineTask/gal/index.ts
@@ -0,0 +1,11 @@
+// online task: set up your: first&last name,email, phone, address, daysOfExpriensInCSS
+var firstName:string = 'gal';
+var lastName:string = 'gross';
+var email: string= 'galgross24@gmail.com';
+var phone: string= '0509452702';
+var address: string= 'kochvei itzhak 5 TLV'
+var daysOfExpriensInCSS: string='approximally a month & a half'
+var fullName:string = `${firstName} ${lastName}`;
+
+console.log(fullName);
+console.log(`Hi ${firstName}, I know your last name. It is: ${lastName}`);
