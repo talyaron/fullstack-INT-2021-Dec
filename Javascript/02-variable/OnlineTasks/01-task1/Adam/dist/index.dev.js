b282c849b28398f27f276322acb2631c6ba0dc68
adamepel <adamepel11@gmail.com>
2022-02-06 19:04:55 +0200

    js

---
+++ Javascript/02-variable/OnlineTasks/01-task1/Adam/dist/index.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var x = 6;
+console.log(6);
+var y = 20;
+console.log(20);
+x = x * x;
+console.log(x);
+y = y % y;
+console.log(y);
+x = x + y;
+console.log(x);
+x = x + 14;
+console.log;
