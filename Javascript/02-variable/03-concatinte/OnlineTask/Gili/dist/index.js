9303082381e584ab73895605838da8d234747e96
GiliEllen <gilimena95@gmail.com>
2022-02-06 20:22:02 +0200

    משימה שלישית

---
+++ Javascript/02-variable/03-concatinte/OnlineTask/Gili/dist/index.js
@@ -0,0 +1,6 @@
+var firstName = "Gili";
+var lastName = "Menahem";
+var email = "gilimena95@gmail.com";
+var address = "Moshav Lorem Ipsum";
+var daysOfExpriensInCSS = "40";
+console.log("Hi " + firstName + " " + lastName + ", your email address is " + email + " and you live in " + address + ". you have " + daysOfExpriensInCSS + " days of expirience.");
---
+++ Javascript/02-variable/03-concatinte/OnlineTask/Gili/index.ts
@@ -0,0 +1,9 @@
+var firstName: string = "Gili";
+var lastName: string = "Menahem";
+var email: string = "gilimena95@gmail.com";
+var address: string = "Moshav Lorem Ipsum";
+var daysOfExpriensInCSS: string = "40"
+
+
+
+console.log(`Hi ${firstName} ${lastName}, your email address is ${email} and you live in ${address}. you have ${daysOfExpriensInCSS} days of expirience.`)
