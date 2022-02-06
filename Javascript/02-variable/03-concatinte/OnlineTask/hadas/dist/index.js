d4c0f1eac1fcf5db7bb38696b11ef075c8c5e604
hadast054 <97188314+hadast054@users.noreply.github.com>
2022-02-07 00:00:23 +0200

    hadas

---
+++ Javascript/02-variable/03-concatinte/OnlineTask/hadas/dist/index.js
@@ -0,0 +1,8 @@
+var firstName = 'hadas';
+var lastName = 'zabahon';
+var phone = '0548490449';
+var emall = 'hadast054@gmail.com';
+var address = 'avraham mahrez 16/8 beer sheva';
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log("Hi " + fullName + ", your email address is " + emall + "and you live in " + address + " ");
---
+++ Javascript/02-variable/03-concatinte/OnlineTask/hadas/index.ts
@@ -0,0 +1,8 @@
+var firstName:string='hadas';
+var lastName:string='zabahon';
+var phone:string='0548490449';
+var emall:string='hadast054@gmail.com';
+var address: string= 'avraham mahrez 16/8 beer sheva';
+var fullName:string = `${firstName} ${lastName}`;
+console.log(fullName);
+console.log(`Hi ${fullName}, your email address is ${emall}and you live in ${address} `)
