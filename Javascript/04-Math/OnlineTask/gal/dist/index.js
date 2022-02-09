8698eb96843f7070ece1ba2006d98bf89b643ca9
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-02-09 18:48:23 +0200

    sdf

--- Javascript/04-Math/OnlineTask/gal/dist/index.js
+++ Javascript/04-Math/OnlineTask/gal/dist/index.js
@@ -1,4 +1,9 @@
-var x = Math.PI;
+var x = 4;
 console.log(x);
-var y = Math.sqrt(81);
+var y = Math.pow(4, 3);
 console.log(y);
+var z = Math.PI;
+console.log(x);
+var a = (z * y * x) / 3;
+console.log(a);
+// (4*r^3*PI)\3
