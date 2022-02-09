d4045df4a17b70e028f8c8dc4a6c4135bf783ddc
Lior Grishin <liorgrishinnew@gmail.com>
2022-02-09 19:16:05 +0200

    online Tasks

--- Javascript/04-Math/OnlineTask/Lior Grishin/app.ts
+++ Javascript/04-Math/OnlineTask/Lior Grishin/app.ts
@@ -1,9 +1,17 @@
-const pi:number = Math.PI;
-console.log(pi);
+const pi: number = Math.PI;
 
-let radius:number = 4;
-let r3:number = Math.pow(radius, 3);
+let radius: number = 4;
+let r3: number = Math.pow(radius, 3);
 
 
-let circleVol:number =  (4*pi * r3)/3
-console.log(circleVol);
+let circleVol: number = (4 * pi * r3) / 3
+console.log(`Circle Volume: ${circleVol}`);
+
+const min: number = 100;
+const max: number = 80000000;          // 0 - 1       79 999 900    +100 
+const randPrize:number = Math.round((Math.random() * (max - min)) + min);
+console.log(`You Won: ${randPrize}`);
+console.log(`Mas took 50%: ${randPrize / 2}$ You left with: ${randPrize / 2}$`);
+console.log(`Mas took 25%: ${randPrize * .25}$ You left with: ${randPrize*.75}$`);
+console.log(`Mas took 10%: ${randPrize * .1}$ You left with: ${randPrize *.9}$`);
+
--- Javascript/04-Math/OnlineTask/Lior Grishin/dist/app.js
+++ Javascript/04-Math/OnlineTask/Lior Grishin/dist/app.js
@@ -1,6 +1,12 @@
 var pi = Math.PI;
-console.log(pi);
 var radius = 4;
 var r3 = Math.pow(radius, 3);
 var circleVol = (4 * pi * r3) / 3;
-console.log(circleVol);
+console.log("Circle Volume: " + circleVol);
+var min = 100;
+var max = 80000000; // 0 - 1       79 999 900    +100 
+var randPrize = Math.round((Math.random() * (max - min)) + min);
+console.log("You Won: " + randPrize);
+console.log("Mas took 50%: " + randPrize / 2 + "$ You left with: " + randPrize / 2 + "$");
+console.log("Mas took 25%: " + randPrize * .25 + "$ You left with: " + randPrize * .75 + "$");
+console.log("Mas took 10%: " + randPrize * .1 + "$ You left with: " + randPrize * .9 + "$");
