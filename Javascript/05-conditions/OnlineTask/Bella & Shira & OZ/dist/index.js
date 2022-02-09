13e26d8d589ed08bedadf6f8adf4c359f9906658
Shira1401 <shiraamar44@gmail.com>
2022-02-09 20:18:02 +0200

    our task

---
+++ Javascript/05-conditions/OnlineTask/Bella & Shira & OZ/dist/index.js
@@ -0,0 +1,5 @@
+var minSalary = 5500;
+var maxSalary = 1e9;
+if (minSalary >= 5500 && maxSalary < 1e9) {
+    console.log('Pay taxes');
+}
---
+++ Javascript/05-conditions/OnlineTask/Bella & Shira & OZ/index.ts
@@ -0,0 +1,9 @@
+
+var minSalary :number = 5500;
+var maxSalary :number = 1e9;
+
+if(minSalary>=5500 && maxSalary <1e9){
+    console.log('Pay taxes');
+}
+
+
