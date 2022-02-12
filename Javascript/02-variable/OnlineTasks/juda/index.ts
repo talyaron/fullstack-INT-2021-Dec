b32affce09cba30e10e5f3bef7c3e3857ab8bcf2
JudaGolan <yehuda6993@gmail.com>
2022-02-12 17:00:18 +0200

    juda ts

---
+++ Javascript/02-variable/OnlineTasks/juda/index.ts
@@ -0,0 +1,6 @@
+var exp:number = 30;
+var address:string = 'RishonLezion';
+var phone:string = 0542141642;
+var email:string = 'yehuda6993@gmail.com'
+
+console.log 
---
+++ Javascript/04-Math/OnlineTask/juda/dist/index.js
@@ -0,0 +1,8 @@
+var salary = 1e9;
+console.log(salary);
+if (salary > 5300 && salary <= 1000000000) {
+    console.log("You have to pay tax");
+}
+else {
+    console.log("You dont need to pay tax");
+}
