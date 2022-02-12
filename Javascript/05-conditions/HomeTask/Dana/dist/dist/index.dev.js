6bc4191f25e77a8cda279bfd3289d32c3ae8ae79
Dana <dana.kemper@payu.com>
2022-02-12 17:42:34 +0200

    HW commit

---
+++ Javascript/05-conditions/HomeTask/Dana/dist/dist/index.dev.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var salary = 10000;
+
+if (salary <= 6450) {
+  console.log(salary * 0.9);
+}
+
+if (salary >= 6451 && salary <= 9240) {
+  console.log(salary * 0.86);
+}
+
+if (salary >= 9241 && salary <= 14840) {
+  console.log(salary * 0.8);
+}
+
+if (salary >= 14841 && salary <= 20620) {
+  console.log(salary * 0.69);
+}
+
+if (salary >= 20621 && salary <= 42910) {
+  console.log(salary * 0.65);
+}
+
+if (salary >= 42911 && salary <= 55270) {
+  console.log(salary * 0.53);
+}
+
+if (salary >= 55271) {
+  console.log(salary * 0.5);
+}
