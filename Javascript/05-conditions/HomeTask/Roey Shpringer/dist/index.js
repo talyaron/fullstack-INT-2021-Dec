4205ceab4b74c16f3bf5b89976f6bc30888dd98d
Rowy92 <roeyshpringer@gmail.com>
2022-02-12 22:32:00 +0200

    js rs 1.0

---
+++ Javascript/05-conditions/HomeTask/Roey Shpringer/dist/index.js
@@ -0,0 +1,13 @@
+var salary = 7000;
+var taxlvl1 = 6450;
+var taxlvl2 = 9240;
+var taxlvl3 = 14840;
+var taxlvl4 = 20620;
+var taxlvl5 = 42910;
+var taxlvl6 = 55270;
+if (salary <= taxlvl1) {
+    salary = salary * 0.9;
+    console.log(salary);
+}
+else if (salary)
+    ;
---
+++ Javascript/05-conditions/HomeTask/Roey Shpringer/index.ts
@@ -0,0 +1,20 @@
+
+var salary:number = 7000;
+
+
+var taxlvl1:number = 6450;
+
+var taxlvl2:number = 9240;
+
+var taxlvl3:number = 14840;
+
+var taxlvl4:number = 20620;
+
+var taxlvl5:number = 42910;
+
+var taxlvl6:number = 55270;
+
+if(salary <= taxlvl1){
+    salary = salary * 0.9;
+    console.log(salary);
+} else if(salary  )
