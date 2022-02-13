89c99f23f2b570cb3730d18a634dd375ab6dd662
hanan <jigaart0@gmail.com>
2022-02-13 20:00:30 +0200

    sasa

--- Javascript/06-loops/OnlineTasks/hanan meir inbar/dist/index.js
+++ Javascript/06-loops/OnlineTasks/hanan meir inbar/dist/index.js
@@ -1,6 +1,9 @@
 var dividedNumber = 5;
 var maxNumber = 100;
 var boom = "boom";
-for (var i = dividedNumber; i <= maxNumber; i++) {
-    console.log(i % maxNumber);
-}
+var num;
+num = prompt('Enter the number');
+if (num % 3 == 0)
+    document.write('Multiple of 3');
+else
+    document.write('Not a multiple of 3');
