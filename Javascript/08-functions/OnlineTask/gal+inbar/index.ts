5cc10a5cf0c260da1b3a306a52e2ceb99fb847ec
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-02-13 20:14:05 +0200

    gal+inbal

---
+++ Javascript/08-functions/OnlineTask/gal+inbar/index.ts
@@ -0,0 +1,16 @@
+
+function age(a:number, b:number):number{
+    return Math.round ((a/b)*100);
+    
+}
+
+const x=81;
+console.log(x);
+const y = age(55,x);
+console.log(y);
+const z=84.5;
+console.log(z);
+const f = age (30,z);
+console.log(f);
+
+
