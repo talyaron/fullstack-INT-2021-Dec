bc8860335712c0fe6e14f0b4939026b0581d925a
Dana <dana.kemper@payu.com>
2022-02-13 11:02:34 +0200

    Changes to log

--- Javascript/05-conditions/HomeTask/Dana/dist/index.js
+++ Javascript/05-conditions/HomeTask/Dana/dist/index.js
@@ -1,12 +1,13 @@
 var salary = 10000;
+
 //salary times 0.9 for basic tax calculations
 var basictax = lvl1max * 0.9;
-var maxtaxLevel2 = basictax + ((salary - lvl1max) * 0.86);
-var maxtaxLevel3 = maxtaxLevel2 + ((salary - lvl2max) * 0.8);
-var maxtaxLevel4 = maxtaxLevel3 + ((salary - lvl3max) * 0.69);
-var maxtaxLevel5 = maxtaxLevel4 + ((salary - lvl4max) * 0.65);
-var maxtaxLevel6 = maxtaxLevel5 + ((salary - lvl5max) * 0.53);
-var maxtaxLevel7 = maxtaxLevel6 + ((salary - lvl6max) * 0.5);
+var maxtaxLevel2 = ((lvl2max - lvl2min) * 0.86);
+var maxtaxLevel3 = ((lvl3max - lvl3min) * 0.8);
+var maxtaxLevel4 = ((lvl4max - lvl4min) * 0.69);
+var maxtaxLevel5 = ((lvl5max - lvl5min) * 0.65);
+var maxtaxLevel6 = ((lvl6max - lvl6min) * 0.53);
+var maxtaxlevel7 = ((lvl7min - lvl6max) * 0.5);
 //level 1 tax range
 var lvl1min = 1;
 var lvl1max = 6450;
@@ -33,20 +34,20 @@ if (salary <= 6450) {
     console.log(salary * 0.9);
 }
 if (salary >= lvl2min && salary <= lvl2max) {
-    console.log(basictax + (salary - lvl1max * 0.86));
+    console.log(basictax + ((salary - lvl1max) * 0.86));
 }
 if (salary >= lvl3min && salary <= lvl3max) {
-    console.log(basictax + (salary - lvl1max) * 0.86);
+    console.log(maxtaxLevel2 + (salary - lvl2max) * 0.8);
 }
 if (salary >= lvl4min && salary <= lvl4max) {
-    console.log(basictax + (salary - lvl1max) * 0.86);
+    console.log(maxtaxLevel3 + (salary - lvl3max) * 0.69);
 }
 if (salary >= lvl5min && salary <= lvl5max) {
-    console.log(basictax + (salary - lvl1max) * 0.86);
+    console.log(maxtaxLevel4 + (salary - lvl4max) * 0.65);
 }
 if (salary >= lvl6min && salary <= lvl6max) {
-    console.log(basictax + (salary - lvl1max) * 0.86);
+    console.log(maxtaxLevel5 + (salary - lvl5max) * 0.53);
 }
 if (salary >= lvl7min) {
-    console.log(basictax + (salary - lvl1max) * 0.86);
+    console.log(maxtaxLevel6 + (salary - lvl6max) * 0.5);
 }
