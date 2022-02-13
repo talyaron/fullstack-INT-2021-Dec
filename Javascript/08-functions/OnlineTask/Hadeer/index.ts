57855a2e46cf921a2ffa61a15539612fc2e77884
hadeer57 <hadeer57@hotmail.com>
2022-02-13 20:31:21 +0200

    class ex

---
+++ Javascript/08-functions/OnlineTask/Hadeer/index.ts
@@ -0,0 +1,22 @@
+
+function gender( a:any, age:number){
+    if( a= `Man`){
+        let x = (age*80/100)
+    }
+
+    else if ( a=`Woman`){
+        let x = (age*88/100)
+    }
+
+    return `You are A ${a} , and you lived  x% of your life` 
+
+
+}
+
+const z = `Man`;
+
+const q = gender(z,30);
+console.log(q)
+
+
+
