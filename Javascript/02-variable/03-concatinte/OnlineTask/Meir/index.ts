46d08b8e93585725321f34acc6f663cda403611d
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-02-15 00:59:10 +0200

    online

---
+++ Javascript/02-variable/03-concatinte/OnlineTask/Meir/index.ts
@@ -0,0 +1,21 @@
+var str:String = "email";
+var str2:String ="Rebmeir770@gmail.com";
+var str3:String ="str+str2";
+
+console.log(str,str2)
+
+var str:String = "phone";
+var num:number =" 0543046081";
+var str3:String ="str+num";
+console.log(str,num)
+
+var str:String = "adress: ";
+var str2:String ="Waitzman";
+var num:number =20;
+var str3:String ="str+str2+num";
+console.log(str,str2,num)
+
+var num:number = 30;
+var str:String = "daysOfExpriensInCSS";
+var str3:String ="str+num+str2";
+console.log(`Hi I've ${num} ${str}`)
