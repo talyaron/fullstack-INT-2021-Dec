1b6ecbb19f50910062e5d3e73e74e28709901e43
Maxim Nagorny <maximng17@gmail.com>
2022-02-15 17:16:15 +0200

    hw

---
+++ Javascript/08-functions/HomeTasks/MaxV2/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+function tfortune(job, Location, partner, kids) {
+    var future = 'you will be a ' + job + 'in ' + Location + ' and suffer from' + partner + 'with' + kids;
+    console.log(future);
+}
+tfortune('HomeLEss', 'BatYam', 'LadyGaga', 3);
+tfortune('DevOps', 'Gaza', 'BritneySpears', 5);
+tfortune('Stripper ', 'Israel', 'Donalda Trump', 0);
---
+++ Javascript/08-functions/HomeTasks/MaxV2/index.ts
@@ -0,0 +1,8 @@
+export {};
+function tfortune (job:string, Location:string, partner:string, kids:number){
+    var future = 'you will be a '+job + 'in ' +Location +' and suffer from' +partner + 'with' +kids 
+    console.log(future);
+}
+tfortune ('HomeLEss', 'BatYam', 'LadyGaga', 3);
+tfortune ('DevOps', 'Gaza', 'BritneySpears', 5);
+tfortune ('Stripper ', 'Israel', 'Donalda Trump', 0);
