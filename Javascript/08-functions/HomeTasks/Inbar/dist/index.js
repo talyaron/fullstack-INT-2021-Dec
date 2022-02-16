02d08941115f600b3d6ecadebe1dc481f8a1d534
Mckipis <97186498+Mckipis@users.noreply.github.com>
2022-02-16 18:38:07 +0200

    TellFortune

---
+++ Javascript/08-functions/HomeTasks/Inbar/dist/index.js
@@ -0,0 +1,11 @@
+function tellFortune(jobTitle, Geolocation, partner, numKids) {
+    var future = "You will be a " + jobTitle + " in " + Geolocation + " and married to " + partner + " with " + numKids + " kids.";
+    return (future);
+}
+tellFortune('Truck Driver', 'Canada', 'Trudo', 5);
+tellFortune('Tech-CEO', 'LA', 'Mark Zuckerberg', 0);
+tellFortune('Human being', 'planet', 'your cat', 1e9);
+function dogage(age) {
+    return;
+    var dogyears = "\"Your doggie is $ years old in dog years!";
+}
