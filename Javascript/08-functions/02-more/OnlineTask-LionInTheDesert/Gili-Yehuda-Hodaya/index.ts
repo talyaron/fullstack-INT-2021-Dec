43974b419b6046eace2526eff2d9195e0bffbb37
GiliEllen <gilimena95@gmail.com>
2022-02-16 20:07:19 +0200

    קוד

--- Javascript/08-functions/02-more/OnlineTask-LionInTheDesert/Gili-Yehuda-Hodaya/index.ts
+++ Javascript/08-functions/02-more/OnlineTask-LionInTheDesert/Gili-Yehuda-Hodaya/index.ts
@@ -1,8 +1,8 @@
 
-    
+function lionIntheDeseret(trys:number) {
     const generatedNumber: number = Math.floor(Math.random() * 1000); // genrating random number for game
     console.log(generatedNumber) // for test
-    for (let i = 0; i < 5;) {
+    for (let i = 0; i < trys;) {
         let answer = +prompt('what is you guess?');
         if (answer == generatedNumber) {
             i = 6
@@ -11,17 +11,20 @@
         else if (answer > generatedNumber) {
             console.log(i)
             i++
-            console.log(`Your number is too big. Try a smaller number. You got ${5-i} trys left`)
-            if (i == 5) {
+            console.log(`Your number is too big. Try a smaller number. You got ${5 - i} trys left`)
+            if (i == trys) {
                 alert(`You lose!`)
             }
         }
         else if (answer < generatedNumber) {
             console.log(i)
             i++
-            console.log(`Your number is too small. Try a bigger number. You got ${5-i} trys left`)
-            if (i == 5) {
+            console.log(`Your number is too small. Try a bigger number. You got ${5 - i} trys left`)
+            if (i == trys) {
                 alert(`You lose!`)
             }
         }
     }
+}
+
+lionIntheDeseret(5)
