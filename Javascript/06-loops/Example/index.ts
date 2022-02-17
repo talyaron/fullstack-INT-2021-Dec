95b3dd5a33bd3ca81a6542d460f21327396ec196
hanochshachar <goralot550@gmail.com>
2022-02-17 04:25:30 +0200

    גש

--- Javascript/06-loops/Example/index.ts
+++ Javascript/06-loops/Example/index.ts
@@ -7,13 +7,5 @@ for(var i=2; i=10000; Math.pow(i,2)){
 var dividedNumber:number = 5;
 var maxNumber:number= 100;
 
-<<<<<<< Updated upstream
 for(var i = 0; i<10; i++){
 console.log(i)}
-=======
-
-for(var i = 5 ;  i<100 ; i++){
-
-}
-
->>>>>>> Stashed changes
