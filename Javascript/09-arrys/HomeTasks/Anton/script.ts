0f2ac9053d7909b8676b05780421517ac311075e
Anton <antoniuskonovalov@gmail.com>
2022-02-18 17:51:05 +0200

    task

--- Javascript/09-arrys/HomeTasks/Anton/script.ts
+++ Javascript/09-arrys/HomeTasks/Anton/script.ts
@@ -1,5 +1,38 @@
+/* first task */
+
 const my_besties:Array<string> = ['Johnny Knoxville', 'Elon Musk', 'Vladimir Putin', 'Fridrich'];
 
 for (var i = 0; i < my_besties.length; i++) {
     console.log(`Welcome: ${my_besties[i]}`)
 }
+/* first task */
+
+
+/* second task */
+const numbers = 10;
+const arr:Array<number> = []
+for (var i=0; i< numbers; i++) {
+     arr.push(i);
+     if (i > 5) {
+         console.log("The number is bigger then 5");
+     }
+}
+/* second task */
+
+
+/* third task */
+function num (var num1, var num2) {
+    var sum= num1+num2;
+    return sum;
+}
+
+const arr2:Array<any> = ['Peter Pan', 3, 'Max Payne', 'Solidity', 5, num]
+for (var i=0; i<arr2.length; i++) {
+    if (typeof arr2[i] === 'function' ) {
+        console.log(num(7,2));
+    } else {
+        console.log(typeof arr2[i]);
+    }
+}
+
+/* third task */
