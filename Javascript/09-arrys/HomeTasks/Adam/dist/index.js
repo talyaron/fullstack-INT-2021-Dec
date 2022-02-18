c9738fc4d35e92279d0406574bf398332b653c69
adamepel <adamepel11@gmail.com>
2022-02-18 18:02:46 +0200

    Home task

---
+++ Javascript/09-arrys/HomeTasks/Adam/dist/index.js
@@ -0,0 +1,12 @@
+//  Task N.1
+var friends = ["Shai", "Ben", "Alma", "Ori"];
+for (var i = 0; i < friends.length; i++) {
+    console.log("Welcome " + friends[i] + "!");
+}
+//Task N.2
+var numArray = [1, 2, 3, 4, 5, 10, 20, 30, 40, 50];
+for (var K = 0; K < numArray.length; K++) {
+    if (numArray[K] > 5) {
+        console.log("The Number " + numArray[K] + " Is Bigger Than 5");
+    }
+}
---
+++ Javascript/09-arrys/HomeTasks/Adam/index.ts
@@ -0,0 +1,19 @@
+//  Task N.1
+const friends: Array <any> = [`Shai`,`Ben`,`Alma`,`Ori`];
+
+for (let i = 0; i < friends.length; i++) {
+    console.log(`Welcome ${friends[i]}!`)
+}
+
+//Task N.2
+const numArray: Array<number> = [1,2,3,4,5,10,20,30,40,50];
+
+for (let K = 0; K < numArray.length; K++) {
+
+    if (numArray[K] > 5) {
+        console.log(`The Number ${numArray[K]} Is Bigger Than 5`)
+    }
+}
+
+
+
