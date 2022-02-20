8dda91dc8cad51844ff1338e78092e0099a59e6b
Bella-UI <bellabrailovsky@gmail.com>
2022-02-20 20:32:07 +0200

    ...

--- Javascript/09-arrys/02-methods1/OnlineTask1/Adam-Bella-Evegeny/dist/index.js
+++ Javascript/09-arrys/02-methods1/OnlineTask1/Adam-Bella-Evegeny/dist/index.js
@@ -1,31 +1,3 @@
-// const arr: Array<any> = [10, 2, 34, 6, "b", "a", "B", "A"];
-// console.log(arr);
-// //methods are function of an object (in our case Array)
-// arr.forEach((elm) => {
-//   try {
-//     console.log(elm);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// });
-// // const index = arr.findIndex(elm=>{return elm === 6});
-// const index = arr.findIndex((elm) => elm === 6);
-// console.log("index:", index);
-// console.log(arr);
-// const sortedArray = arr;
-// sortedArray.sort((a, b) =>{
-//     return a - b
-// });
-// console.log(sortedArray);
-// if(2 === '2') console.log('2 == "2"')
-// else console.log('2 != "2"')
-//map build a new array from previous array
-// const newArr = arr.map(elm=>{
-//     if(typeof elm === 'number') return elm*100
-//     else return "This wasnt a number!";
-// }).filter(elm=>typeof elm === 'number');
-// console.log (newArr);
-// console.log(arr)
 var arr = [10, 2, 34, 6, 120, 130, 99, 10000000, 200];
 var newArr = arr.map(function (elm) {
     if (elm >= 100)
