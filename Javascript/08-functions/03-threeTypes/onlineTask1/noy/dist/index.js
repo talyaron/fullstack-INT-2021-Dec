42541872742fc7825347ca829071df1124fafb8a
MartinoySolomon <noy.kakoun@gmail.com>
2022-02-20 18:30:29 +0200

    online task 1

---
+++ Javascript/08-functions/03-threeTypes/onlineTask1/noy/dist/index.js
@@ -0,0 +1,12 @@
+function sum(a, b) {
+    return a + b;
+}
+var sum2 = function (a, b) {
+    return a + b;
+};
+var sum3 = function (a, b) {
+    return a + b;
+};
+console.log(sum(2, 1));
+console.log(sum2(2, 1));
+console.log(sum3(2, 1));
