1f06c7a6c5d8d9eaa5a710753de067ad807ce05b
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-02-20 17:48:08 +0200

    exercisies1

--- Javascript/08-functions/01-basic/HomeTasks/Meir/dist/index.js
+++ Javascript/08-functions/01-basic/HomeTasks/Meir/dist/index.js
@@ -10,5 +10,23 @@ var time3 = tellFortune("artist", "Peru", "Jenny", 3);
 console.log(time3);
 //Puppy Age Calculator
 function caculateDogAge(puppysAge) {
-    return ("Your doggie is " + puppysAge + " years old in in dog years!");
+    return puppysAge * 7;
 }
+var age = caculateDogAge(2);
+console.log("Your doggie is " + age + " years old in in dog years!");
+var age = caculateDogAge(3);
+console.log("Your doggie is " + age + " years old in in dog years!");
+var age = caculateDogAge(4);
+console.log("Your doggie is " + age + " years old in in dog years!");
+//Lifetime Supply Calculator
+function calculateSupply(age, amountPerDay) {
+    var maxAge = 120;
+    return (amountPerDay * 365) * (maxAge - age);
+}
+var snacks = calculateSupply(45, 52);
+console.log("You will need " + snacks + "  to last you until the ripe old age of 120 years old");
+var snacks = calculateSupply(37, 48.5);
+console.log("You will need " + snacks + "  to last you until the ripe old age of 120 years old");
+var snacks = calculateSupply(12, 18);
+console.log("You will need " + snacks + "  to last you until the ripe old age of 120 years old");
+//The Geometrizer
--- Javascript/08-functions/01-basic/HomeTasks/Meir/index.ts
+++ Javascript/08-functions/01-basic/HomeTasks/Meir/index.ts
@@ -13,7 +13,26 @@ console.log(time3)
 
 //Puppy Age Calculator
 function caculateDogAge(puppysAge:number) {
-    return (`Your doggie is `+ puppysAge +` years old in in dog years!`)
-    
+    return puppysAge*7
+}  
 
+const age = caculateDogAge(2)
+console.log(`Your doggie is `+ age +` years old in in dog years!`)
+const age = caculateDogAge(3)
+console.log(`Your doggie is `+ age +` years old in in dog years!`)
+const age = caculateDogAge(4)
+console.log(`Your doggie is `+ age +` years old in in dog years!`)
+
+//Lifetime Supply Calculator
+function calculateSupply (age:number, amountPerDay:number) {
+   const maxAge = 120;
+   return (amountPerDay * 365) * (maxAge - age)
 }
+const snacks = calculateSupply(45, 52)
+console.log(`You will need `+ snacks +`  to last you until the ripe old age of 120 years old`)
+const snacks = calculateSupply(37, 48.5)
+console.log(`You will need `+ snacks +`  to last you until the ripe old age of 120 years old`)
+const snacks = calculateSupply(12, 18)
+console.log(`You will need `+ snacks +`  to last you until the ripe old age of 120 years old`)
+ 
+//The Geometrizer
