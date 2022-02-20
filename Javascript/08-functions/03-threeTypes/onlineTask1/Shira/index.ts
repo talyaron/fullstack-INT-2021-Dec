9a22c92e2bc2ce9135a9728a7a2d2275d26b0883
Shira1401 <shiraamar44@gmail.com>
2022-02-20 18:37:45 +0200

    Three forms of the function

---
+++ Javascript/08-functions/03-threeTypes/onlineTask1/Shira/index.ts
@@ -0,0 +1,27 @@
+// create a function that add 2 arguments with three forms of the function (regular, anonymous, arrow)
+// 2 try to use them before the defintion and after the defition
+
+// 1: regular function
+
+console.log(sumNumbers(5,6));
+function sumNumbers(a:number, b:number):number{
+    return a+b;
+}
+
+// 2: anonymous function
+
+
+// console.log(sumNumbers2(5,7));
+const sumNumbers2 = function(a:number,b:number):number{
+return a+b;
+}
+console.log(sumNumbers2(5,7));
+
+
+// 3: arrow function
+
+// console.log(sumNumbers3(5,8));
+const sumNumbers3 =  (a:number,b:number):number =>{
+return a+b;
+}
+console.log(sumNumbers3(5,8));
