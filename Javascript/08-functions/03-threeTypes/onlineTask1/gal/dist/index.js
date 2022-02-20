3843980b0e685ad6afca5abdc9b492274dd6e852
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-02-20 20:04:47 +0200

    2

--- Javascript/08-functions/03-threeTypes/onlineTask1/gal/dist/index.js
+++ Javascript/08-functions/03-threeTypes/onlineTask1/gal/dist/index.js
@@ -1,5 +1,3 @@
-// create a function that add 2 arguments with three forms of the function (regular, anonymous, arrow)
-// 2 try to use them before the defintion and after the defition
 // ----regular func---
 function multy(a, b) {
     return a + b;
