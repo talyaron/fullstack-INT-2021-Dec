27615a4454c7d7886d7dd0e6f1f236bba331dd47
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-02-21 23:59:37 +0200

    finish hw

--- Javascript/08-functions/HomeTasks/Gal/dist/index.js
+++ Javascript/08-functions/HomeTasks/Gal/dist/index.js
@@ -1,13 +1,16 @@
 var array = [1, "a", 2, "b", 3, "c", 4, "d"];
 var answer = prompt("pls type \"number\" or \"string\"");
-var number = array.filter(function (elm) { return typeof elm === "number"; });
-var string = array.filter(function (elm) { return typeof elm === "string"; });
+// const number = array.filter(elm => typeof elm === `number`);
+// const string= array.filter(elm => typeof elm === `string`);
+// const number:number= `numbers` //cannot do number because in the function its string
+var number = "number";
+var string = "string";
 var newarray = homework(answer); //ליצור מערך חדשה
 console.log(newarray);
 function homework(answer) {
     try {
         if (answer !== number && answer !== string) { //מגדירים מה זו בעצם שגיאה
-            throw new Error("Function not implemented");
+            throw new Error("Function not implemented"); // שווה ערך לפונקציה שרשומה בקומנטים למטה (קונסול לוג לא עובד)
         }
     }
     catch (erorr) {
@@ -15,21 +18,21 @@ function homework(answer) {
     }
     if (answer === number) { //קופצים למים ומתחילים להגדיר לחלק את המערך הישן וליצור חדש
         console.log("there are numbers");
-        var newNumbersArry = array.map(function (elm) {
+        var newNumbersArray = array.map(function (elm) {
             if (typeof elm === "number")
                 return elm;
         })
             .filter(function (elm) { return typeof elm === 'number'; }); //מפלטרים
-        return newNumbersArry;
+        return newNumbersArray;
     }
     else if (answer === string) {
         console.log("These are strings:");
-        var newStringArry = array.map(function (elm) {
+        var newStringArray = array.map(function (elm) {
             if (typeof elm === "string")
                 return elm;
         })
             .filter(function (elm) { return typeof elm === 'string'; });
-        return newStringArry;
+        return newStringArray;
     }
 }
 // function err(err: any) {
--- Javascript/08-functions/HomeTasks/Gal/index.ts
+++ Javascript/08-functions/HomeTasks/Gal/index.ts
@@ -2,8 +2,12 @@ const array: Array<any> = [1, `a`, 2, `b`, 3, `c`, 4, `d`]
 
 
 const answer:any = prompt (`pls type "number" or "string"`)
-const number = array.filter(elm => typeof elm === `number`);
-const string= array.filter(elm => typeof elm === `string`);
+// const number = array.filter(elm => typeof elm === `number`);
+// const string= array.filter(elm => typeof elm === `string`);
+
+// const number:number= `numbers` //cannot do number because in the function its string
+const number:string=`number`
+const string:string=`string`
 
 const newarray= homework (answer) //ליצור מערך חדשה
 console.log (newarray) 
@@ -11,7 +15,7 @@ console.log (newarray)
 function homework (answer:any){
     try{
         if (answer !== number && answer !== string) { //מגדירים מה זו בעצם שגיאה
-            throw new Error("Function not implemented")
+            throw new Error("Function not implemented")// שווה ערך לפונקציה שרשומה בקומנטים למטה (קונסול לוג לא עובד)
         }
     }
     catch (erorr) {
@@ -19,19 +23,19 @@ function homework (answer:any){
     }
     if(answer===number){ //קופצים למים ומתחילים להגדיר לחלק את המערך הישן וליצור חדש
         console.log (`there are numbers`)
-        const newNumbersArry = array.map(elm => { //  שולפים בעזרת (map) את המספרים ויוצרים מערך חדש שיוצג בקונסול שהמשתמש יקיש Number
+        const newNumbersArray = array.map(elm => { //  שולפים בעזרת (map) את המספרים ויוצרים מערך חדש שיוצג בקונסול שהמשתמש יקיש Number
             if (typeof elm === "number") return elm
         })
         .filter(elm => typeof elm === 'number'); //מפלטרים
-        return newNumbersArry
+        return newNumbersArray
     }
     else if (answer === string) {
         console.log(`These are strings:`)
-        const newStringArry = array.map(elm => {// כנל לגביי הסטרינגים
+        const newStringArray = array.map(elm => {// כנל לגביי הסטרינגים
             if (typeof elm === "string") return elm
         })
         .filter(elm => typeof elm === 'string');
-        return newStringArry 
+        return newStringArray 
     }
 }
 
