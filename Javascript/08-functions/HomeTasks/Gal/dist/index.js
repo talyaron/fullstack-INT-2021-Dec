78dde16db56800a8a266d15227e00cf45f768c81
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-02-21 21:30:53 +0200

    split array without pronpt

--- Javascript/08-functions/HomeTasks/Gal/dist/index.js
+++ Javascript/08-functions/HomeTasks/Gal/dist/index.js
@@ -1,4 +1,20 @@
 var array = [1, "a", 2, "b", 3, "c", 4, "d"];
-var ans1 = prompt("pls type number or something else");
+// const ans1:any=prompt(`pls type number or something else`);
 var num1 = array.filter(function (elm) { return typeof elm === "number"; });
-var stri1 = array.filter(function (elm) { return typeof elm === "number"; });
+var stri1 = array.filter(function (elm) { return typeof elm === "string"; });
+array.forEach(function (elm) {
+    try {
+        if (typeof elm === "number") {
+            console.log(num1);
+        }
+        else if (typeof elm === "string") {
+            console.log(stri1);
+        }
+        else {
+            console.log("error");
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
--- Javascript/08-functions/HomeTasks/Gal/index.ts
+++ Javascript/08-functions/HomeTasks/Gal/index.ts
@@ -1,6 +1,23 @@
 const array:Array<any>= [1,`a`,2,`b`,3,`c`,4,`d`]
  
-const ans1:any=prompt(`pls type number or something else`);
+// const ans1:any=prompt(`pls type number or something else`);
 const num1= array.filter(elm => typeof elm===`number`);
-const stri1= array.filter(elm => typeof elm===`number`);
+const stri1= array.filter(elm => typeof elm===`string`);
 
+array.forEach(elm=>{
+    try {
+        if(typeof elm=== "number"){
+            console.log(num1)
+            }
+            else if(typeof elm==="string") 
+            { console.log(stri1)}
+            else{
+                console.log(`error`)
+            }
+
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+)
