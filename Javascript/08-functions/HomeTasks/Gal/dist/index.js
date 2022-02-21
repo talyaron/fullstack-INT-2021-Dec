9da488b43f7bf8769ccdb9daa6f076413b00c8fa
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-02-21 21:25:00 +0200

    לח

---
+++ Javascript/08-functions/HomeTasks/Gal/dist/index.js
@@ -0,0 +1,4 @@
+var array = [1, "a", 2, "b", 3, "c", 4, "d"];
+var ans1 = prompt("pls type number or something else");
+var num1 = array.filter(function (elm) { return typeof elm === "number"; });
+var stri1 = array.filter(function (elm) { return typeof elm === "number"; });
---
+++ Javascript/08-functions/HomeTasks/Gal/index.ts
@@ -0,0 +1,6 @@
+const array:Array<any>= [1,`a`,2,`b`,3,`c`,4,`d`]
+ 
+const ans1:any=prompt(`pls type number or something else`);
+const num1= array.filter(elm => typeof elm===`number`);
+const stri1= array.filter(elm => typeof elm===`number`);
+
