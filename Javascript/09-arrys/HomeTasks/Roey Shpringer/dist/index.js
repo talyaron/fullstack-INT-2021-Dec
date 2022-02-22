0335be9945c34a7e8dece3c4a5120f94a8608f38
Rowy92 <roeyshpringer@gmail.com>
2022-02-23 01:35:45 +0200

    Roey Shpringer's 2nd array hw

---
+++ Javascript/09-arrys/HomeTasks/Roey Shpringer/dist/index.js
@@ -0,0 +1,16 @@
+var array = [1, "x", 32, 42, 51, "abcd", 11, "ajax", "emma", "takemichi", 12, 1];
+function arraySort(array, typeOf) {
+    if (typeOf == 'numbers' || typeOf == 'strings') {
+        for (var i = 0; i < array.length; i++) {
+            if (typeOf == 'numbers' && typeof (array[i]) == 'number') {
+                console.log(array[i]);
+            }
+            else if (typeOf == 'strings' && typeof (array[i]) == 'string') {
+                console.log(array[i]);
+            }
+        }
+    }
+    else
+        console.log("you entered " + typeOf + ", it is not a string and not a number");
+}
+arraySort(array, 'strings');
