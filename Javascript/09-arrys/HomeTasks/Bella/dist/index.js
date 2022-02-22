613f64ff41224e1bacd7abdad21b1ccb0cefd2bd
Bella-UI <bellabrailovsky@gmail.com>
2022-02-22 11:49:17 +0200

    fix

--- Javascript/09-arrys/HomeTasks/Bella/dist/index.js
+++ Javascript/09-arrys/HomeTasks/Bella/dist/index.js
@@ -1,48 +1,25 @@
-// // pt 1 -----------------------------------------------------------------------------------------------------
-// const arr: Array < any > = [10, "Wacom Cintiq", "4", 8, 120, 130, "HUION Kamvas Pro", true];
-// // const userInput: any = +prompt("Enter string or number below")
-// const num: number = 0;
-// const str: string = ""
-// // pt 2 -----------------------------------------------------------------------------------------------------
-// function identifyInput(userInput:any) {
-//   if (userInput === num) {
-//     return `${userInput} "is a number"`;
-//   } else if (userInput === str) {
-//     return `${userInput} "is a string"`;
-//   } else(userInput !== num && userInput !== str); {
-//     throw SyntaxError("input is not valid.");
-//   }
-// }
-// const arr = [7,'c1','d2',18,'dr',21,2];
-// let numbersOnly = (val) => {
-//   if (typeof(val) === 'number') {      // replace number with string to get only string values in an array.
-//     return val;
-//   }
-// }
-// let numbers = arr.filter(numbersOnly);
-// document.write(`${numbers} are numbers`);
-// function typeOfvar(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     if (typeof arr[i] === "string") {
-//       return str;
-//     } else if (typeof arr[i] === "number") {
-//       return num;
-//     }
-//   }
-// }
-// 3st assignment ----------------------------------------------------------------------------------------------------
-// const items: Array < any > = [random, "Tel aviv", 4, "Tomer 1", 1 + 1, true, 3.6, false, undefined, null];
-// for (var i = 0; i < items.length; i++) {
-//   if (typeof items[i] === "function") {
-//       console.log(random(1, 9));
-//   } else {
-//       console.log(typeof items[i]);
-//   }
-// }
-// const arr = [7,'c1','d2',18,'dr',21,2];
-// function game (num:number, str:string){
-// for (let i = 0; i < arr.length; i++){
-// if (typeof num==="number")
-// return 
-//   }
-// }
+// pt 1: -------------------------------------------------------------------------------------------------------
+var arr = [88, "Wacom Cintiq", 8, 120, 144, "Photoshop", null, true];
+// back log ----------------------------------------------------------------------------------------------------
+// const userInput:any = prompt("Enter string or number below")
+// arr.push(`${userInput}`);
+// pt 2: -------------------------------------------------------------------------------------------------------
+//info: New "string" array out of arr
+var arrStr = arr.map(function (elm) {
+    if (typeof elm === "string")
+        return elm;
+}).filter(function (elm) { return typeof elm === "string"; });
+console.log(arrStr);
+// pt 3: -------------------------------------------------------------------------------------------------------
+//info: New "numbers" array out of arr
+var arrNum = arr.map(function (elm) {
+    if (typeof elm === "number")
+        return elm;
+}).filter(function (elm) { return typeof elm === "number"; });
+console.log(arrNum);
+// pt 4: -------------------------------------------------------------------------------------------------------
+//info: New "error/invalid" array out of arr
+var arrError = arr.map(function (elm) {
+    if (typeof elm !== "number" && elm !== "string")
+        throw "Not valid";
+});
--- Javascript/09-arrys/HomeTasks/Bella/index.ts
+++ Javascript/09-arrys/HomeTasks/Bella/index.ts
@@ -1,69 +1,31 @@
-// // pt 1 -----------------------------------------------------------------------------------------------------
-// const arr: Array < any > = [10, "Wacom Cintiq", "4", 8, 120, 130, "HUION Kamvas Pro", true];
-// // const userInput: any = +prompt("Enter string or number below")
-// const num: number = 0;
-// const str: string = ""
+
+// pt 1: -------------------------------------------------------------------------------------------------------
+const arr: Array<any> = [88, "Wacom Cintiq", 8, 120, 144, "Photoshop", null, true];
+// back log ----------------------------------------------------------------------------------------------------
+// const userInput:any = prompt("Enter string or number below")
+// arr.push(`${userInput}`);
 
 
-// // pt 2 -----------------------------------------------------------------------------------------------------
-// function identifyInput(userInput:any) {
-//   if (userInput === num) {
-//     return `${userInput} "is a number"`;
+// pt 2: -------------------------------------------------------------------------------------------------------
+//info: New "string" array out of arr
+const arrStr = arr.map(elm => {
+    if (typeof elm === "string") return elm
 
-//   } else if (userInput === str) {
-//     return `${userInput} "is a string"`;
+}).filter(elm => typeof elm === "string");
+console.log(arrStr)
 
-//   } else(userInput !== num && userInput !== str); {
-//     throw SyntaxError("input is not valid.");
-//   }
+// pt 3: -------------------------------------------------------------------------------------------------------
+//info: New "numbers" array out of arr
+const arrNum = arr.map(elm => {
+    if (typeof elm === "number") return elm
+  
+}).filter(elm => typeof elm === "number");
+console.log(arrNum)
 
-// }
+// pt 4: -------------------------------------------------------------------------------------------------------
+//info: New "error/invalid" array out of arr
+const arrError = arr.map(elm => {
+    if (typeof elm !== "number" && elm !== "string") 
+    throw "Not valid";
+})
 
-
-// const arr = [7,'c1','d2',18,'dr',21,2];
-
-// let numbersOnly = (val) => {
-//   if (typeof(val) === 'number') {      // replace number with string to get only string values in an array.
-//     return val;
-//   }
-// }
-
-// let numbers = arr.filter(numbersOnly);
-// document.write(`${numbers} are numbers`);
-
-
-
-
-// function typeOfvar(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     if (typeof arr[i] === "string") {
-//       return str;
-
-//     } else if (typeof arr[i] === "number") {
-//       return num;
-
-
-//     }
-//   }
-// }
-
-
-// 3st assignment ----------------------------------------------------------------------------------------------------
-
-// const items: Array < any > = [random, "Tel aviv", 4, "Tomer 1", 1 + 1, true, 3.6, false, undefined, null];
-// for (var i = 0; i < items.length; i++) {
-//   if (typeof items[i] === "function") {
-//       console.log(random(1, 9));
-//   } else {
-//       console.log(typeof items[i]);
-//   }
-// }
-
-// const arr = [7,'c1','d2',18,'dr',21,2];
-
-// function game (num:number, str:string){
-// for (let i = 0; i < arr.length; i++){
-// if (typeof num==="number")
-// return 
-//   }
-// }
