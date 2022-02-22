6f2a91e15721bf322bffc707fa8e136b6309ccb5
carmacookies <carmacookies@gmail.com>
2022-02-22 18:09:58 +0200

    carmel work

---
+++ Javascript/08-functions/03-threeTypes/onlineTask1/crnl/index.ts
@@ -0,0 +1,11 @@
+function regularFun(a: number, b:number) {
+    return(a+b)
+}
+
+let b = function annonymosFun(a: number, b:number) {
+    return(a+b)
+}
+
+let arrowFun = (a: number, b:number):number =>{
+    return(a+b)
+}
