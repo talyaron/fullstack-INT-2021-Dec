009f4486601c0e30614f623dd20b56ac8d8c3294
Dana <dana.kemper@payu.com>
2022-02-23 18:06:01 +0200

    Dana objects task

---
+++ Javascript/10-Objects/01-simple/OnlineTasks/Simple/Dana/index.ts
@@ -0,0 +1,25 @@
+
+
+const inTheNameOfTheFather = {
+    title: "inTheNameOfTheFather",
+    director: "Jim Sheridan",
+    yearOfRelease: 1993,
+}
+
+
+const indianaJones = {
+
+    title: "indianaJones", 
+    director: "James Mangold",
+    yearOfRelease: 2023, 
+}
+
+
+const joker = {
+
+    title: "Joker", 
+    director: "Todd Phillips",
+    yearOfRelease: 2019, 
+}
+
+
