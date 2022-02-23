2eb48fad4c37d24696dc1ecc0750403b506f448c
YakirD <38473489+yakirdv@users.noreply.github.com>
2022-02-23 13:27:56 +0200

    tests

--- Javascript/09-arrys/HomeTasks/Yakir/dist/index.js
+++ Javascript/09-arrys/HomeTasks/Yakir/dist/index.js
@@ -1,9 +1,10 @@
 function returnArray(arr, arrTypeReturn) {
+    console.log(arr); // for console check and debuging
     var filterArrType;
     if (arrTypeReturn === "numbers") {
         filterArrType = "number";
     }
-    else if (arrTypeReturn === "Strings") {
+    else if (arrTypeReturn === "strings") {
         filterArrType = "String";
     }
     var ArrNew = arr.map(function (elm) { return elm; }).filter(function (elm) { return typeof elm === filterArrType; });
@@ -11,11 +12,26 @@ function returnArray(arr, arrTypeReturn) {
         return ArrNew;
     }
     else {
-        alert("\u05DC\u05D0 \u05E7\u05D9\u05D1\u05DC\u05EA\u05D9 \u05E4\u05E7\u05D5\u05D3\u05D4 \u05DE\u05EA\u05D0\u05D9\u05DE\u05D4");
-        return [undefined];
+        // alert( `לא קיבלתי פקודה מתאימה` )
+        console.log("\u05DC\u05D0 \u05E7\u05D9\u05D1\u05DC\u05EA\u05D9 \u05E4\u05E7\u05D5\u05D3\u05D4 \u05DE\u05EA\u05D0\u05D9\u05DE\u05D4");
+        return [];
     }
 }
 // ****** Tester ****
-var inputArray = [1, 2, 3, 4, 5, 6, 7, 8, "gfgfg"];
-var NewArr = returnArray(inputArray, "numbers");
-console.log(NewArr);
+//*** Array Numbers  ***/
+var inputNewArr = [1, 2, 3, "gdfgdf", 4, 5, 6, 7, 8, "gfgfg"]; // will use for the testing
+var outputNewArr;
+console.log("Test:");
+console.log("--------------:");
+console.log("test 1: numbers and string ==> to Numbers Only");
+outputNewArr = returnArray(inputNewArr, "numbers");
+console.log(outputNewArr);
+console.log("--------------:");
+console.log("test 2: numbers and string ==> to Strings Only");
+outputNewArr = returnArray(inputNewArr, "strings");
+console.log(outputNewArr);
+console.log("--------------:");
+console.log("test 2: numbers and string ==> to Error Check");
+outputNewArr = returnArray(inputNewArr, "test error jkhjk");
+console.log(outputNewArr);
+console.log("--------------:");
--- Javascript/09-arrys/HomeTasks/Yakir/index.ts
+++ Javascript/09-arrys/HomeTasks/Yakir/index.ts
@@ -2,10 +2,12 @@
 
 function returnArray(arr:Array<any>, arrTypeReturn:String):Array<any> {
 
+    console.log (arr); // for console check and debuging
+
     var filterArrType:String;
     if(arrTypeReturn === "numbers") {
         filterArrType = "number";
-} else if(arrTypeReturn === "Strings"){
+} else if(arrTypeReturn === "strings"){
     filterArrType = "String";
 }
 
@@ -14,17 +16,33 @@ function returnArray(arr:Array<any>, arrTypeReturn:String):Array<any> {
     if(ArrNew.length >= 1){
         return ArrNew;
     } else{
-        alert( `לא קיבלתי פקודה מתאימה` )
-        return [undefined];
+        // alert( `לא קיבלתי פקודה מתאימה` )
+        console.log( `לא קיבלתי פקודה מתאימה` )
+        return [];
     }
 }
 
 // ****** Tester ****
+//*** Array Numbers  ***/
+const inputNewArr:Array<any> = [1,2,3,`gdfgdf`, 4,5,6,7,8,"gfgfg"]; // will use for the testing
+var outputNewArr:Array<any>;
 
-const inputArray:Array<any> = [1,2,3,4,5,6,7,8,"gfgfg"];
-const NewArr:Array<any> = returnArray(inputArray,`numbers`);
+console.log ("Test:");
+console.log ("--------------:");
+console.log ( `test 1: numbers and string ==> to Numbers Only`);
+outputNewArr = returnArray(inputNewArr,`numbers`);
+console.log (outputNewArr);
+console.log ("--------------:");
+console.log ( `test 2: numbers and string ==> to Strings Only`);
+outputNewArr = returnArray(inputNewArr,`strings`);
+console.log (outputNewArr);
+console.log ("--------------:");
+console.log ( `test 2: numbers and string ==> to Error Check`);
+outputNewArr = returnArray(inputNewArr,`test error jkhjk`);
+console.log (outputNewArr);
+console.log ("--------------:");
 
-console.log (NewArr);
+
 
 
 
