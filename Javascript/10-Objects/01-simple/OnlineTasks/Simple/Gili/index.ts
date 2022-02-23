85700c3a61006e460fa0faccb0ecbb7264df98da
GiliEllen <gilimena95@gmail.com>
2022-02-23 18:20:47 +0200

    function added

--- Javascript/10-Objects/01-simple/OnlineTasks/Simple/Gili/index.ts
+++ Javascript/10-Objects/01-simple/OnlineTasks/Simple/Gili/index.ts
@@ -17,3 +17,11 @@ const movie3 = {
     director:"Chris Buck",
     yearOfPublication: "2013",
 }
+
+function movieInfo(movie) {
+    return `the movie ${movie.name} was directed by ${movie.director} at the year ${movie.yearOfPublication}`
+}
+
+const info1 = movieInfo(movie1)
+const info2 = movieInfo(movie2)
+const info3 = movieInfo(movie3)
