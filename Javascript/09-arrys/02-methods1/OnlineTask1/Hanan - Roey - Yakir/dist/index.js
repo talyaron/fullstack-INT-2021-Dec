c0f8fcdc6ae67378b80b8355b841aa78dfa1170b
YakirD <38473489+yakirdv@users.noreply.github.com>
2022-02-23 09:41:16 +0200

    online-task

--- Javascript/09-arrys/02-methods1/OnlineTask1/Hanan - Roey - Yakir/dist/index.js
+++ Javascript/09-arrys/02-methods1/OnlineTask1/Hanan - Roey - Yakir/dist/index.js
@@ -1,4 +1,5 @@
 var arrayNumbers = [10, 15, 60, 80, 90, 100, 101, 110, 200, 300];
+console.log(arrayNumbers);
 var newArr = arrayNumbers.map(function (elm) {
     if (typeof elm === 'number') {
         if (elm > 100) {
--- Javascript/09-arrys/02-methods1/OnlineTask1/Hanan - Roey - Yakir/index.ts
+++ Javascript/09-arrys/02-methods1/OnlineTask1/Hanan - Roey - Yakir/index.ts
@@ -1,8 +1,8 @@
 
 
 const arrayNumbers: Array<any> = [10, 15, 60, 80, 90, 100, 101, 110, 200, 300];
-
-const newArr = arrayNumbers.map(elm=>{
+console.log(arrayNumbers);
+const newArr = arrayNumbers.map( elm=> {
     if(typeof elm === 'number') {
      if(elm>100)   {
         return elm;
