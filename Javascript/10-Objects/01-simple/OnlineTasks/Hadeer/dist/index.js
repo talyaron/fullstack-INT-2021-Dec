600c964adfaea71fee255c285464a203f2002b64
GiliEllen <gilimena95@gmail.com>
2022-02-23 20:06:59 +0200

    סוף המשימה

--- Javascript/10-Objects/01-simple/OnlineTasks/Hadeer/dist/index.js
+++ Javascript/10-Objects/01-simple/OnlineTasks/Hadeer/dist/index.js
@@ -23,7 +23,23 @@ var SixthSurprise = {
     penalty: "-500,000 $"
 };
 var SurpriseArray = [FirstSurprise, SecondSurprise, ThirdSurprise, FourthSurprise, FivthSurprise, SixthSurprise];
-function RandomSurprise(arrayOfSurprise, key) {
+function RandomSurprise(arrayOfSurprise) {
     var SurpriseNum = Math.floor(Math.random() * arrayOfSurprise.length);
-    console.log(SurpriseNum);
+    return (SurpriseNum);
 }
+function prizeOrPenalty(arrayOfSurprise, key1, key2, SurpriseNum) {
+    var penaltyOrPrize = (Math.floor(Math.random() * 2)) + 1;
+    if (penaltyOrPrize == 1) {
+        var result = arrayOfSurprise[SurpriseNum][key1];
+        alert("Your surprise is: " + result);
+        return result;
+    }
+    else if (penaltyOrPrize == 2) {
+        var result = arrayOfSurprise[SurpriseNum][key2];
+        alert("You owe us: " + result);
+        return result;
+    }
+}
+var randomNumber = RandomSurprise(SurpriseArray);
+var whatIsYourSurprise = prizeOrPenalty(SurpriseArray, "prize", "penalty", randomNumber);
+console.log(whatIsYourSurprise);
--- Javascript/10-Objects/01-simple/OnlineTasks/Hadeer/index.ts
+++ Javascript/10-Objects/01-simple/OnlineTasks/Hadeer/index.ts
@@ -1,47 +1,67 @@
-interface Surprise{
+interface Surprise {
     prize: string,
     penalty: any,
 }
 
-const FirstSurprise:Surprise ={
+const FirstSurprise: Surprise = {
     prize: "Pen",
     penalty: "-1$",
 }
 
-const SecondSurprise:Surprise={
+const SecondSurprise: Surprise = {
     prize: "Mug",
     penalty: "-5$"
 }
 
-const ThirdSurprise:Surprise={
+const ThirdSurprise: Surprise = {
     prize: "RayBan Sunglasess",
     penalty: "-200$",
 }
 
-const FourthSurprise:Surprise={
+const FourthSurprise: Surprise = {
     prize: "Chanel Watch",
     penalty: "-500$",
 }
 
-const FivthSurprise:Surprise={
+const FivthSurprise: Surprise = {
     prize: "Vications in the Bahamas",
     penalty: "-100,000$",
 }
 
-const SixthSurprise:Surprise={
+const SixthSurprise: Surprise = {
     prize: "BMW 320i",
     penalty: "-500,000 $",
 }
 
-const SurpriseArray :Array<any>= [FirstSurprise,SecondSurprise,ThirdSurprise,FourthSurprise,FivthSurprise,SixthSurprise];
+const SurpriseArray: Array<any> = [FirstSurprise, SecondSurprise, ThirdSurprise, FourthSurprise, FivthSurprise, SixthSurprise];
 
-function RandomSurprise(arrayOfSurprise:Array<Surprise>,key:any){
+function RandomSurprise(arrayOfSurprise: Array<Surprise>) {
 
     const SurpriseNum = Math.floor(Math.random() * arrayOfSurprise.length);
-    console.log(SurpriseNum);
+    return (SurpriseNum);
+}
+
+function prizeOrPenalty(arrayOfSurprise:Array<Surprise>, key1:string, key2:string, SurpriseNum:number) {
+
+    const penaltyOrPrize = (Math.floor(Math.random() * 2))+1;
+
+    if (penaltyOrPrize == 1) {
+        const result = arrayOfSurprise[SurpriseNum][key1]
+        alert(`Your surprise is: ${result}`)
+
+        return result
+    }
+    else if (penaltyOrPrize == 2) {
+        const result = arrayOfSurprise[SurpriseNum][key2]
+        alert(`You owe us: ${result}`)
+        return result
     }
 
+}
 
+const randomNumber = RandomSurprise(SurpriseArray)
+const whatIsYourSurprise = prizeOrPenalty(SurpriseArray, "prize", "penalty", randomNumber)
+console.log(whatIsYourSurprise)
 
 
 
