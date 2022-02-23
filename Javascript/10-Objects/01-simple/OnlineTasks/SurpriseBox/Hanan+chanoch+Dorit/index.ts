67842d10802148e1dc5caae6e621e3e42f65c6c2
doritgy <63942750+doritgy@users.noreply.github.com>
2022-02-23 19:55:13 +0200

    excercise

---
+++ Javascript/10-Objects/01-simple/OnlineTasks/SurpriseBox/Hanan+chanoch+Dorit/index.ts
@@ -0,0 +1,45 @@
+interface surprise{
+    prize:string,
+    penalty:number,
+}
+
+const prize1:surprise = {
+    prize: "checkers",
+    penalty:-15
+
+}
+
+const prize2:surprise = {
+    prize: "chess",
+    penalty:-25
+
+}
+
+const prize3:surprise = {
+    prize: "lego",
+    penalty:-150
+
+}
+
+const prize4:surprise = {
+    prize: "ferrari",
+    penalty:-1500
+
+}
+
+let prizes:Array<surprise> = [prize1,prize2,prize3,prize4]
+
+function getSurprise(prizes:Array<surprise> ){
+    const sur = Math.round(Math.random()*3)
+    //console.log(sur)
+    const rand=Math.round(Math.random()*1)
+    if (rand ===0){
+        return  prizes[sur].prize
+    }
+    else {
+        return  prizes[sur].penalty
+    }
+    
+}
+
+console.log(getSurprise(prizes));
