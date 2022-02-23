4809cefb4a74548034a5df7604727d6a0505a461
adamepel <adamepel11@gmail.com>
2022-02-23 18:16:02 +0200

    FIX

--- Javascript/10-Objects/01-simple/OnlineTasks/Simple/Adam/dist/index.js
+++ Javascript/10-Objects/01-simple/OnlineTasks/Simple/Adam/dist/index.js
@@ -16,9 +16,9 @@ var truman = {
 function Movie(Movieintro) {
     return " " + Movieintro.MoviesName + " " + Movieintro.Director + " " + Movieintro.yearOfPublication;
 }
-var movie1 = (Queen);
+var movie1 = Movie(Queen);
 console.log(movie1);
-var movie2 = (Forrest);
+var movie2 = Movie(Forrest);
 console.log(movie2);
-var movie3 = (truman);
+var movie3 = Movie(truman);
 console.log(movie3);
