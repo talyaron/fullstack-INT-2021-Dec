9a747988b7c4cafab2df3695b01b57f4e103d8af
LitalCohen479 <litalcohen479@gmail.com>
2022-02-23 19:10:37 +0200

    s

--- Javascript/10-Objects/01-simple/OnlineTasks/Simple/Hodaya Cohen/dist/index.js
+++ Javascript/10-Objects/01-simple/OnlineTasks/Simple/Hodaya Cohen/dist/index.js
@@ -1,24 +1,24 @@
-var movie1 = {
+var movieTinder = {
     name: "We'm the tinder",
     stageName: "Plisiti Moris",
     releaseDate: "2/2/2022"
 };
-var movie2 = {
+var movieWW84 = {
     name: "WW84",
     stageName: "Pati Jankings",
     releaseDate: "27/5/2021"
 };
-var movie3 = {
+var movieHomeTeam = {
     name: "Home Team",
     stageName: "Daniel Kinana",
     releaseDate: "28/1/2022"
 };
-function theater(pesonObj) {
-    return " The movie " + pesonObj.name + "Name of the director " + pesonObj.stagName + " You made a premiere " + pesonObj.releaseDate;
+function theater(movies) {
+    return " The movie " + movies.name + "Name of the director " + movies.stagName + " You made a premiere " + movies.releaseDate;
 }
-var movie1 = theater(movie1);
+var movie1 = theater(movieTinder);
 console.log((movie1));
-var movie2 = theater(movie2);
+var movie2 = theater(movieWW84);
 console.log(movie2);
-var movie3 = theater(movie3);
+var movie3 = theater(movieHomeTeam);
 console.log(movie3);
--- Javascript/10-Objects/01-simple/OnlineTasks/Simple/Hodaya Cohen/index.ts
+++ Javascript/10-Objects/01-simple/OnlineTasks/Simple/Hodaya Cohen/index.ts
@@ -1,24 +1,24 @@
-const movie1 = {
+const movieTinder = {
     name: "We'm the tinder",
     stageName: "Plisiti Moris",
     releaseDate: "2/2/2022",
 };
-const movie2 = {
+const movieWW84 = {
     name: "WW84",
     stageName: "Pati Jankings",
     releaseDate: "27/5/2021",
 };
-const movie3 = {
+const movieHomeTeam = {
     name: "Home Team",
     stageName: "Daniel Kinana",
     releaseDate: "28/1/2022",
 };
-function theater(pesonObj){
-    return ` The movie ${pesonObj.name}Name of the director ${pesonObj.stagName} You made a premiere ${pesonObj.releaseDate}`
+function theater(movies){
+    return ` The movie ${movies.name}Name of the director ${movies.stagName} You made a premiere ${movies.releaseDate}`
 }
-const movie1 = theater(movie1);
+const movie1 = theater(movieTinder);
 console.log((movie1)
-const movie2 = theater(movie2);
+const movie2 = theater(movieWW84);
 console.log(movie2)
-const movie3 = theater(movie3);
+const movie3 = theater(movieHomeTeam);
 console.log(movie3)
