c354ab4494573a55ec864bfe0c777a80f4e40cdb
Rowy92 <roeyshpringer@gmail.com>
2022-02-25 19:05:00 +0200

    rs started object hw

---
+++ Javascript/10-Objects/01-simple/HomeTask/Roey Shpringer/index.ts
@@ -0,0 +1,35 @@
+
+
+interface friends  {
+    firstName: string,
+    lastName: string,
+    adress: string,
+    age: number
+}
+
+let friendOne:friends = {
+    firstName: 'Ziv',
+    lastName: 'Morgan',
+    adress: 'Tel Mond',
+    age : 19
+}
+
+let friendTwo:friends = {
+    firstName: 'Or',
+    lastName: 'Oren',
+    adress: 'Hulon',
+    age : 20
+}
+
+let friendThree:friends = {
+    firstName: 'Eve',
+    lastName: 'Idk',
+    adress: 'Eilat',
+    age : 23
+}
+
+let friendsArray = [friendOne, friendTwo, friendThree];
+
+function friendsAdresses(array){
+    
+}
