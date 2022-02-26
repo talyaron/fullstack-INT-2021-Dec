2159e70d0e94d5da1b5b15676b3bddaf72e92692
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-02-26 21:12:19 +0200

    with uncomplited challenge

--- Javascript/10-Objects/01-simple/HomeTask/gal/challenge.ts
+++ Javascript/10-Objects/01-simple/HomeTask/gal/challenge.ts
@@ -1,15 +1,31 @@
 class myFriends{
-    firstName:string
-    lastNmae:string
-    address:string
-    age:number
-    friends:myFriends[]
-    constructor (firstName, lastName ,address, age, friends){
+    firstName: string;
+    lastNmae: string;
+    address: string;
+    age: number;
+    friends: myFriends[];
+    constructor (firstName:string, lastName:string ,address:string, age:number, friends:Array<myFriends>){
         this.firstName=firstName;
         this.lastNmae=lastName;
         this.address=address;
         this.age=age;
-        this.friends=friends;
+        this.friends=friends
+    }
+    
+    MyArr(){
+        let arr:Array<any> =[]
+        this.friends.forEach(elm => {
+            arr.push(elm.address)
+        });
+        return arr
     }
 }
 
+let Shahar= new myFriends ("Shahar", "Abu", "Harish",24,[])
+let Yam= new myFriends ("Yam", "Rafaeli", "Natanya",23,[Shahar])
+let Ofek= new myFriends ("Ofek", "Sery", "Ramat-Gan",28,[Shahar,Yam])
+let Rotem= new myFriends ("Rotem", "Fishler", "Tel-Aviv",23,[Shahar,Yam,Ofek])
+let Kobe= new myFriends ("Kobe", "Font", "Eilat",24,[Shahar,Yam,Ofek,Rotem])
+Shahar['friends'].push(Shahar,Yam,Ofek,Rotem,Kobe)
+console.log(Shahar.MyArr())
+// console.log (`You have ${} friends and they live in ${}`)
--- Javascript/10-Objects/01-simple/HomeTask/gal/dist/challenge.js
+++ Javascript/10-Objects/01-simple/HomeTask/gal/dist/challenge.js
@@ -6,5 +6,20 @@ var myFriends = /** @class */ (function () {
         this.age = age;
         this.friends = friends;
     }
+    myFriends.prototype.MyArr = function () {
+        var arr = [];
+        this.friends.forEach(function (elm) {
+            arr.push(elm.address);
+        });
+        return arr;
+    };
     return myFriends;
 }());
+var Shahar = new myFriends("Shahar", "Abu", "Harish", 24, []);
+var Yam = new myFriends("Yam", "Rafaeli", "Natanya", 23, [Shahar]);
+var Ofek = new myFriends("Ofek", "Sery", "Ramat-Gan", 28, [Shahar, Yam]);
+var Rotem = new myFriends("Rotem", "Fishler", "Tel-Aviv", 23, [Shahar, Yam, Ofek]);
+var Kobe = new myFriends("Kobe", "Font", "Eilat", 24, [Shahar, Yam, Ofek, Rotem]);
+Shahar['friends'].push(Shahar, Yam, Ofek, Rotem, Kobe);
+console.log(Shahar.MyArr());
+// console.log (`You have ${} friends and they live in ${}`)
