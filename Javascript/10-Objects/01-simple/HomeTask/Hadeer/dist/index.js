21396eb0f1fa763ee7bbc9b33df38791e523c25a
hadeer57 <hadeer57@hotmail.com>
2022-03-02 18:31:54 +0200

    hometask

---
+++ Javascript/10-Objects/01-simple/HomeTask/Hadeer/dist/index.js
@@ -0,0 +1,30 @@
+var MyDetails = {
+    name: "Hadeer",
+    lastName: "HAbiballah",
+    address: "Tira",
+    job: "Tissue Typing LAB"
+};
+var Mustafa = {
+    name: "Mustafa",
+    lastName: "Qassim",
+    address: "Tira",
+    job: "Blood Bank Lab"
+};
+var Rula = {
+    name: "Rula",
+    lastName: "Mansour",
+    address: "Haifa",
+    job: "Teacher"
+};
+var Abeer = {
+    name: "Abeer",
+    lastName: "Dabbah",
+    address: "Maker",
+    job: "Nurse"
+};
+var Rana = {
+    name: "Rana",
+    lastName: "Khamisey",
+    address: "Ako",
+    job: "Enviroment"
+};
