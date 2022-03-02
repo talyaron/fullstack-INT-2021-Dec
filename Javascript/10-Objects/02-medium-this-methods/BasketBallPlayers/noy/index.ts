f6bea15d762bf466b1b4e9f71faae5cff3cfdab3
MartinoySolomon <noy.kakoun@gmail.com>
2022-03-02 20:22:29 +0200

    .

--- Javascript/10-Objects/02-medium-this-methods/BasketBallPlayers/noy/index.ts
+++ Javascript/10-Objects/02-medium-this-methods/BasketBallPlayers/noy/index.ts
@@ -6,21 +6,21 @@
 // for each type (with object that contain the results)
 
 
-const player ={
+const player = {
     name: "Moshe",
-    gameScores: [30,18,24],
-    averageScore: function():number {
-        let sum:number = 0;
+    gameScores: [30, 18, 24],
+    averageScore: function (): number {
+        let sum: number = 0;
         this.gameScores.forEach(element => {
-            sum+=element;
+            sum += element;
         });
-        return (sum/(this.gameScores.length));
+        return (sum / (this.gameScores.length));
     },
-    threePointers: [3,5,7],
-    twoPointers: [2,8,6],
-    freeThrows: [1,8,4],
-    averageScoreThrows: function() {
-        let sum:number = 0;
+    threePointers: [3, 5, 7],
+    twoPointers: [2, 8, 6],
+    freeThrows: [1, 8, 4],
+    averageScoreThrows: function () {
+        let sum: number = 0;
         let averages = {
             threePointers: 0,
             twoPointers: 0,
@@ -28,20 +28,20 @@ const player ={
         }
 
         this.threePointers.forEach(element => {
-            sum+=element;
+            sum += element;
         })
-        averages.threePointers = sum/this.threePointers.length;
+        averages.threePointers = sum / this.threePointers.length;
         sum = 0;
         this.twoPointers.forEach(element => {
-            sum+=element;
+            sum += element;
         })
-        averages.twoPointers = sum/this.twoPointers.length;
-        sum=0;
+        averages.twoPointers = sum / this.twoPointers.length;
+        sum = 0;
         this.freeThrows.forEach(element => {
-            sum+=element;
+            sum += element;
         })
-        averages.freeThrows = sum/this.freeThrows.length;
-        
+        averages.freeThrows = sum / this.freeThrows.length;
+
         return (averages);
     },
 
