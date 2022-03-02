7b2eeb1af577260f598f095f2410120390f86706
LitalCohen479 <litalcohen479@gmail.com>
2022-03-02 19:18:45 +0200

    v

--- Javascript/10-Objects/02-medium-this-methods/followup/Lital Cohen/index.ts
+++ Javascript/10-Objects/02-medium-this-methods/followup/Lital Cohen/index.ts
@@ -1,19 +1,15 @@
 const tzavi = {
     name: "TzaviTsav",
     preferdFoods: ["ice cream", "candy"],
-    HatedFoods:["lettuce", "carrot"],
-    myFoodTaste: function ():string {
-   
-      return `Hello, my name is ${this.name}, and I like eating ${this.preferdFoods[0]} and I hate eating ${this.HatedFoods[1]}`;
+    HatedFoods: ["lettuce", "carrot"],
+    myFoodTaste: function (): string {
+
+        return `Hello, my name is ${this.name}, and I like eating ${this.preferdFoods[0]} and I hate eating ${this.HatedFoods[1]}`;
     },
-   
-  };
-  
-  const myFood:string = tzavi.myFoodTaste();
-  
 
-  
-  console.log(myFood);
+};
+
+const myFood: string = tzavi.myFoodTaste();
+console.log(myFood);
 
 
-  
