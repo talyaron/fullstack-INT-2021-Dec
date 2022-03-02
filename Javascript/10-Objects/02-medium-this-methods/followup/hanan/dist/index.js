88153e6892596a073d0606766a18563b4684f190
hanan <jigaart0@gmail.com>
2022-03-02 19:07:20 +0200

    dsa

--- Javascript/10-Objects/02-medium-this-methods/followup/hanan/dist/index.js
+++ Javascript/10-Objects/02-medium-this-methods/followup/hanan/dist/index.js
@@ -4,9 +4,7 @@ var tzav = {
     HatedFoods: ['tuna', "and jellyfishes"],
     SayFoodloveAndHate: function () {
         return "hi my name is " + this.name + " and i want to tell you about my diet i love to eat " + this.preferdFoods + " \n        and cant stand " + this.HatedFoods;
-    },
-    let: let,
-    goodFood: string = tzavi.SayFoodloveAndHate(),
-    console: console,
-    : .log(goodFood)
+    }
 };
+var goodFood = tzav.SayFoodloveAndHate();
+console.log(goodFood);
