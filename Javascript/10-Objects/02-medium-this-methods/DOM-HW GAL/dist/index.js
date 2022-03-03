badd622f92cb25e1eb62c4796d13346caa3c3e2d
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-03-04 00:08:02 +0200

    ג

---
+++ Javascript/10-Objects/02-medium-this-methods/DOM-HW GAL/dist/index.js
@@ -0,0 +1,10 @@
+var firhead = document.querySelector("#firhead"); //any is also fine (in place of `htmlElement`)
+var thehead = document.querySelector("#thehead");
+firhead.style.background = 'red';
+// function copyPast (element:HTMLElement):void{ //Tal video's example
+//     for (let i=0; i<10; i++){
+//         element.innerText +=i;
+//     }
+// }
+// copyPast (firhead)
+var firhead = userList.querySelectorAll(".highlighted");
---
+++ Javascript/10-Objects/02-medium-this-methods/DOM-HW GAL/index.ts
@@ -0,0 +1,15 @@
+const firhead:HTMLElement=document.querySelector("#firhead"); //any is also fine (in place of `htmlElement`)
+const thehead:HTMLElement=document.querySelector("#thehead");
+
+firhead.style.background='red'
+
+// function copyPast (element:HTMLElement):void{ //Tal video's example
+//     for (let i=0; i<10; i++){
+//         element.innerText +=i;
+//     }
+// }
+
+// copyPast (firhead)
+
+const firhead = userList.querySelectorAll(".highlighted");
+
