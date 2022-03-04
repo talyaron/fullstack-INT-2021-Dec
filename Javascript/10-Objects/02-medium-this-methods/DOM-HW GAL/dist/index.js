3bb67b7529a16d1e1811553e7c35d676163f0ff9
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-03-04 19:20:12 +0200

    finish work

--- Javascript/10-Objects/02-medium-this-methods/DOM-HW GAL/dist/index.js
+++ Javascript/10-Objects/02-medium-this-methods/DOM-HW GAL/dist/index.js
@@ -1,9 +1,12 @@
 var firhead = document.querySelector("#firhead"); //any is also fine (in place of `htmlElement`)
 var thehead = document.querySelector("#thehead");
+// console.log(firhead)
+// console.log(thehead)
 firhead.style.background = 'red';
-// function copyPast (element:HTMLElement):void{ //Tal video's example
-//     for (let i=0; i<10; i++){
-//         element.innerText +=i;
-//     }
-// }
-// copyPast (firhead)
+function copyPast(element) {
+    firhead.innerText = thehead.innerText;
+    return thehead;
+}
+copyPast(firhead);
+// firhead.innerText="100 pls" //the change (turn the comment off)
+// thehead.innerText="turn the comment off"
