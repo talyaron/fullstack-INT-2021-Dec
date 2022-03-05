01c74cb472629415de8c94a9726aadb8a204e329
GiliEllen <gilimena95@gmail.com>
2022-03-05 19:21:18 +0200

    שינוי שם לפונקציה לפי הסרטון

--- Javascript/10-Objects/03-Dom manipulation/HW/Gili/index.ts
+++ Javascript/10-Objects/03-Dom manipulation/HW/Gili/index.ts
@@ -4,9 +4,9 @@ const form = document.querySelector('form');
 const textToCopy: HTMLElement = document.querySelector('#copy_text');
 const textToPaste: HTMLElement = document.querySelector('#paste_text');
 
-function copyAndPaste(element: HTMLElement) {
+function copyText(element: HTMLElement) {
     textToPaste.innerText = textToCopy.innerText
     return textToPaste
 }
 
-copyAndPaste(textToCopy)
+copyText(textToCopy)
