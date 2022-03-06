03dadb9672c9ed04689865159c52399a625ee053
Rowy92 <roeyshpringer@gmail.com>
2022-03-06 19:35:38 +0200

    rs onlineWork one

---
+++ Javascript/11-DOM-maanipulations/MultipleElements/Randomness/Roey Shpringer/index.ts
@@ -0,0 +1,36 @@
+
+const boxes: any = document.querySelectorAll('.box')
+
+function getRandomEverything(){
+for (let i = 0; i < boxes.length; i++) {
+    boxes[i].style.backgroundColor = getRandomColor()
+    boxes[i].style.top = getRandomTop()
+    boxes[i].style.left = getRandomLeft()
+}
+}
+setInterval(getRandomEverything, 1000)
+console.dir(boxes)
+
+
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+function getRandomTop() {
+    let top = Math.round(Math.random() * screen.height) + "px"
+    return top
+}
+console.log(getRandomLeft())
+console.log(getRandomTop())
+
+function getRandomLeft(){
+    let left = Math.round(Math.random() * screen.width) + "px"
+    return left
+}
