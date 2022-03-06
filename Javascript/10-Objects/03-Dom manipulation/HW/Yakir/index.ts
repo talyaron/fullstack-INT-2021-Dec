ea28a41753431421f8b01e937c8eb50c7be05c35
YakirD <38473489+yakirdv@users.noreply.github.com>
2022-03-06 17:44:46 +0200

    fix 1 after the zoom

--- Javascript/10-Objects/03-Dom manipulation/HW/Yakir/index.ts
+++ Javascript/10-Objects/03-Dom manipulation/HW/Yakir/index.ts
@@ -1,4 +1,5 @@
-function copytext (text1:HTMLElement, text2:HTMLElement){
-    text2.innerText=text2.innerText
+function copytext (){
+    document.querySelector('#text2').textContent = document.querySelector('#text1').textContent
 }
+
 
