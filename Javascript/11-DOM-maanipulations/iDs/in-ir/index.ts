d282d95a4d3a734a00b26dd02db814aa391c15c8
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-03-06 20:33:34 +0200

    eee

---
+++ Javascript/11-DOM-maanipulations/iDs/in-ir/index.ts
@@ -0,0 +1,15 @@
+const box:NodeListOf<Element> = document.querySelectorAll(`#box`);
+
+
+
+function changeText(text) {
+    document.getElementById("id").innerText=text
+  }
+  
+//   function changeText(text) {
+//     document.getElementById("title").innerText=text
+//   }
+//   const button = document.getElementById('button');
+//   button.addEventListener('click', () => changeText('the new text I want'));
+  
+  
