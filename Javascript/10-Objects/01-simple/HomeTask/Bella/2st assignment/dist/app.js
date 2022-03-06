d7edb16a9ea15d37d42db61e70b854bf1dd71a3b
Bella-UI <bellabrailovsky@gmail.com>
2022-03-06 10:40:24 +0200

    06/03

--- Javascript/10-Objects/01-simple/HomeTask/Bella/2st assignment/dist/app.js
+++
@@ -1,9 +0,0 @@
-var a = +prompt("Please enter a positive number");
-var b = +prompt("Please enter another positive number");
-var dif = (a - b);
-function calculator(dif) {
-    if (typeof (a + " && " + b) === "number") {
-        return dif;
-    }
-}
-console.log(dif);
