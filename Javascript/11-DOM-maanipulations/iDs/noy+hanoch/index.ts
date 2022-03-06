6f27a1c32950e113a501b860efd3d15e612c6551
MartinoySolomon <noy.kakoun@gmail.com>
2022-03-06 19:53:34 +0200

    fix

--- Javascript/11-DOM-maanipulations/iDs/noy+hanoch/index.ts
+++ Javascript/11-DOM-maanipulations/iDs/noy+hanoch/index.ts
@@ -1,5 +1,5 @@
 const boxes: any = document.querySelectorAll('.box')
 
-    boxes.forEach(element => {
-        element.innerText = element.id
-    })
+boxes.forEach(element => {
+    element.innerText = element.id
+})
