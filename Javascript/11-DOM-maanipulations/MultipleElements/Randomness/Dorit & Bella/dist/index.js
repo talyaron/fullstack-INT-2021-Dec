07829cb95b4e2028a2cf38ca41deb34ebf59da89
Bella-UI <bellabrailovsky@gmail.com>
2022-03-06 18:26:46 +0200

    06/03

---
+++ Javascript/11-DOM-maanipulations/MultipleElements/Randomness/Dorit & Bella/dist/index.js
@@ -0,0 +1,28 @@
+var boxs = document.querySelectorAll('.box');
+console.dir(boxs);
+boxs[0].style.backgroundColor = 'yellow';
+boxs.forEach(function (element) {
+    element.style.backgroundColor = getRandomColor(),
+        element.style.offsetHeight = getRandomPosition(element),
+        element.style.offsetWidth = getRandomPosition(element);
+});
+//create random postions on the screen
+//create random sizes
+//* change it every 1 second (and use transfer )css
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    console.log(color);
+    return color;
+}
+function getRandomPosition(element) {
+    var x = document.body.offsetHeight - element.clientHeight;
+    var y = document.body.offsetWidth - element.clientWidth;
+    var randomX = Math.floor(Math.random() * x);
+    var randomY = Math.floor(Math.random() * y);
+    return [randomX, randomY];
+}
+console.dir(boxs);
---
+++ Javascript/11-DOM-maanipulations/MultipleElements/Randomness/Dorit & Bella/index.ts
@@ -0,0 +1,33 @@
+const boxs:any = document.querySelectorAll('.box');
+
+console.dir(boxs)
+boxs[0].style.backgroundColor = 'yellow';
+boxs.forEach(element=>{
+    element.style.backgroundColor = getRandomColor(),
+    element.style.offsetHeight = getRandomPosition(element),
+    element.style.offsetWidth = getRandomPosition(element)
+})
+
+
+//create random postions on the screen
+//create random sizes
+//* change it every 1 second (and use transfer )css
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    console.log(color)
+    return color;
+  }
+
+  function getRandomPosition(element) {
+    let x = document.body.offsetHeight-element.clientHeight;
+    let y = document.body.offsetWidth-element.clientWidth;
+    let randomX = Math.floor(Math.random()*x);
+    let randomY = Math.floor(Math.random()*y);
+    return [randomX,randomY];
+  }
+  console.dir (boxs)
+  
