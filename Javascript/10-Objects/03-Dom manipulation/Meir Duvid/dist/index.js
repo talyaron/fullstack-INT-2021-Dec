c182cf704e39680bb7f5f59dec87527d91528836
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-03-06 19:39:39 +0200

    eeee

--- Javascript/10-Objects/03-Dom manipulation/Meir Duvid/dist/index.js
+++ Javascript/10-Objects/03-Dom manipulation/Meir Duvid/dist/index.js
@@ -2,3 +2,8 @@ var root1 = document.querySelector("#root1");
 var root2 = document.querySelector("#root2");
 console.dir(root1);
 console.dir(root2);
+function copy(copy) {
+    root1.innerText = root2.innerText;
+    return root1;
+}
+copy(root2);
--- Javascript/10-Objects/03-Dom manipulation/Meir Duvid/index.ts
+++ Javascript/10-Objects/03-Dom manipulation/Meir Duvid/index.ts
@@ -3,3 +3,12 @@ const root2:HTMLElement = document.querySelector("#root2");
 
 console.dir(root1);
 console.dir(root2);
+
+
+function copy(copy:HTMLElement) {
+    root1.innerText= root2.innerText
+    return root1
+
+}
+
+copy(root2);
