1e86e5e99050dfd66ed0a3543bebe64b9a1677e4
hadeer57 <hadeer57@hotmail.com>
2022-03-07 20:33:08 +0200

    ONLINETASK

--- Javascript/11-DOM-maanipulations/iDs/Hodaya,Hadeer,Juda/dist/index.js
+++ Javascript/11-DOM-maanipulations/iDs/Hodaya,Hadeer,Juda/dist/index.js
@@ -1,6 +1,6 @@
 var boxId = document.querySelectorAll('.box');
 boxId.forEach(function (element) {
-    var showId = element.id;
+    var showId = element.id + element.style.background;
     element.innerText = showId;
     console.log(showId);
 });
--- Javascript/11-DOM-maanipulations/iDs/Hodaya,Hadeer,Juda/index.ts
+++ Javascript/11-DOM-maanipulations/iDs/Hodaya,Hadeer,Juda/index.ts
@@ -2,9 +2,11 @@
 const boxId:any = document.querySelectorAll('.box');
 
 boxId.forEach(element=>{
-    let showId:string = element.id ;
+
+    let showId:any = element.id +element.style.background ;
     element.innerText = showId;
     console.log(showId);
 })
+
 
 
