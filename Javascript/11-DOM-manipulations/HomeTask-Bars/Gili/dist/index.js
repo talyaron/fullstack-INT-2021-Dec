0871e33d08a2e7dada7e8f20725726f1770b1bdb
GiliEllen <gilimena95@gmail.com>
2022-03-07 18:38:17 +0200

    עבודת עמודות

---
+++ Javascript/11-DOM-manipulations/HomeTask-Bars/Gili/dist/index.js
@@ -0,0 +1,17 @@
+console.log("hi");
+/*Array of bars heights defiend*/
+/*Adding numbers to the array will add a matching bar*/
+var barsHeight = [47, 90, 30, 15, 85, 20, 50];
+/*For each elemnt in the array define a height as the number
+suggests, and write the number*/
+barsHeight.forEach(function (element) {
+    var addedDiv = document.createElement("wrapper__bar");
+    console.log(addedDiv);
+    addedDiv.style.height = (element * 2) + "px"; //The number is multyplayed by two for definition purposes
+    addedDiv.style.width = 100 + "px";
+    addedDiv.style.backgroundColor = "gray";
+    var text = document.createTextNode("" + element);
+    addedDiv.appendChild(text);
+    var wrapper = document.getElementById("wrapper");
+    wrapper.appendChild(addedDiv);
+});
