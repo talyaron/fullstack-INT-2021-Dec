b9c89807098b2c008ca4390c369da967a84dc5ec
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-03-08 20:56:21 +0200

    Meir&Hannan

--- Javascript/11-DOM-manipulations/HomeTask-Bars/Hanan-Meir/index.ts
+++ Javascript/11-DOM-manipulations/HomeTask-Bars/Hanan-Meir/index.ts
@@ -1,7 +1,11 @@
 const numberArray: Array<number> = [25, 55, 30];
 const warp: HTMLDivElement = document.querySelector(`.wrapper`);
 const sti: any = document.querySelectorAll(`.stick`);
-const barColors = ['#FF6633', '#FFB399', '#FF33FF'];
+
+
+console.dir(sti)
+
+console.log(sti[0].id)
 
 for (let i = 0; i < numberArray.length; i++) {
     let bar = sti[i];
@@ -14,3 +18,21 @@ for (let i = 0; i < numberArray.length; i++) {
  
 
   
+function refresh() {
+    for (let i = 0; i < sti.length; i++) {
+        sti[i].innerHTML = sti[i].id
+        
+        sti[i].style.backgroundColor = getRandomColorRgb()
+    }
+}
+
+setInterval(refresh)
+
+function getRandomColorRgb() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+} 
