82a6a8bd6fa927e0a3c4271615b47a75bcf331f2
YakirD <38473489+yakirdv@users.noreply.github.com>
2022-03-09 20:32:31 +0200

    overLapCheck

--- Javascript/11-DOM-manipulations/shira - hanan - yakir/dist/index.js
+++ Javascript/11-DOM-manipulations/shira - hanan - yakir/dist/index.js
@@ -4,6 +4,11 @@ function coronaMove() {
         element.style.top = randomPosition() + "vh";
         element.style.left = randomPosition() + "vw";
     });
+    // console.log(ugiCron[5].getBoundingClientRect().top);
+    // console.log(ugiCron[5].getBoundingClientRect().bottom);
+    // console.log(ugiCron[5].getBoundingClientRect().left);
+    // console.log(ugiCron[5].getBoundingClientRect().right);
+    overLapCheck();
 }
 console.log(ugiCron);
 function randomPosition() {
@@ -11,4 +16,20 @@ function randomPosition() {
     postion = Math.random() * 100;
     return postion;
 }
-var intervalID = setInterval(coronaMove, 1000);
+var intervalID = setInterval(coronaMove, 10000);
+function overLapCheck() {
+    for (var i = 0; i < 26; i++) {
+        for (var j = i + 1; j < i - 1; j++) {
+            if (ugiCron[i].getBoundingClientRect().top >
+                ugiCron[j].getBoundingClientRect().bottom ||
+                ugiCron[i].getBoundingClientRect().right <
+                    ugiCron[j].getBoundingClientRect().left ||
+                ugiCron[i].getBoundingClientRect().bottom <
+                    ugiCron[j].getBoundingClientRect().top ||
+                ugiCron[i].getBoundingClientRect().left >
+                    ugiCron[j].getBoundingClientRect().right) {
+                console.log("covid Collision id " + i + " and " + j);
+            }
+        }
+    }
+}
