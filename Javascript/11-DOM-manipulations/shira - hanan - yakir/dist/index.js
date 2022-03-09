581d300789c25bef3ffa5f5d5af413e4c0926981
YakirD <38473489+yakirdv@users.noreply.github.com>
2022-03-09 19:22:48 +0200

    update shira hanan yakir

--- Javascript/11-DOM-manipulations/shira - hanan - yakir/dist/index.js
+++ Javascript/11-DOM-manipulations/shira - hanan - yakir/dist/index.js
@@ -6,9 +6,6 @@ function coronaMove() {
     });
 }
 console.log(ugiCron);
-// document.documentElement.clientHeight /* 800 */
-// window.innerHeight /* 800 */
-// window.outerHeight /* 900 */
 function randomPosition() {
     var postion;
     postion = Math.random() * 100;
--- Javascript/11-DOM-manipulations/shira - hanan - yakir/index.ts
+++ Javascript/11-DOM-manipulations/shira - hanan - yakir/index.ts
@@ -10,9 +10,6 @@ ugiCron.forEach((element) => {
 }
 console.log(ugiCron);
 
-// document.documentElement.clientHeight /* 800 */
-// window.innerHeight /* 800 */
-// window.outerHeight /* 900 */
 
 function randomPosition() {
   let postion: number;
