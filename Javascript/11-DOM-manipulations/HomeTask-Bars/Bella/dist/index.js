6f470da719e0a4bfb3bbf6cd2fc671cf283004f4
Bella-UI <bellabrailovsky@gmail.com>
2022-03-10 17:08:00 +0200

    ...

--- Javascript/11-DOM-manipulations/HomeTask-Bars/Bella/dist/index.js
+++ Javascript/11-DOM-manipulations/HomeTask-Bars/Bella/dist/index.js
@@ -8,3 +8,25 @@
 // })
 // }
 // getTextbYId()
+// const rowsArray = [1,2,3,4,5,6,7]
+// const columnArray = ["a", "b", "c", "d"]
+// const data = [4,5,7]
+// function addDataValues () {
+//   data.forEach(Element=>{
+//     element = data.push
+//   })
+// }
+var coundArr = [22, 45, 61, 88];
+console.dir(coundArr);
+function dataVisual(data) {
+    data.forEach(function (elm) {
+        var addData = document.createElement("wrapper__column");
+        addData.classList.add("wrapper__column");
+        addData.style.height = elm * 3 + "px";
+        var text = document.createTextNode("" + elm);
+        addData.appendChild(text);
+        var dataBars = document.getElementById("wrapper");
+        dataBars.appendChild(addData);
+    });
+}
+dataVisual(coundArr);
