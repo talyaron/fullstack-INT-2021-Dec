dae9e1d84616933ed71d50255e9ed6f1ee45e86e
Bella-UI <bellabrailovsky@gmail.com>
2022-03-10 17:58:20 +0200

    ...

--- Javascript/11-DOM-manipulations/HomeTask-Bars/Bella/index.ts
+++ Javascript/11-DOM-manipulations/HomeTask-Bars/Bella/index.ts
@@ -1,43 +1,28 @@
-
-// const boxes:any = document.querySelectorAll('.box');
-// console.dir(boxes)
-
-// function getTextbYId() {
-//   boxes.forEach(element=>{
-//     element.innerText = element.id
-//     console.log (element.id)
-//     return element.innerText
-// })
-// }
-
-// getTextbYId()
+const coundArr: Array < number > = [22, 45, 102, 88];
+console.dir(coundArr);
 
 
-// const rowsArray = [1,2,3,4,5,6,7]
-// const columnArray = ["a", "b", "c", "d"]
-// const data = [4,5,7]
+function dataVisual(data: Array < number > ) {
+  data.forEach((elm) => {
+    let addData = document.createElement("wrapper__column");
+    addData.classList.add("wrapper__column");
+    addData.style.height = elm * 3 + "px";
+    let text = document.createTextNode(`${elm}`);
+    addData.appendChild(text);
+    //random coloring
+    let randomColor: any = function getRandomColor() {
+      let letters = '0123456789ABCDEF';
+      let color = '#';
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+    addData.style.backgroundColor = randomColor();
 
-// function addDataValues () {
-//   data.forEach(Element=>{
-//     element = data.push
-    
-//   })
-// }
 
-
-const coundArr: Array <number> = [22, 45, 61, 88];
-console.dir (coundArr);
-
-function dataVisual (data:Array <number>){
-  data.forEach ((elm)=>{
-let addData = document.createElement ("wrapper__column");
-addData.classList.add ("wrapper__column");
-addData.style.height = elm * 3 + "px";
-let text = document.createTextNode (`${elm}`);
-addData.appendChild (text);
-
-let dataBars = document.getElementById ("wrapper")
-dataBars.appendChild (addData);
-    });
+    let dataBars = document.getElementById("wrapper")
+    dataBars.appendChild(addData);
+  });
 }
-dataVisual (coundArr);
+dataVisual(coundArr);
