d965476c06e34659c55df9d4d417ac0f9f54f7a7
hanochshachar <goralot550@gmail.com>
2022-03-13 01:00:29 +0200

    אסטרואידים

--- Javascript/11-DOM-manipulations/flyingRocks/hanoch/index.ts
+++ Javascript/11-DOM-manipulations/flyingRocks/hanoch/index.ts
@@ -1,62 +1,74 @@
 function moveMeteor(){
-   const random = Math.round(Math.random()*90) 
-    return random
-}
-const meteorImg:NodeListOf<any> = document.querySelectorAll('.meteorImg')
-
-function move(){
-meteorImg.forEach(img=>{
-    img.style.left = moveMeteor() + 'vw'
-    img.style.top = moveMeteor()+ 'vh'
-    // const elmPosition1 = {
-    //     leftPosition:img.getBoundingClientRect().x,
-    //     rightPosition:img.getBoundingClientRect().right,
-    //     topPosition:img.getBoundingClientRect().y,
-    //     bottomPosition:img.getBoundingClientRect().bottom
-    // }
-    // const elmPosition2 = {
-    //     leftPosition:img.getBoundingClientRect().x,
-    //     rightPosition:img.getBoundingClientRect().right,
-    //     topPosition:img.getBoundingClientRect().y,
-    //     bottomPosition:img.getBoundingClientRect().bottom
-    // }
-  
-    // if(elmPosition1.rightPosition> elmPosition2.leftPosition && elmPosition1.topPosition < elmPosition2.bottomPosition && elmPosition1.leftPosition <elmPosition2.rightPosition){
-    //     console.log('boom')
-    // }
-
-})
-for ( let i = 0; i < meteorImg.length ; i++){
-    const elmPosition0 = {
-        leftPosition:meteorImg[0].getBoundingClientRect().x,
-        rightPosition:meteorImg[0].getBoundingClientRect().right,
-        topPosition:meteorImg[0].getBoundingClientRect().y,
-        bottomPosition:meteorImg[0].getBoundingClientRect().bottom
-    }
-    const elmPosition1 = {
-        leftPosition:meteorImg[1].getBoundingClientRect().x,
-        rightPosition:meteorImg[1].getBoundingClientRect().right,
-        topPosition:meteorImg[1].getBoundingClientRect().y,
-        bottomPosition:meteorImg[1].getBoundingClientRect().bottom
-    }
-    const elmPosition2 = {
-        leftPosition:meteorImg[2].getBoundingClientRect().x,
-        rightPosition:meteorImg[2].getBoundingClientRect().right,
-        topPosition:meteorImg[2].getBoundingClientRect().y,
-        bottomPosition:meteorImg[2].getBoundingClientRect().bottom
-    }
-    const elmPosition3 = {
-        leftPosition:meteorImg[3].getBoundingClientRect().x,
-        rightPosition:meteorImg[3].getBoundingClientRect().right,
-        topPosition:meteorImg[3].getBoundingClientRect().y,
-        bottomPosition:meteorImg[3].getBoundingClientRect().bottom
-    }
-    if (elmPosition0.rightPosition[i]> elmPosition1.leftPosition[i] 
-         && elmPosition0.leftPosition[i]< elmPosition1.leftPosition[i]){
-            console.log('boom')
-        }
-}
-
-}
-setInterval(move, 5000)
-move()
+    const random = Math.round(Math.random()*90) 
+     return random
+ }
+ const meteorImg:NodeListOf<any> = document.querySelectorAll('.meteorImg')
+ 
+ function move(){
+     meteorImg.forEach(img=>{
+         img.style.left = moveMeteor() + 'vw'
+         img.style.top = moveMeteor()+ 'vh'
+     })
+     const meteorImg0 = meteorImg[0].getBoundingClientRect()
+     const meteorImg1 = meteorImg[1].getBoundingClientRect()
+     const meteorImg2 = meteorImg[2].getBoundingClientRect()
+     const meteorImg3 = meteorImg[3].getBoundingClientRect()
+     const meteors = [
+         {
+             leftPosition:meteorImg0.x,
+             rightPosition:meteorImg0.right,
+             topPosition:meteorImg0.y,
+             bottomPosition:meteorImg0.bottom
+         },
+         {
+             leftPosition:meteorImg1.x,
+             rightPosition:meteorImg1.right,
+             topPosition:meteorImg1.y,
+             bottomPosition:meteorImg1.bottom
+         },
+         {
+             leftPosition:meteorImg2.x,
+             rightPosition:meteorImg2.right,
+             topPosition:meteorImg2.y,
+             bottomPosition:meteorImg2.bottom
+         },
+         {
+             leftPosition:meteorImg3.x,
+             rightPosition:meteorImg3.right,
+             topPosition:meteorImg3.y,
+             bottomPosition:meteorImg3.bottom
+         }
+     ]
+     for ( let i = 0; i < meteorImg.length ; i++){
+         for ( let j = 0; i < meteorImg.length; i++ ){
+             if (i != j && meteors[i].rightPosition  >= meteors[j].leftPosition 
+                 && meteors[i].leftPosition <= meteors[j].leftPosition &&
+                  meteors[i].topPosition <= meteors[j].bottomPosition &&
+                    meteors[i].bottomPosition >= meteors[j].bottomPosition) {
+                     console.log('boom')
+                 }
+                 else if( i != j && meteors[i].leftPosition <= meteors[j].rightPosition &&
+                        meteors[i].rightPosition >= meteors[j].rightPosition &&
+                        meteors[i].topPosition <= meteors[j].bottomPosition &&
+                        meteors[i].bottomPosition >= meteors[j].bottomPosition){
+                            console.log('boom')
+                        }
+                else if(i != j && meteors[i].rightPosition >= meteors[j].leftPosition &&
+                        meteors[i].leftPosition <= meteors[j].leftPosition &&
+                         meteors[i].bottomPosition >= meteors[j].topPosition &&
+                         meteors[i].topPosition <= meteors[j].topPosition){
+                            console.log('boom')
+                         } 
+                else if( i != j && meteors[i].leftPosition <= meteors[j].rightPosition &&
+                            meteors[i].rightPosition >= meteors[j].rightPosition &&
+                            meteors[i].bottomPosition >= meteors[j].topPosition &&
+                            meteors[i].topPosition <= meteors[j].topPosition){
+                               console.log('boom')
+                            } 
+         }
+ }
+ 
+ }
+ setInterval(move, 5000)
+ move()
+ 
