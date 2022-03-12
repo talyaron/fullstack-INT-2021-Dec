5da71e0d1518486fdbc4c383baa9092e63998019
Evgeny <zakman336@gmail.com>
2022-03-12 20:05:09 +0200

    hw

--- Javascript/11-DOM-manipulations/iDs/evg+ meir+ inbar+ juda/dist/java.js
+++ Javascript/11-DOM-manipulations/iDs/evg+ meir+ inbar+ juda/dist/java.js
@@ -1,3 +1,14 @@
-var meteaor = document.querySelector("#boom-boi");
-console.dir(meteaor);
-meteaor.b;
+var meteaor = document.querySelectorAll(".boom-boi");
+//  window.addEventListener("keydown", move)
+// console.dir(meteaor)
+function createNewPosition() {
+    meteaor.forEach(function (a) {
+        a.style.top = getRandomNumber() + "vh";
+        a.style.left = getRandomNumber() + "vw";
+    });
+}
+function getRandomNumber() {
+    var random = Math.floor(Math.random() * 90);
+    return random;
+}
+setInterval(createNewPosition, 1000);
--- Javascript/11-DOM-manipulations/iDs/evg+ meir+ inbar+ juda/java.ts
+++ Javascript/11-DOM-manipulations/iDs/evg+ meir+ inbar+ juda/java.ts
@@ -1,5 +1,19 @@
-const meteaor:any = document.querySelector("#boom-boi")
+const meteaor:any = document.querySelectorAll(".boom-boi")
+//  window.addEventListener("keydown", move)
+// console.dir(meteaor)
 
-
-console.dir(meteaor)
-meteaor.b
+function createNewPosition(){
+    meteaor.forEach(a => {
+        
+        a.style.top = `${getRandomNumber()}vh`
+        a.style.left = `${getRandomNumber()}vw`
+    
+    });
+}
+function getRandomNumber (){
+    const random = Math.floor(Math.random() * 90);
+    return random
+}
+setInterval(createNewPosition,1000)
+  
+  
