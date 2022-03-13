c48b94466b7de8e591f7aefd89032c9d4b6e35df
YakirD <38473489+yakirdv@users.noreply.github.com>
2022-03-13 20:28:46 +0200

    noy yakir

--- Javascript/12-events/01-DOM events/InvestigateOnlinTask/yakir+noy/dist/index.js
+++ Javascript/12-events/01-DOM events/InvestigateOnlinTask/yakir+noy/dist/index.js
@@ -6,10 +6,12 @@ box.forEach(function (element) {
 function handleEvent(ev) {
     console.dir(ev.target);
     // ev.target.style.backgroundColor = "#000000"
-    ev.target.style.visibility = "hidden";
+    // ev.target.style.visibility = "hidden"
+    ev.target.style.opacity = "0";
 }
 function handleEventLeave(ev) {
     console.dir(ev.target);
     // ev.target.style.backgroundColor = "#f1f1f1"
-    ev.target.style.visibility = "visible";
+    // ev.target.style.visibility = "visible"
+    ev.target.style.opacity = "1";
 }
--- Javascript/12-events/01-DOM events/InvestigateOnlinTask/yakir+noy/index.ts
+++ Javascript/12-events/01-DOM events/InvestigateOnlinTask/yakir+noy/index.ts
@@ -10,11 +10,13 @@ box.forEach(element => {
 function handleEvent(ev) {
     console.dir(ev.target);
     // ev.target.style.backgroundColor = "#000000"
-    ev.target.style.visibility = "hidden"
+    // ev.target.style.visibility = "hidden"
+    ev.target.style.opacity = "0"
 }
 
 function handleEventLeave(ev) {
     console.dir(ev.target);
     // ev.target.style.backgroundColor = "#f1f1f1"
-    ev.target.style.visibility = "visible"
+    // ev.target.style.visibility = "visible"
+    ev.target.style.opacity = "1"
 }
