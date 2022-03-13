15cb45fb6c8964e3b97c5507df3397222dc3dc12
Bella-UI <bellabrailovsky@gmail.com>
2022-03-13 11:39:25 +0200

    ...

--- Javascript/11-DOM-manipulations/flyingRocks/Bella/dist/index.js
+++ Javascript/11-DOM-manipulations/flyingRocks/Bella/dist/index.js
@@ -1,17 +1,39 @@
-function moveObj() {
-    var $span = $("#random");
-    $span.animate([
-        // keyframes
-        { transform: 'translateY(100vh)' },
-        { transform: 'translateX(100vw)' }
-    ], function () {
-        var maxLeft = $(window).width() - $span.width();
-        var maxTop = $(window).height() - $span.height();
-        var leftPos = Math.floor(Math.random() * (maxLeft + 10));
-        var topPos = Math.floor(Math.random() * (maxTop + 10));
-        $span.css({ left: leftPos, top: topPos });
+// function moveObj() {
+//     const $span = $("#random");
+//     $span.animate([
+//         // keyframes
+//         { transform: 'translateY(100vh)' },
+//         { transform: 'translateX(100vw)' }
+//       ], function() {
+//         let maxLeft = $(window).width() - $span.width();
+//         let maxTop = $(window).height() - $span.height();
+//         let leftPos = Math.floor(Math.random() * (maxLeft + 10))
+//         let topPos = Math.floor(Math.random() * (maxTop + 10))
+//         $span.css({ left: leftPos, top: topPos });
+//     });
+// };
+// moveObj();
+// setInterval(moveObj,100);
+var spaceRocks = document.querySelectorAll('.spaceRock');
+spaceRocks.forEach(function (element) {
+    element.style.left = getRandomPosition();
+    element.style.top = getRandomPosition();
+    setInterval(spaceRocks, 3000);
+});
+var counter = 360;
+function getRandomMove() {
+    spaceRocks.forEach(function (element) {
+        element.style.left = getRandomPosition();
+        element.style.top = getRandomPosition();
+        // element.style.transform = `rotate(${counter}deg)`;
+        // counter += 360;
     });
 }
-;
-moveObj();
-setInterval(moveObj, 100);
+function getRandomNumber() {
+    var boxsize = (((Math.random() * 100) + 60).toFixed()) + 'px';
+    return boxsize;
+}
+function getRandomPosition() {
+    var positionRand = (((Math.random() * 800) + 60).toFixed()) + 'px';
+    return positionRand;
+}
--- Javascript/11-DOM-manipulations/flyingRocks/Bella/index.ts
+++ Javascript/11-DOM-manipulations/flyingRocks/Bella/index.ts
@@ -1,24 +1,62 @@
 
 
-function moveObj() {
-    const $span = $("#random");
+// function moveObj() {
+//     const $span = $("#random");
     
 
-    $span.animate([
-        // keyframes
-        { transform: 'translateY(100vh)' },
-        { transform: 'translateX(100vw)' }
-      ], function() {
-        let maxLeft = $(window).width() - $span.width();
-        let maxTop = $(window).height() - $span.height();
-        let leftPos = Math.floor(Math.random() * (maxLeft + 10))
-        let topPos = Math.floor(Math.random() * (maxTop + 10))
+//     $span.animate([
+//         // keyframes
+//         { transform: 'translateY(100vh)' },
+//         { transform: 'translateX(100vw)' }
+//       ], function() {
+//         let maxLeft = $(window).width() - $span.width();
+//         let maxTop = $(window).height() - $span.height();
+//         let leftPos = Math.floor(Math.random() * (maxLeft + 10))
+//         let topPos = Math.floor(Math.random() * (maxTop + 10))
 
-        $span.css({ left: leftPos, top: topPos });
-    });
-};
+//         $span.css({ left: leftPos, top: topPos });
+//     });
+// };
 
-moveObj();
-setInterval(moveObj,100);
+// moveObj();
+// setInterval(moveObj,100);
 
 
+
+const spaceRocks: any = document.querySelectorAll('.spaceRock');
+
+spaceRocks.forEach(element => {
+    element.style.left = getRandomPosition()
+    element.style.top = getRandomPosition()
+    setInterval(spaceRocks,3000)
+})
+
+let counter = 360;
+
+function getRandomMove() {
+
+  spaceRocks.forEach(element => {
+
+        element.style.left = getRandomPosition()
+        element.style.top = getRandomPosition()
+        // element.style.transform = `rotate(${counter}deg)`;
+        // counter += 360;
+        
+
+    })
+    
+}
+
+
+
+function getRandomNumber() {
+
+    let boxsize = (((Math.random() * 100) + 60).toFixed()) + 'px';
+
+    return boxsize;
+}
+
+function getRandomPosition() {
+    let positionRand = (((Math.random() * 800) + 60).toFixed()) + 'px';
+    return positionRand;
+}
