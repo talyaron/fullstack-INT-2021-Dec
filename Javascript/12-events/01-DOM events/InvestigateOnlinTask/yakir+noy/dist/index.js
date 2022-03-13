ba606dfcbfb3ba994f7f39a02ffb08b485d81914
YakirD <38473489+yakirdv@users.noreply.github.com>
2022-03-13 20:25:27 +0200

    onevent mouseenter/leave

--- Javascript/12-events/01-DOM events/InvestigateOnlinTask/yakir+noy/dist/index.js
+++ Javascript/12-events/01-DOM events/InvestigateOnlinTask/yakir+noy/dist/index.js
@@ -1,11 +1,15 @@
-var box = document.querySelector("#box");
-box.onmousedown = handleEvent;
-box.onmouseup = handleEventLeave;
+var box = document.querySelectorAll("#box");
+box.forEach(function (element) {
+    element.onmouseenter = handleEvent;
+    element.onmouseleave = handleEventLeave;
+});
 function handleEvent(ev) {
     console.dir(ev.target);
-    ev.target.style.backgroundColor = "#000000";
+    // ev.target.style.backgroundColor = "#000000"
+    ev.target.style.visibility = "hidden";
 }
 function handleEventLeave(ev) {
     console.dir(ev.target);
-    ev.target.style.backgroundColor = "#f1f1f1";
+    // ev.target.style.backgroundColor = "#f1f1f1"
+    ev.target.style.visibility = "visible";
 }
--- Javascript/12-events/01-DOM events/InvestigateOnlinTask/yakir+noy/index.ts
+++ Javascript/12-events/01-DOM events/InvestigateOnlinTask/yakir+noy/index.ts
@@ -1,15 +1,20 @@
-const box: HTMLDivElement = document.querySelector("#box");
+const box:Array<any>  = document.querySelectorAll("#box");
 
-box.onmousedown = handleEvent
-box.onmouseup = handleEventLeave
+box.forEach(element => {
+    element.onmouseenter  = handleEvent
+    element.onmouseleave = handleEventLeave
+});
+
 
 
 function handleEvent(ev) {
     console.dir(ev.target);
-    ev.target.style.backgroundColor = "#000000"
+    // ev.target.style.backgroundColor = "#000000"
+    ev.target.style.visibility = "hidden"
 }
 
 function handleEventLeave(ev) {
     console.dir(ev.target);
-    ev.target.style.backgroundColor = "#f1f1f1"
+    // ev.target.style.backgroundColor = "#f1f1f1"
+    ev.target.style.visibility = "visible"
 }
