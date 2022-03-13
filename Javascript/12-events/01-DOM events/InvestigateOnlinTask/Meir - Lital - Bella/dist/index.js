026ea06973eca73378b82ccaca4e0515302b2c8f
Bella-UI <bellabrailovsky@gmail.com>
2022-03-13 19:52:03 +0200

    ..

--- Javascript/12-events/01-DOM events/InvestigateOnlinTask/Meir - Lital - Bella/dist/index.js
+++ Javascript/12-events/01-DOM events/InvestigateOnlinTask/Meir - Lital - Bella/dist/index.js
@@ -1,5 +1,6 @@
 var aBox = document.querySelector("#aBox");
 // document.onload = handleClick;
+var body = document.querySelector("body");
 document.onscroll = handleClick;
 var counter = 0;
 function handleClick(ev) {
@@ -7,5 +8,15 @@ function handleClick(ev) {
     counter++;
     ev.stopPropagation();
     var element = ev.target;
+    element.style.backgroundColor = getRandomColor();
+    // body.innerHTML += `${ev.x}, ${ev.y}`;
 }
 console.dir(aBox);
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
