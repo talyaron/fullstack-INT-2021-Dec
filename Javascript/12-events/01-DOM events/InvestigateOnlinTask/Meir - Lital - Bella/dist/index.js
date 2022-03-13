7989d6926b46e1b67a7f9b5b0c0721d2dad0fd48
Bella-UI <bellabrailovsky@gmail.com>
2022-03-13 19:32:16 +0200

    ...

--- Javascript/12-events/01-DOM events/InvestigateOnlinTask/Meir - Lital - Bella/dist/index.js
+++ Javascript/12-events/01-DOM events/InvestigateOnlinTask/Meir - Lital - Bella/dist/index.js
@@ -1,21 +1,11 @@
 var aBox = document.querySelector("#aBox");
 // document.onload = handleClick;
 document.onscroll = handleClick;
+var counter = 0;
 function handleClick(ev) {
-    if () {
-    }
-    console.log(ev);
+    console.log(counter);
+    counter++;
     ev.stopPropagation();
     var element = ev.target;
-    element.style.backgroundColor = getRandomColor();
-    element.innerHTML += ev.x + ", " + ev.y;
 }
 console.dir(aBox);
-function getRandomColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
--- Javascript/12-events/01-DOM events/InvestigateOnlinTask/Meir - Lital - Bella/index.ts
+++ Javascript/12-events/01-DOM events/InvestigateOnlinTask/Meir - Lital - Bella/index.ts
@@ -2,26 +2,13 @@ const aBox: HTMLDivElement = document.querySelector("#aBox");
 // document.onload = handleClick;
 
 document.onscroll = handleClick;
+let counter = 0;
 
 function handleClick(ev) {
-  if (){
-    
-  }
-  console.log(ev);
+  console.log(counter);
+  counter++
   ev.stopPropagation();
   const element = ev.target;
-
-  element.style.backgroundColor = getRandomColor();
-  element.innerHTML += `${ev.x}, ${ev.y}`;
 }
 
 console.dir(aBox);
-
-function getRandomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
