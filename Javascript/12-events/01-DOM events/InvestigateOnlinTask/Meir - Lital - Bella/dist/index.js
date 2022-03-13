43ea3dddb05efb2c9abb41417e61617f01e7d3f0
Bella-UI <bellabrailovsky@gmail.com>
2022-03-13 20:30:20 +0200

    ...

--- Javascript/12-events/01-DOM events/InvestigateOnlinTask/Meir - Lital - Bella/dist/index.js
+++ Javascript/12-events/01-DOM events/InvestigateOnlinTask/Meir - Lital - Bella/dist/index.js
@@ -1,22 +1,32 @@
-var aBox = document.querySelector("#aBox");
-// document.onload = handleClick;
-var body = document.querySelector("body");
-document.onscroll = handleClick;
-var counter = 0;
-function handleClick(ev) {
-    console.log(counter);
-    counter++;
-    ev.stopPropagation();
-    var element = ev.target;
-    element.style.backgroundColor = getRandomColor();
-    // body.innerHTML += `${ev.x}, ${ev.y}`;
-}
-console.dir(aBox);
-function getRandomColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+// const aBox: HTMLDivElement = document.querySelector("#aBox");
+// // document.onload = handleClick;
+// const body: HTMLBodyElement = document.querySelector("body");
+// document.onscroll = handleClick;
+// let counter = 0;
+// function handleClick(ev) {
+//   console.log(counter);
+//   counter++
+//   ev.stopPropagation();
+//   const element = ev.target;
+//  element.style.backgroundColor = getRandomColor();
+//   // body.innerHTML += `${ev.x}, ${ev.y}`;
+// }
+// console.dir(aBox);
+// function getRandomColor() {
+//   var letters = "0123456789ABCDEF";
+//   var color = "#";
+//   for (var i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return color;
+// }
+var container = document.querySelector("#container");
+container.onmouseover = container.onmouseout = handler;
+function handler(event) {
+    if (event.type == 'mouseover') {
+        event.target.style.background = 'pink';
     }
-    return color;
+    if (event.type == 'mouseout') {
+        event.target.style.background = '';
+    }
 }
