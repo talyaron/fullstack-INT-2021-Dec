d5a0955bf4659fde28ea27e8af881cbaaa9680ae
MartinoySolomon <noy.kakoun@gmail.com>
2022-03-13 19:33:07 +0200

    classwork

---
+++ Javascript/12-events/01-DOM events/InvestigateOnlinTask/yakir+noy/index.ts
@@ -0,0 +1,15 @@
+const box: HTMLDivElement = document.querySelector("#box");
+
+box.onmousedown = handleEvent
+box.onmouseup = handleEventLeave
+
+
+function handleEvent(ev) {
+    console.dir(ev.target);
+    ev.target.style.backgroundColor = "#000000"
+}
+
+function handleEventLeave(ev) {
+    console.dir(ev.target);
+    ev.target.style.backgroundColor = "#f1f1f1"
+}
