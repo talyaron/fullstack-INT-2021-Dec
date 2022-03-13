0fae05780b495e1a469cc08bd4dece3a81ecac5d
adamepel <adamepel11@gmail.com>
2022-03-13 20:25:13 +0200

    1

--- Javascript/12-events/01-DOM events/InvestigateOnlinTask/Adam&Hanoch/dist/index.js
+++ Javascript/12-events/01-DOM events/InvestigateOnlinTask/Adam&Hanoch/dist/index.js
@@ -1,19 +1,4 @@
-var aBox = document.querySelector("#aBox");
-document.onload = handleClick;
-aBox.onclick = handleClick;
-function handleClick(ev) {
-    console.log(ev);
-    ev.stopPropagation();
-    var element = ev.target;
-    element.style.backgroundColor = getRandomColor();
-    element.innerHTML += "<div>(" + ev.x + ", " + ev.y + ")</div>";
-}
-console.dir(aBox);
-function getRandomColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+var boxes = document.querySelectorAll(".box");
+boxes.forEach(function (element) {
+    boxes.mouseover = handleClick;
+});
--- Javascript/12-events/01-DOM events/InvestigateOnlinTask/Adam&Hanoch/index.ts
+++ Javascript/12-events/01-DOM events/InvestigateOnlinTask/Adam&Hanoch/index.ts
@@ -1,24 +1,5 @@
-const aBox:any = document.querySelector("#aBox");
-document.onload = handleClick;
-
-aBox.onclick = handleClick;
-
-function handleClick(ev) {
-  console.log(ev);
-  ev.stopPropagation();
-  const element = ev.target;
-
-  element.style.backgroundColor = getRandomColor();
-  element.innerHTML += `<div>(${ev.x}, ${ev.y})</div>`;
-}
-
-console.dir(aBox);
-
-function getRandomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+const boxes:any=document.querySelectorAll(".box");
+boxes.forEach(element => {
+  boxes.mouseover=handleClick
+  
+});
