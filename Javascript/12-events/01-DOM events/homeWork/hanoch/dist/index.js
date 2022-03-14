df50feab638e263645dedde671cc4bb91268e865
hanochshachar <goralot550@gmail.com>
2022-03-15 01:41:50 +0200

    hh

---
+++ Javascript/12-events/01-DOM events/homeWork/hanoch/dist/index.js
@@ -0,0 +1,28 @@
+var images = document.querySelectorAll(".meteorImg");
+console.log(images);
+function randomMovement() {
+    var random = Math.round(Math.random() * 90);
+    return random;
+}
+function moveImages() {
+    images.forEach(function (element) {
+        element.style.left = randomMovement() + "vw";
+        element.style.top = randomMovement() + "vh";
+        console.log(element.style.left);
+        console.log(element.style.top);
+    });
+}
+window.requestAnimationFrame(moveImages);
+setInterval(moveImages, 4500);
+console.log(randomMovement());
+moveImages();
+images.forEach(function (elm) {
+    elm.onclick = disappear;
+    elm.onmouseup = undisappear;
+});
+function disappear(click) {
+    click.target.style.visibility = "hidden";
+}
+function undisappear(click) {
+    click.target.style.visibility = "visible";
+}
