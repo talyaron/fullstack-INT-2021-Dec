9848a71985da5987484367eb8ca5bd04457d3ac5
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-03-15 19:32:20 +0200

    1

---
+++ תרגול/2- JavaScript/15.03 - asteroidim/Functions/index.js
@@ -0,0 +1,15 @@
+/*
+Read 3 variables from the user: a, b, c. These will be the a, b & c coefficients of a quadratic equation.
+(prompt)
+Print the values of the following calculations to the console:
+ 1. -b
+ 2. 2*a
+ 3. b*b -4*a*c
+
+ console.log()
+*/
+
+function fun1() {
+
+}
+
