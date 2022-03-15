073ebcb716890c3a74d769105f361fa954637ed0
hadast054 <97188314+hadast054@users.noreply.github.com>
2022-03-15 23:13:52 +0200

    hadas

---
+++ Javascript/11-DOM-manipulations/flyingRocks/hadas/index.ts
@@ -0,0 +1,62 @@
+const images: NodeListOf<any> = document.querySelectorAll(".meteorImg")
+
+console.log(images)
+
+
+function randomMovement() {
+    const random = Math.round(Math.random() * 90)
+    return random
+}
+
+    
+function checkcollition()
+{
+
+    for (let i = 0; i < images.length; i++)
+     {
+        for (let j = 0; j < images.length; j++)
+         {
+            if(i===j){
+                continue;
+            }
+            if(images[i].x <images[j].x+ images[j].width &&
+                         images[i].x + images[i].width > images[j].x &&
+                         images[i].y <images[j].y+ images[j].height &&
+                         images[i].y + images[i].height > images[j].y 
+                        )
+                    {
+                        console.log("boom");
+                       
+                    }
+            
+        }
+        
+    }
+
+    
+}
+function moveImages() {
+    images.forEach(element => {
+        element.style.left = randomMovement() + "vw"
+        element.style.top = randomMovement() + "vh"
+    });
+
+    checkcollition();
+}
+
+
+window.requestAnimationFrame(moveImages);
+
+setInterval(moveImages, 4500)
+
+// //חלק 2//
+
+// function handleClick (img)
+// {
+//     img.target.style.display = 'none';
+// }
+
+// for (let i = 0; i < images.length; i++) {
+//    images[i].onclick = handleClick;
+    
+// }
