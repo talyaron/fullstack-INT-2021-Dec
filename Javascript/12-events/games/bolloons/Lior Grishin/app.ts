b7fcb7ece0d3d6e63750d2d75753918e9095640a
Lior Grishin <94124882+lorGR@users.noreply.github.com>
2022-03-20 19:26:45 +0200

    PoP Ballon Game - Changes
    Added Comments to each function
    
    NEW Convenstion:
    - If function returns something
    start at ****get****
    - If function just sets something start at ****set****

--- Javascript/12-events/games/bolloons/Lior Grishin/app.ts
+++ Javascript/12-events/games/bolloons/Lior Grishin/app.ts
@@ -4,24 +4,32 @@ const ballons:NodeListOf<HTMLImageElement> = document.querySelectorAll('.ballon'
 
 body.style.backgroundImage = 'url(./images/Background.jpg)'
 
-function moveBallonDown(ballons:NodeListOf<HTMLImageElement>){
+// Function :
+// Setting Ballon position off-screen (bot) //
+function setBallonDown(ballons:NodeListOf<HTMLImageElement>){
     ballons.forEach(element => {
         element.style.top = '100vh';
     });
 }
 
-function moveBallonUp(ballons:NodeListOf<HTMLImageElement>){
+// Function :
+// Setting ballon off-screen (top)//
+function setBallonUp(ballons:NodeListOf<HTMLImageElement>){
     ballons.forEach(element => {
         element.style.top= '-20vh';
     });
 }
 
+// Function:
+// Setting ballon in random position on x axies
 function setBallonRandomPosition(ballons:NodeListOf<HTMLImageElement>){
     ballons.forEach(element => {
-        element.style.left = `${randomWidthStart()}px`;
+        element.style.left = `${getRandomWidthStart()}px`;
     });
 }
 
-function randomWidthStart():number{
+// Function:
+// returns number between 0 - body's width
+function getRandomWidthStart():number{
     return Math.random() * body.offsetWidth;
 }
