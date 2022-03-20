45f244bdaa70a4cc544f69e3f409b36f233895be
Bella-UI <bellabrailovsky@gmail.com>
2022-03-20 19:52:35 +0200

    ...

--- Javascript/12-events/games/bolloons/Bella/dist/index.js
+++ Javascript/12-events/games/bolloons/Bella/dist/index.js
@@ -1,9 +1,15 @@
-var getRandom = function (min, max) { return Math.floor(Math.random() * (max - min + 1) + min); };
+// const getRandom = (min:number, max:number) => Math.floor(Math.random()*(max-min+1)+min);
 var balloons = document.querySelectorAll('.baloonns');
+console.dir(balloons);
 function moveBalloons() {
     balloons.forEach(function (element) {
-        element.style.left = getRandom + 'px'; // 👈🏼 Horizontally
-        element.style.top = getRandom + 'px'; // 👈🏼 Vertically 
+        element.style.top = "-12vh";
+        element.style.left = getRandom() + "vw";
     });
-    setInterval(moveBalloons, 1000);
 }
+function getRandom() {
+    var random = Math.floor(Math.random() * 100);
+    return random;
+}
+// setInterval(moveBalloons, 1000);
+moveBalloons();
