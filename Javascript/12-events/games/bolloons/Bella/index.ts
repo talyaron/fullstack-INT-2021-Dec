31ce6da216dba0eb6fb3ee73c98dce40e16180bb
Bella-UI <bellabrailovsky@gmail.com>
2022-03-20 20:18:08 +0200

    ...

--- Javascript/12-events/games/bolloons/Bella/index.ts
+++ Javascript/12-events/games/bolloons/Bella/index.ts
@@ -1,8 +1,18 @@
 
 // const getRandom = (min:number, max:number) => Math.floor(Math.random()*(max-min+1)+min);
 
-const balloons: any = document.querySelectorAll('.baloonns');
-console.dir(balloons)
+// const balloons:any = document.querySelectorAll('.baloonns');
+
+const container = document.querySelector ('.container');
+
+for (i=1; i<=4; i++) {
+
+const balloons = document.createElement ('img');
+balloons.src = "./balloons.png"
+balloons.width = 200;
+
+container.appendChild (balloons);
+}  
 
 function moveBalloons() {
     balloons.forEach(element => {
@@ -11,11 +21,10 @@ function moveBalloons() {
     });
 }
 
-
 function getRandom() {
     let random = Math.floor(Math.random() * 100);
     return random;
 }
 
-// setInterval(moveBalloons, 1000);
 moveBalloons()
+// setInterval(moveBalloons, 1000);
