cabc642817a9e5bc74128736724b4e46fa746b56
YakirD <38473489+yakirdv@users.noreply.github.com>
2022-03-20 19:08:37 +0200

    1

--- Javascript/12-events/games/bolloons/yakir/dist/index.js
+++ Javascript/12-events/games/bolloons/yakir/dist/index.js
@@ -1,9 +1,12 @@
+var baloons = document.querySelectorAll(".baloon");
 function randomPosition() {
     var postion;
     postion = Math.random() * 90;
     return postion;
 }
-var baloons = document.querySelectorAll("baloon");
+baloonRandom();
+window.requestAnimationFrame(baloonRandom);
+var intervalID = setInterval(baloonRandom, 5000);
 function baloonRandom() {
     baloons.forEach(function (element) {
         //random position
@@ -11,6 +14,3 @@ function baloonRandom() {
         element.style.left = randomPosition() + "vw";
     });
 }
-baloonRandom();
-window.requestAnimationFrame(baloonRandom);
-var intervalID = setInterval(baloonRandom, 5000);
