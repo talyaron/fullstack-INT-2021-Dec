7ba3875b0ec531fa6d7704b069d82ecc2971d331
hanan <jigaart0@gmail.com>
2022-03-21 07:53:43 +0200

    גדשג

--- Javascript/12-events/games/bolloons/hanan/index.ts
+++ Javascript/12-events/games/bolloons/hanan/index.ts
@@ -3,13 +3,9 @@ const ballons: NodeListOf<HTMLElement> = document.querySelectorAll(".ballons");
 function ballonFly(ballons) {
   ballons.forEach(element => {
 
-
-
-
-
     element.style.top = `-12vh`;
     element.style.left = randomPosition() + "vw";
-     
+
 
   });
   function randomPosition() {
