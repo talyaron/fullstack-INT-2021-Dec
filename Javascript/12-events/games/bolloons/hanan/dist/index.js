81d21ea9ca8028a3f80d87d2245b50f091c68317
hanan <jigaart0@gmail.com>
2022-03-21 08:55:29 +0200

    dsa

--- Javascript/12-events/games/bolloons/hanan/dist/index.js
+++ Javascript/12-events/games/bolloons/hanan/dist/index.js
@@ -1,4 +1,5 @@
 var ballons = document.querySelectorAll(".ballons");
+var boss = document.querySelectorAll(".boss");
 function ballonFly(ballons) {
     ballons.forEach(function (element) {
         element.style.top = "-12vh";
@@ -9,5 +10,17 @@ function ballonFly(ballons) {
         postion = Math.random() * 90;
         return postion;
     }
+    function bossballonFly(boss) {
+        ballons.forEach(function (element) {
+            element.style.top = "-12vh";
+            element.style.left = randomPosition() + "vw";
+        });
+        function randomPosition() {
+            var postion;
+            postion = Math.random() * 90;
+            return postion;
+        }
+    }
 }
 ballonFly(ballons);
+ballonFly(boss);
--- Javascript/12-events/games/bolloons/hanan/index.ts
+++ Javascript/12-events/games/bolloons/hanan/index.ts
@@ -1,5 +1,5 @@
 const ballons: NodeListOf<HTMLElement> = document.querySelectorAll(".ballons");
-
+const boss: NodeListOf<HTMLElement> = document.querySelectorAll(".boss");
 function ballonFly(ballons) {
   ballons.forEach(element => {
 
@@ -13,10 +13,23 @@ function ballonFly(ballons) {
     postion = Math.random() * 90;
     return postion;
   }
+  function bossballonFly(boss) {
+    ballons.forEach(element => {
+  
+      element.style.top = `-12vh`;
+      element.style.left = randomPosition() + "vw";
+  
+  
+    });
+    function randomPosition() {
+      let postion: number;
+      postion = Math.random() * 90;
+      return postion;
+    }
+  }
+
+
 
 }
-
 ballonFly(ballons);
-
-
-
+ballonFly(boss);
