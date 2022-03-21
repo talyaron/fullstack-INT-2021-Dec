408f48a07dc1c71e695c4db2541f211cd8014a71
hanochshachar <goralot550@gmail.com>
2022-03-22 01:14:55 +0200

    hh

--- Javascript/12-events/games/bolloons/hanoch/dist/index.js
+++ Javascript/12-events/games/bolloons/hanoch/dist/index.js
@@ -1,8 +1,14 @@
 var balloons = document.querySelectorAll('.baloons');
 function moveBalloons() {
     balloons.forEach(function (elm) {
-        elm.style.top = "-15vh";
+        elm.style.top = "15vh";
         elm.style.left = getRandomNumber() + "vw";
+    });
+}
+function moveBalloons1() {
+    balloons.forEach(function (elm) {
+        elm.style.top = "90vh";
+        // elm.style.left = `${getRandomNumber()}vw`
     });
 }
 // setInterval(moveBalloons(),500)
@@ -10,4 +16,9 @@ function getRandomNumber() {
     var random = Math.floor(Math.random() * 90);
     return random;
 }
-setInterval(moveBalloons, 6000);
+function moveBalloons2() {
+    moveBalloons();
+    moveBalloons1();
+}
+setInterval(moveBalloons, 5000);
+setInterval(moveBalloons1, 5000);
