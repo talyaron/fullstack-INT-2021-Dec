76a26655024ad3b5dd903a245234c111adf51587
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-03-22 14:33:18 +0200

    pre

--- Javascript/12-events/games/bolloons/Meir/dist/index.js
+++
@@ -1,6 +0,0 @@
-var ballons = document.querySelectorAll('#ballon');
-console.dir(ballons);
-function createBallon() {
-    var ballonId = uid();
-    document.body.innerHTML += "<img id='" + ballonId + "' class=ballon src=\"./dist/ballon.svg.png\"/>";
-}
--- Javascript/12-events/games/bolloons/Meir/index.ts
+++ Javascript/12-events/games/bolloons/Meir/index.ts
@@ -1,10 +1,2 @@
-const ballons:NodeListOf<HTMLImageElement> = document.querySelectorAll('#ballon')
-console.dir(ballons);
 
-function createBallon(){
-    const ballonId = uid();
-    document.body.innerHTML +=`<img id='${ballonId}' class=ballon src="./dist/ballon.svg.png"/>`
-
-    
-}
 
