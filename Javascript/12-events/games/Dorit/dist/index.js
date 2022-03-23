caaa8b089f8380516d68cd06959526bf0cc6a0d0
doritgy <63942750+doritgy@users.noreply.github.com>
2022-03-23 17:32:40 +0200

    מק'

--- Javascript/12-events/games/Dorit/dist/index.js
+++ Javascript/12-events/games/Dorit/dist/index.js
@@ -1,28 +1,44 @@
-var balloons = document.querySelectorAll('.balloon');
-console.dir(balloons);
-// moveBalloons(balloons)
-// function moveBalloons(balloons:NodeListOf<HTMLImageElement>){
-//     balloons.forEach(balloon=>{
-//         moveBalloon(balloon);
-//     })
-// }
-//create balloon
-// -> move balloon
-// in random times....
-function createBalloon() {
-    var balloonId = uid();
-    document.body.innerHTML += "<img id='" + balloonId + "' class='balloon' src=\"./dist/img/balloon-blue.png\" />";
+function createBalloon1() {
+    var balloonId = uid1();
+    document.body.innerHTML += "<img id=" + balloonId + " class=\"balloon\" src=\"https://freesvg.org/storage/img/thumb/rg1024-two-ballons.png\"/>";
     var balloon = document.querySelector("#" + balloonId);
+    //moveBalloon1(balloon);
+    //console.log("to move balloon")
+    balloon.onclick = bombPicture;
     moveBalloon(balloon);
 }
-function moveBalloon(balloon) {
-    balloon.style.top = '112vh';
+// const bombPicture = document.body.innerHTML = `<img src ="/bomb/istockphoto-114409917-170667a.jpg"/>`
+function bombPicture(ev) {
+    //console.log("we are here")
+    bombImg = document.querySelector("#bombImg");
+    myAudio.play();
+    bombImg.style.height = "75px";
+    bombImg.style.width = "75px";
+    bombImg.style.position = "absolute";
+    ev.target.style.top = "50vh";
+    bombImg.style.left = ev.target.style.left;
+    ev.target.style.opacity = "0";
+    bombImg.style.opacity = "1";
+    setTimeout(function () {
+        bombImg.style.opacity = "0";
+    }, 200);
+}
+function moveBalloon1(balloon) {
+    balloon.style.top = '90vh';
     balloon.style.left = Math.random() * 90 + "vw";
+    balloon.onclick = bombPicture;
     setTimeout(function () {
         balloon.style.top = '-12vh';
     }, 100);
 }
-setInterval(createBalloon, 1000);
-function uid() {
+function uid1() {
     return "id-" + Math.ceil(Math.random() * 1e8);
 }
+var myAudio = new Audio('./bomb/mixkit-bomb-explosion-in-battle-2800.wav');
+document.body.innerHTML += "<img id =\"bombImg\" src=\"./bomb/istockphoto-114409917-170667a.jpg\"/>";
+var bombImg = document.querySelector("#bombImg");
+bombImg.style.opacity = "0";
+var randomNumber = Math.floor(Math.random() * 2000) + 3000;
+setInterval(function () {
+    createBalloon1();
+}, randomNumber);
