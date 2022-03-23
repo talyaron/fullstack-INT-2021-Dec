6d5209feb9493fc24767f8eb2b9c1f1df04d3c07
MartinoySolomon <noy.kakoun@gmail.com>
2022-03-23 19:47:08 +0200

    ex

---
+++ Javascript/13-input-forms/01-inputs/Exercise-Form/noy/dist/index.js
@@ -0,0 +1,6 @@
+var root = document.querySelector("#root");
+function handleName(event) {
+    root.innerHTML += "<div id=\"#name\"><p>Name: " + event.target.value + "</p></div>";
+}
+function handleEmail(event) {
+}
