9791122f10a4d8090c99167db33ebf5cf99c4a37
hanan <jigaart0@gmail.com>
2022-03-23 20:04:14 +0200

    dsa

---
+++ Javascript/13-input-forms/01-inputs/hanan/index.ts
@@ -0,0 +1,12 @@
+const userName:HTMLInputElement = document.querySelector(`#name`);
+const email:HTMLInputElement = document.querySelector(`#email`);
+const password:HTMLInputElement = document.querySelector(`#password`);
+
+
+
+function inputUserName(event){
+
+
+
+}
+
