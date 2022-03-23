cd091b2b81940cfcf660d1e2a0bf70f8b7a22a74
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-03-23 05:51:16 +0200

    ballonspre

---
+++ Javascript/12-events/games/bolloons/Meir/dist/index.js
@@ -0,0 +1,12 @@
+var ballons = document.querySelectorAll("#ballon");
+function flyBallon() {
+    ballons.forEach(function (element) {
+        element.style.top = '-16vh';
+        element.style.left = getRandomPosition + "vw";
+    });
+}
+function getRandomPosition() {
+    var random = Math.floor(Math.random() * 100);
+    return random;
+}
+flyBallon(ballons);
--- Javascript/12-events/games/bolloons/Meir/index.ts
+++ Javascript/12-events/games/bolloons/Meir/index.ts
@@ -1,2 +1,18 @@
+const ballons:any = document.querySelectorAll("#ballon");
+
+function flyBallon(){
+    ballons.forEach(element => {
+        element.style.top ='-16vh'
+        element.style.left = `${getRandomPosition}vw`
+    })
+    
+}
 
 
+
+function getRandomPosition() {
+    let random = Math.floor(Math.random()*100);
+    return random;
+}
+
+flyBallon(ballons)
