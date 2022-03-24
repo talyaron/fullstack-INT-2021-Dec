bd0aacf3e59c0819b1a70c9758a8fc4a5dabe135
hanan <jigaart0@gmail.com>
2022-03-24 02:53:44 +0200

    new game
    dsa

--- Javascript/12-events/games/bolloons/hanan/dist/index.js
+++ Javascript/12-events/games/bolloons/hanan/dist/index.js
@@ -1,32 +1,98 @@
-var ballons = document.querySelectorAll(".ballons");
-var boss = document.querySelectorAll(".boss");
-//////////תעופה של בלונים ובלון בוס function of movement for the objects////////////
-function ballonFly(ballons, hight) {
-    // newBallon()
-    ballons.forEach(function (element) {
-        element.style.top = hight + "vh";
-        element.style.left = randomPosition() + "vw";
-    });
+var ballons = ["ballon"];
+var windowWidth = window.innerWidth;
+var body = document.querySelector(".body");
+var windowHeight = window.innerHeight;
+var scores = document.querySelectorAll(".score");
+var noPop = 0;
+var total = 45;
+var currentBallon = 0;
+var gameOver = false;
+var shadow = document.querySelector(".shadow");
+var startBtn = document.querySelectorAll(".start-btn");
+function createBallon() {
+    var div = document.createElement("div");
+    var rand = Math.floor(Math.random() * ballons.length);
+    div.className = "ballon ballon-" + ballons[rand];
+    rand = Math.floor(Math.random() * (windowWidth - 150));
+    div.style.left = rand + "px";
+    div.dataset.number = currentBallon;
+    currentBallon++;
+    document.body.appendChild(div);
 }
-function randomPosition() {
-    var postion;
-    postion = Math.random() * 90;
-    return postion;
+;
+animateBallon(div);
+function animateBallon(elem) {
+    var position = 0;
+    var random = Math.floor(Math.random() * 6 - 3);
+    var interval = setInterval(frame, 12 - Math.floor(noPop /
+        10) + random);
 }
-//function newBallon(){
-//document.body.innerHTML += `<div class="ballons"><img src="imgs/PikPng.com_pink-balloon-png-transparent_5494729.png" id="ballon">`
-//}
-/////////////////////reset/////////////////////
-function newBallon() {
-    ballons.forEach(function (element) {
-        element.style.top = 100 + "vh";
-    });
+;
+function frame() {
+    if (position >= (windowHeight + 150) && (document.querySelector("[data-number =\"" + elem.detaset.number + "\"]") !== null)) {
+        clearInterval(Interval);
+    }
+    else {
+        position++;
+        elem.style.top = windowHeight - position + "px";
+    }
 }
-function rungame() {
-    newBallon();
-    ballonFly(ballons, -14);
-    ballonFly(boss, -34);
-    console.log("test");
+;
+function popBallons(elem) {
+    elem.remove();
+    noPop++;
+    scorupdate();
 }
-setInterval(newBallon, 2000);
-setInterval(rungame, 5000);
+function scorupdate() {
+    for (var i = 0; i < scores.length; i++)
+        scores[i].textContent = noPop;
+}
+function startGame() {
+    restartGame();
+    var timeOut = 0;
+    var loop = setInterval(function () {
+        timeOut = Math.floor(Math.random() * 600 - 100);
+        if (!gameOver && noPop !== total) {
+            createBallon();
+        }
+        else if (noPop !== total) {
+            clearInterval(loop);
+            shadow.style.display = "flex";
+            shadow.querySelector(".loser").style.display =
+                "block";
+        }
+        else {
+            clearInterval(loop);
+            shadow.style.display = "flex";
+            shadow.querySelector(".winner").style.display =
+                "block";
+        }
+    }, 800 + timeOut);
+    function restartGame() {
+        var forRemoving = document.querySelectorAll(".ballon");
+        for (var i = 0; i < forRemoving.length; i++) {
+            forRemoving[i].remove();
+        }
+        gameOver = false;
+        noPop = 0;
+        scorupdate();
+    }
+}
+document.addEventListener("click", function (ev) {
+    if (ev.target.classList.contains("ballon")) {
+        popBallons(ev.target);
+    }
+});
+document.querySelector(".restart").addEventListener("click", function () {
+    shadow.style.display = "none";
+    shadow.querySelector(".winner").style.display = "none";
+    shadow.querySelector(".loser").style.display = "none";
+    startGame();
+});
+document.querySelector("cancel").addEventListener(".click", function () {
+    shadow.style.display = "none";
+});
+startBtn.addEventListener("click", function () {
+    startGame();
+    document.querySelector(".main-game").style.display = "none";
+});
--- Javascript/12-events/games/bolloons/hanan/index.ts
+++ Javascript/12-events/games/bolloons/hanan/index.ts
@@ -1,54 +1,118 @@
+const ballons = [`ballon`]
+const windowWidth = window.innerWidth;
+const body = document.querySelector(`.body`);
+const windowHeight = window.innerHeight;
+const scores = document.querySelectorAll(`.score`);
+const noPop = 0;
+const total = 45;
+const currentBallon = 0;
+const gameOver = false;
+const shadow = document.querySelector(".shadow");
+const startBtn = document.querySelectorAll(`.start-btn`);
+
+function createBallon() {
+   let div = document.createElement(`div`);
+   let rand = Math.floor(Math.random() * ballons.length);
+   div.className = `ballon ballon-` + ballons[rand];
+   rand = Math.floor(Math.random() * (windowWidth - 150))
+   div.style.left = rand + `px`;
+
+ div.dataset.number = currentBallon;
+ currentBallon++;
+ 
+ document.body.appendChild(div);
+
+};
 
 
+animateBallon(div);
 
-const ballons: NodeListOf<HTMLElement> = document.querySelectorAll(".ballons");
-const boss: NodeListOf<HTMLElement> = document.querySelectorAll(".boss");
+function animateBallon(elem) {
+   let position = 0;
+   let random = Math.floor(Math.random() * 6 - 3);
+   let interval = setInterval(frame, 12 - Math.floor(noPop /
+      10) + random);
 
-//////////תעופה של בלונים ובלון בוס function of movement for the objects////////////
-function ballonFly(ballons, hight) {
-  // newBallon()
-  ballons.forEach(element => {
+};
 
-    element.style.top = `${hight}vh`;
-    element.style.left = randomPosition() + "vw";
-  });
+function frame() {
+   if (position >= (windowHeight + 150) && (document.querySelector(`[data-number ="` + elem.detaset.number + `"]`) !== null)) {
+      clearInterval(Interval);
+   }
+   else {
+      position++
+      elem.style.top = windowHeight - position + `px`;
+   }
+
+};
+
+function popBallons(elem) {
+   elem.remove();
+   noPop++;
+   scorupdate();
 }
-
-function randomPosition() {
-  let postion: number;
-  postion = Math.random() * 90;
-  return postion;
+function scorupdate() {
+   for (let i = 0; i < scores.length; i++)
+      scores[i].textContent = noPop;
 }
+function startGame() {
+   restartGame();
+   let timeOut = 0;
+   let loop = setInterval(function () {
+      timeOut = Math.floor(Math.random() * 600 - 100);
+      if (!gameOver && noPop !== total) {
+         createBallon();
+      }
+      else if (noPop !== total) {
+         clearInterval(loop);
+         shadow.style.display = `flex`;
+         shadow.querySelector(`.loser`).style.display =
+            `block`
+      }
+      else {
+         clearInterval(loop);
+         shadow.style.display = `flex`;
+         shadow.querySelector(`.winner`).style.display =
+            `block`
+      }
+   }, 800 + timeOut);
+   function restartGame() {
+      let forRemoving = document.querySelectorAll(`.ballon`);
+      for (let i = 0; i < forRemoving.length; i++) {
+         forRemoving[i].remove();
+      }
+      gameOver = false;
+      noPop = 0;
+      scorupdate();
+   }
 
 
-//function newBallon(){
-  //document.body.innerHTML += `<div class="ballons"><img src="imgs/PikPng.com_pink-balloon-png-transparent_5494729.png" id="ballon">`
 
-//}
-
-
-
-
-/////////////////////reset/////////////////////
-
-function newBallon(){
-ballons.forEach(element => {
-  element.style.top = 100 +`vh`
+}
+document.addEventListener(`click`, function (ev) {
+   if (ev.target.classList.contains(`ballon`)) {
+      popBallons(ev.target);
+   }
+  
 });
 
+document.querySelector(`.restart`).addEventListener(`click`, function(){
+   shadow.style.display = `none`;
+   shadow.querySelector(`.winner`).style.display = `none`;
+   shadow.querySelector(`.loser`).style.display = `none`;
+   startGame();
+
+});
+document.querySelector(`cancel`).addEventListener(`.click`,function (){
+shadow.style.display = `none`;
+});
+
+startBtn.addEventListener(`click`, function(){
+
+startGame();
+document.querySelector(`.main-game`).style.display = `none`;
 
 
-}
-
-function rungame(){
-  newBallon()
-  ballonFly(ballons, -14);
-  ballonFly(boss, -34);
-  console.log("test")
-
-}
 
 
-
-setInterval(newBallon,2000);
-setInterval(rungame,5000);
+})
