06019b083bcad225331357670d9670c8abe890fc
hadast054 <97188314+hadast054@users.noreply.github.com>
2022-03-25 00:36:28 +0200

    hadas

--- Javascript/12-events/games/bolloons/hadas/index.ts
+++ Javascript/12-events/games/bolloons/hadas/index.ts
@@ -1,17 +1,43 @@
-const balloons: any = document.querySelectorAll (".balloon");
+const balloons: NodeListOf<HTMLImageElement> = document.querySelectorAll (".balloon");
+console.dir(balloons);
 
-function moveBalloons() {
-    balloons.forEach(element => {
-        element.style.top = `-25vh`;
-        element.style.left = `${getRandom()}vw`;
+// moveBalloons(balloons)
+
+// function moveBalloons(balloons:NodeListOf<HTMLImageElement>){
+//     balloons.forEach(balloon=>{
+//         moveBalloon(balloon);
+//     })
+// }
+
+function createBalloon(){
+    const balloonId = uid()
+    document.body.innerHTML += `<img id = ${balloonId} class ='balloon' src= "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Red_toy_balloon.svg/491px-Red_toy_balloon.svg.png?20090312193428"/>` 
+
+    const balloon:HTMLImageElement = document.querySelector (`#${balloonId}`);
+    moveBalloon(balloon);
+}
+
+
+function moveBalloon(balloon:HTMLImageElement) {
+   
+        balloon.style.top = `125vh`;
+        balloon.style.left = `${Math.random()*90}vw`;
+        setTimeout (()=>{
+            balloon.style.top = `-25vh`;
+             },100)
         
-    });
     
+    };
+       setInterval(createBalloon,6000)
+
+
+function uid(){
+    return `id-${Math.floor(Math.random()*1e8)}`
 }
 
-function getRandom(){
-    let random = Math.floor(Math.random()*90);
-    return random;
+function handleClick (ev){
+    ev as HTMLElement;
+    let audio = new Audio(`1.wav`,);
+    audio.play();
+   
 }
-
-moveBalloons()
