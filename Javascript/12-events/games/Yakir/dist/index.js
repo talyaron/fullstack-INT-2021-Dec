e33abf19ed7498b832f2525950c9c6e8b4b9b053
YakirD <38473489+yakirdv@users.noreply.github.com>
2022-03-27 01:57:04 +0300

    crate

---
+++ Javascript/12-events/games/Yakir/dist/index.js
@@ -0,0 +1,5 @@
+// New Baloon element
+// random fly baloon
+// baloon explosion + count player score
+// game time is 30sec each game
+// Baloons crates in same time
---
+++ Javascript/12-events/games/Yakir/index.ts
@@ -0,0 +1,11 @@
+// New Baloon element
+
+
+// random fly baloon
+
+
+// baloon explosion + count player score
+// game time is 30sec each game
+
+
+// Baloons crates in same time
