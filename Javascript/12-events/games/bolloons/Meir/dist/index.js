a4e743e41e66d0c985705c3a660139e1484f82b2
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-03-27 00:08:42 +0300

    newfiles

--- Javascript/12-events/games/bolloons/Meir/dist/index.js
+++ Javascript/12-events/games/bolloons/Meir/dist/index.js
@@ -3,13 +3,13 @@ console.dir(ballons);
 ///////// Function create ballon
 function createBalloon() {
     var balloonId = uid();
-    document.body.innerHTML += "<img id=" + balloonId + " 'class=\"ballon\" src=\"ballon.svg.png\"'/>";
+    document.body.innerHTML += "<img id='" + balloonId + "' class=\"balloon\" src=\"balloons.png\"/>";
     var balloon = document.querySelector("#" + balloonId);
     moveBalloon(balloon);
 }
-//////// Function move baloon
+//////// Function move baloon ramdomly
 function moveBalloon(balloon) {
-    balloon.style.top = '115vh';
+    balloon.style.top = '110vh';
     balloon.style.left = Math.random() * 90 + "vw";
     setTimeout(function () {
         balloon.style.top = '-15vh';
@@ -19,3 +19,11 @@ setInterval(createBalloon, 4000);
 function uid() {
     return "id-" + Math.ceil(Math.random() * 1e8);
 }
+ballons.forEach(function (box) {
+    box.onclick = exploteBalloon;
+    function exploteBalloon() {
+        document.getElementById("image").src = "bombPicture.png";
+        var audio = new Audio("poomballon.wav");
+        audio.play();
+    }
+});
--- Javascript/12-events/games/bolloons/Meir/index.ts
+++ Javascript/12-events/games/bolloons/Meir/index.ts
@@ -4,20 +4,21 @@ console.dir(ballons);
 ///////// Function create ballon
 function createBalloon(){
     const balloonId = uid();
-    document.body.innerHTML += `<img id=${balloonId} 'class="ballon" src="ballon.svg.png"'/>`
+    document.body.innerHTML += `<img id='${balloonId}' class="balloon" src="balloons.png"/>`
     const balloon:HTMLImageElement = document.querySelector(`#${balloonId}`);
     moveBalloon(balloon);
     
 }
 
-//////// Function move baloon
+//////// Function move baloon ramdomly
 function moveBalloon(balloon:HTMLImageElement){
-    balloon.style.top = '115vh';
+    balloon.style.top = '110vh';
     balloon.style.left = `${Math.random()*90}vw`
    
     setTimeout(()=>{
         balloon.style.top = '-15vh'
     },100)
+    
 }
 
 setInterval(createBalloon,4000)
@@ -25,6 +26,21 @@ setInterval(createBalloon,4000)
 function uid(){
     return `id-${Math.ceil(Math.random()*1e8)}`
 }
+
+ballons.forEach((box:any)=>{
+    
+    box.onclick = exploteBalloon;
+    
+    function exploteBalloon() {
+        document.getElementById("image").src="bombPicture.png";
+        let audio = new Audio("poomballon.wav");
+        audio.play();
+    }
+})
+  
+
+
+
 
 
 
