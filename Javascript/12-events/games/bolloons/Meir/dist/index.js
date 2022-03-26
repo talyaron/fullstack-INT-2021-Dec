6059b5cb669f0c367a971e255ae10f7afb3a48d1
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-03-26 22:11:42 +0300

    newfiles

--- Javascript/12-events/games/bolloons/Meir/dist/index.js
+++ Javascript/12-events/games/bolloons/Meir/dist/index.js
@@ -1,22 +1,21 @@
-// const ballons:NodeListOf<HTMLImageElement> = document.querySelectorAll("#ballon");
-// console.dir(ballons);
+var ballons = document.querySelectorAll("#ballon");
+console.dir(ballons);
 ///////// Function create ballon
 function createBalloon() {
     var balloonId = uid();
     document.body.innerHTML += "<img id=" + balloonId + " class=\"ballon\" src=\"ballon.svg.png\"/>";
-    var ballon = document.querySelector("#" + balloonId);
-    balloonId.onclick = bombPicture;
+    var balloon = document.querySelector("#" + balloonId);
     moveBalloon(balloon);
 }
 //////// Function move baloon
 function moveBalloon(balloon) {
-    balloon.style.top = '90vh';
+    balloon.style.top = '115vh';
     balloon.style.left = Math.random() * 90 + "vw";
-    balloon.onclick = bombPicture;
     setTimeout(function () {
-        balloon.style.top = '-16vh';
-    }, 700);
+        balloon.style.top = '-15vh';
+    }, 100);
 }
+setInterval(createBalloon, 4000);
 function uid() {
     return "id-" + Math.ceil(Math.random() * 1e8);
 }
--- Javascript/12-events/games/bolloons/Meir/index.ts
+++ Javascript/12-events/games/bolloons/Meir/index.ts
@@ -1,24 +1,26 @@
-// const ballons:NodeListOf<HTMLImageElement> = document.querySelectorAll("#ballon");
-// console.dir(ballons);
+const ballons:NodeListOf<HTMLImageElement> = document.querySelectorAll("#ballon");
+console.dir(ballons);
 
 ///////// Function create ballon
 function createBalloon(){
     const balloonId = uid();
     document.body.innerHTML += `<img id=${balloonId} class="ballon" src="ballon.svg.png"/>`
-    let ballon:any = document.querySelector(`#${balloonId}`);
-    balloonId.onclick=bombPicture
-    moveBalloon(balloon)
+    const balloon:HTMLImageElement = document.querySelector(`#${balloonId}`);
+    moveBalloon(balloon);
+    
 }
 
 //////// Function move baloon
 function moveBalloon(balloon:HTMLImageElement){
-    balloon.style.top = '90vh';
+    balloon.style.top = '115vh';
     balloon.style.left = `${Math.random()*90}vw`
-    balloon.onclick = bombPicture
+   
     setTimeout(()=>{
-        balloon.style.top = '-16vh'
-    },700)
+        balloon.style.top = '-15vh'
+    },100)
 }
+
+setInterval(createBalloon,4000)
 
 function uid(){
     return `id-${Math.ceil(Math.random()*1e8)}`
