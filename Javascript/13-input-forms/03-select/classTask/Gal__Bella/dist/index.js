a8908956b6dd34bdb070a5b5aed61e12540998af
Bella-UI <bellabrailovsky@gmail.com>
2022-03-27 20:26:41 +0300

    ...

--- Javascript/13-input-forms/03-select/classTask/Gal__Bella/dist/index.js
+++ Javascript/13-input-forms/03-select/classTask/Gal__Bella/dist/index.js
@@ -1,10 +1,39 @@
 function handleSelectCountry(ev) {
     console.log(ev.target.value);
 }
-var flag = document.querySelector('.img');
-function handleUrl(ev) {
-    console.dir(ev);
-    var inputNumber = document.querySelector("#" + ev.target.name);
-    var value = ev.target.value;
-    inputNumber.innerHTML = value;
+var countries = document.querySelectorAll('.option');
+var findFlag = countriesFlags.find(countries);
+var countriesFlags = [
+    {
+        name: "israel",
+        src: "https://www.countryflags.com/wp-content/uploads/israel-flag-png-large.png"
+    },
+    {
+        name: "Greece",
+        src: "https://www.countryflags.com/wp-content/uploads/israel-flag-png-large.png"
+    },
+    {
+        name: "U.S.A",
+        src: "https://www.countryflags.com/wp-content/uploads/israel-flag-png-large.png"
+    }
+];
+function findCountry() {
+    for (var i = 0, i = void 0; , i++;)
+        ;
+}
+function countryOptions(option) {
+    return "<option value=" + option + ">" + option + "</option>";
+}
+function handleUrl(options, functionName, element) {
+    try {
+        var html_1 = "<select onchange=\"" + functionName + "(event)\"> \n        <option value=\"\" disabled selected>Choose</option>";
+        options.forEach(function (el) {
+            html_1 += countryOptions(el);
+        });
+        html_1 += "</select>";
+        element.innerHTML = html_1;
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
--- Javascript/13-input-forms/03-select/classTask/Gal__Bella/index.ts
+++ Javascript/13-input-forms/03-select/classTask/Gal__Bella/index.ts
@@ -1,13 +1,51 @@
+
 function handleSelectCountry(ev: any){
     console.log(ev.target.value)
 }
+const countries:any = document.querySelectorAll('.option')
+const findFlag = countriesFlags.find(countries)
 
-const flag:HTMLImageElement = document.querySelector('.img');
+
+const countriesFlags:Array<any> = [
+    {
+        name:"israel", 
+        src: "https://www.countryflags.com/wp-content/uploads/israel-flag-png-large.png"
+    },
+    {
+        name:"Greece", 
+        src: "https://www.countryflags.com/wp-content/uploads/israel-flag-png-large.png"
+    },
+    {
+        name:"U.S.A", 
+        src: "https://www.countryflags.com/wp-content/uploads/israel-flag-png-large.png"
+    }
+]
 
 
-function handleUrl (ev){
-    console.dir(ev)
-    let inputNumber:HTMLElement = document.querySelector(`#${ev.target.name}`);
-    const value = ev.target.value;
-    inputNumber.innerHTML = value
+
+
+function findCountry (){
+    for (let i=0, i<countriesFlags.length, i++)
+}
+
+
+
+
+function countryOptions(option:string){
+    return `<option value=${option}>${option}</option>`
+}
+
+function handleUrl(options:Array<string>, functionName:URL, element:Element){
+    try {
+        let html = `<select onchange="${functionName}(event)"> 
+        <option value="" disabled selected>Choose</option>`;
+        options.forEach(el => {
+            html += countryOptions(el)
+        });
+        html += `</select>`
+        element.innerHTML = html;
+
+    } catch (error) {
+        console.log(error)
+    }
 }
