2d332d8625234a86349ba2ec31f61abaf2db4513
adamepel <adamepel11@gmail.com>
2022-03-27 20:26:24 +0300

    1

---
+++ Javascript/13-input-forms/03-select/classTask/Lital + Adam/complex/index.ts
@@ -0,0 +1,18 @@
+
+function renderimg(event:any){
+    console.log(event)
+    switch (event.target.value)
+    
+    {
+        case "messi":
+            document.getElementById("selectFootball").innerHTML="<img src='messi.jpg'>";
+            break;
+            case "swissFlag":
+                document.getElementById("flagImg").innerHTML="<img src='./dist/swiss.png'>";
+                break;
+                case "japanFlag":
+                    document.getElementById("flagImg").innerHTML="<img src='./dist/japan.png'>";
+                    break;
+
+    }
+}
