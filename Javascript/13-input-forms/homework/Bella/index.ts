e605cd3763b27feaf911a6701da50036f1f904db
Bella-UI <bellabrailovsky@gmail.com>
2022-03-27 18:19:49 +0300

    ...

--- Javascript/13-input-forms/homework/Bella/index.ts
+++ Javascript/13-input-forms/homework/Bella/index.ts
@@ -1,1 +1,9 @@
+const form = document.querySelector('#root')
 
+function handlePhoneNumber(event) {
+    console.dir(event)
+    let inputNumber:HTMLElement = document.querySelector(`#${event.target.name}`);
+    const value = event.target.value;
+    inputNumber.innerHTML = value
+    form.append(value);
+};
