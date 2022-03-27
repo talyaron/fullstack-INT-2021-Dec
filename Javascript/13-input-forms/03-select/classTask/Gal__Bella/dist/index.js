fd432554f7f2e67ef55225911eebbf8e090ff813
Bella-UI <bellabrailovsky@gmail.com>
2022-03-27 19:48:43 +0300

    ...

--- Javascript/13-input-forms/03-select/classTask/Gal__Bella/dist/index.js
+++ Javascript/13-input-forms/03-select/classTask/Gal__Bella/dist/index.js
@@ -1,3 +1,10 @@
-function handleSelectFlavor(ev) {
+function handleSelectCountry(ev) {
     console.log(ev.target.value);
+}
+var flag = document.querySelector('.img');
+function handleUrl(ev) {
+    console.dir(ev);
+    var inputNumber = document.querySelector("#" + ev.target.name);
+    var value = ev.target.value;
+    inputNumber.innerHTML = value;
 }
