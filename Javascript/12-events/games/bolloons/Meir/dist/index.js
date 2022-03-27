36b5b58793ab030f92128ea1e52e294568995e34
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-03-27 15:47:39 +0300

    otherfiles

--- Javascript/12-events/games/bolloons/Meir/dist/index.js
+++ Javascript/12-events/games/bolloons/Meir/dist/index.js
@@ -1,25 +1,39 @@
-var ballons = document.querySelectorAll("#ballon");
+var ballons = document.querySelectorAll(".balloon");
 console.dir(ballons);
 ///////// Function create ballon
 function createBalloon() {
-    var balloonId = uid();
-    document.body.innerHTML += "<img id='" + balloonId + "' class=\"balloon\" src=\"balloons.png\"/>";
-    var balloon = document.querySelector("#" + balloonId);
-    moveBalloon(balloon);
+    var balloonId = document.createElement('div');
+    body.append(balloonId);
+    balloonId.classList.add('balloon');
+    var balloonImg = document.createElement('img');
+    balloonId.append(balloonImg);
+    balloonImg.src = "ballons.png";
     balloonId.addEventListener('click', handleClick);
     return balloonId;
+    moveBalloon(balloon);
 }
 //////// Function move baloon ramdomly
-function moveBalloon(balloon) {
-    balloon.style.top = '110vh';
-    balloon.style.left = Math.random() * 90 + "vw";
+function moveBalloon(balloon, interval) {
+    //// balloon.style.top = '110vh';
+    //// balloon.style.left = `${Math.random()*90}vw`
+    balloon.style.left = getRandomPosition() + 'vw';
     setTimeout(function () {
-        balloon.style.top = '-15vh';
-    }, 100);
+        balloon.style.top = '-25vh';
+    }, interval);
 }
-setInterval(createBalloon, 4000);
-function uid() {
-    return "id-" + Math.ceil(Math.random() * 1e8);
+// Function that get random start position:
+function getRandomPosition() {
+    var random = Math.floor(Math.random() * 50);
+    return random;
+}
+// ////function uid(){
+//     return `id-${Math.ceil(Math.random()*1e8)}`
+// }
+// Function that get random time that new balloons created in screen and start go up:
+function getRandomInterval() {
+    var random = Math.floor(Math.random() * 10000);
+    console.log(random / 1000 + "s");
+    return random;
 }
 ///////// Function explote ballons
 function handleClick(ev) {
@@ -28,7 +42,7 @@ function handleClick(ev) {
     audio.play();
     ev.target.src = 'bombPicture.png';
 }
-// ballons.forEach((box:any)=>{
+////// ballons.forEach((box:any)=>{
 //     box.onclick = exploteBalloon;
 //     function exploteBalloon() {
 //         const balloonId = uid();
@@ -38,3 +52,12 @@ function handleClick(ev) {
 //         audio.play();
 //     }
 // })
+// Start game function , create balloon that start go up after 5 seconds :
+function startGame() {
+    balloons.forEach(function (balloon) {
+        moveBalloon(createBalloon(), 5000);
+    });
+    moveBalloon(createBalloon(), getRandomInterval());
+}
+// Time to start a new game:
+setInterval(startGame, 2000);
--- Javascript/12-events/games/bolloons/Meir/index.ts
+++ Javascript/12-events/games/bolloons/Meir/index.ts
@@ -1,31 +1,45 @@
-const ballons:NodeListOf<HTMLImageElement> = document.querySelectorAll("#ballon");
+const ballons:NodeListOf<HTMLImageElement> = document.querySelectorAll(".balloon");
 console.dir(ballons);
 
 ///////// Function create ballon
-function createBalloon(){
-    const balloonId = uid();
-    document.body.innerHTML += `<img id='${balloonId}' class="balloon" src="balloons.png"/>`
-    const balloon:HTMLImageElement = document.querySelector(`#${balloonId}`);
-    moveBalloon(balloon);
+function createBalloon(): any{
+    const balloonId = document.createElement('div')
+    body.append(balloonId)
+    balloonId.classList.add('balloon')
+    const balloonImg = document.createElement('img')
+    balloonId.append(balloonImg)
+    balloonImg.src = "ballons.png"
     balloonId.addEventListener('click',handleClick)
     return balloonId
+    moveBalloon(balloon)
 }
 
 //////// Function move baloon ramdomly
-function moveBalloon(balloon:HTMLImageElement){
-    balloon.style.top = '110vh';
-    balloon.style.left = `${Math.random()*90}vw`
+function moveBalloon(balloon, interval: number){
+    //// balloon.style.top = '110vh';
+    //// balloon.style.left = `${Math.random()*90}vw`
+    balloon.style.left = getRandomPosition() + 'vw';
    
     setTimeout(()=>{
-        balloon.style.top = '-15vh'
-    },100)
+        balloon.style.top = '-25vh'
+    }, interval)
     
 }
+// Function that get random start position:
+function getRandomPosition(): number {
+    let random = Math.floor(Math.random() * 50);
+    return random
+}
 
-setInterval(createBalloon,4000)
+// ////function uid(){
+//     return `id-${Math.ceil(Math.random()*1e8)}`
+// }
 
-function uid(){
-    return `id-${Math.ceil(Math.random()*1e8)}`
+// Function that get random time that new balloons created in screen and start go up:
+function getRandomInterval(): number {
+    let random = Math.floor(Math.random() * 10000);
+    console.log(`${random / 1000}s`)
+    return random
 }
 
 ///////// Function explote ballons
@@ -35,7 +49,7 @@ function handleClick(ev) {
     audio.play();
     ev.target.src='bombPicture.png'
 }
-// ballons.forEach((box:any)=>{
+////// ballons.forEach((box:any)=>{
     
 //     box.onclick = exploteBalloon;
     
@@ -47,6 +61,19 @@ function handleClick(ev) {
 //         audio.play();
 //     }
 // })
+
+// Start game function , create balloon that start go up after 5 seconds :
+function startGame() {
+    balloons.forEach(balloon => {
+        moveBalloon(createBalloon(), 5000)
+    })
+    moveBalloon(createBalloon(), getRandomInterval())
+}
+
+// Time to start a new game:
+setInterval(startGame, 2000)
+
+
   
 
 
