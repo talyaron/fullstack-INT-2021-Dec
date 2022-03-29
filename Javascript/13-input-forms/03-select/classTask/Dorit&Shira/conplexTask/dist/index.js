c8bb62337f465f2819e5c4d0726440d1dfbf7025
doritgy <63942750+doritgy@users.noreply.github.com>
2022-03-29 17:12:46 +0300

    hw

--- Javascript/13-input-forms/03-select/classTask/Dorit&Shira/conplexTask/dist/index.js
+++ Javascript/13-input-forms/03-select/classTask/Dorit&Shira/conplexTask/dist/index.js
@@ -3,23 +3,6 @@ var userimg = document.querySelector('.user_img');
 var basketballArray = ["KareenAbdulJabbar", "KevinDurant", "KobeBrayant", "MaginJonson", "MichalJordan", "StephenCurry"];
 var footballArray = ["CristianoRonaldo", "DiegoMaradona", "LionelMessi", "Pele", "ZinedineZidan"];
 var tennisArray = ["MariaSharapova", "NovakDjokovic", "RafaelNadal", "RogerFederer", "VenusWilliams"];
-// function checkurl(event) {
-//     const url:URL = event.target.value;
-//     userimg.src = `${url}`;
-// }
-// function checkfile() {
-//     const fileInput = document.getElementById('fileup');
-//     const selectedFile = fileInput.files[0];
-//     var path = (window.URL || window.webkitURL).createObjectURL(selectedFile);
-//     console.log('path', path);
-//     if (selectedFile.type === "image/jpeg") {
-//         console.log(`jpg`)
-//         userimg.src = `${path}`
-//     }
-//     else {
-//         alert(`Only upload jpg files please!`)
-//     }
-// }
 function handleSelectsports(event) {
     var sportType = event.target.value;
     var root = document.querySelector("#root");
@@ -57,50 +40,3 @@ function handleDisplay(event) {
     var outPut = document.querySelector(".img");
     outPut.innerHTML = "<img src=\"" + src + "\" width=\"120px\" height=\"120px\">";
 }
-//     switch (sportType) {
-//         case "basketball":
-//             handleBaskeball()
-//             break;
-//         case "football":
-//             handleFootball()
-//             break;
-//         case "tennis":
-//             handleTennis()
-//             break;
-//         default:
-//             console.log('defualt')
-//     } 
-// }
-// function handleBaskeball(){
-//     let root:HTMLElement = document.querySelector("#root")
-//     root.innerHTML+=`<select name="basketball" id="" multiple onchange="handleDisplayBB(event)">`
-//     root.innerHTML+=`<option value = "" disabled selected>Choose a player to display</option>`
-//     basketballArray.forEach((Element)=>{
-//     root.innerHTML+=`<option value="${Element}">${Element}</option>`  
-//     })
-//     root.innerHTML+=`</select>`
-// }
-// function handleFootball(){
-//     let root:HTMLElement = document.querySelector("#root")
-//     root.innerHTML+=`<select name="football" id="" multiple onchange="handleDisplayFB(event)">`
-//     root.innerHTML+=`<option value = "" disabled selected>Choose a player to display</option>`
-//     basketballArray.forEach((Element)=>{
-//     root.innerHTML+=`<option value="${Element}">${Element}</option>`   
-// })
-// root.innerHTML+=`</select>`
-// }
-// function handleTennis(){
-//     let root:HTMLElement = document.querySelector("#root")
-//     root.innerHTML+=`<select name="tennis" id="" multiple onchange="handleDisplayTn(event)">`
-//     root.innerHTML+=`<option value = "" disabled selected>Choose a player to display</option>`
-//     basketballArray.forEach((Element)=>{
-//     root.innerHTML+=`<option value="${Element}">${Element}</option>`   
-// })
-// root.innerHTML+=`</select>`
-// }    
-// <select name="sports" id="" multiple onchange="handleSelectsports(eve)">
-//             <option value="" disabled selected>Choose sports type</option>
-//             <option value="football">Football</option>
-//             <option value="basketball">Basketball</option>
-//             <option value="tennis">Tennis</option>
-//         </select>
--- Javascript/13-input-forms/03-select/classTask/Dorit&Shira/conplexTask/index.ts
+++ Javascript/13-input-forms/03-select/classTask/Dorit&Shira/conplexTask/index.ts
@@ -5,24 +5,6 @@ const footballArray:Array<string>=["CristianoRonaldo","DiegoMaradona","LionelMes
 const tennisArray:Array<string>=["MariaSharapova","NovakDjokovic","RafaelNadal","RogerFederer","VenusWilliams"]
 
 
-// function checkurl(event) {
-//     const url:URL = event.target.value;
-//     userimg.src = `${url}`;
-// }
-// function checkfile() {
-//     const fileInput = document.getElementById('fileup');
-//     const selectedFile = fileInput.files[0];
-//     var path = (window.URL || window.webkitURL).createObjectURL(selectedFile);
-//     console.log('path', path);
-//     if (selectedFile.type === "image/jpeg") {
-//         console.log(`jpg`)
-//         userimg.src = `${path}`
-//     }
-//     else {
-//         alert(`Only upload jpg files please!`)
-//     }
-// }
-
 function handleSelectsports(event){
     let sportType:string = event.target.value
     let root:HTMLElement = document.querySelector("#root")
@@ -63,72 +45,3 @@ function handleSelectsports(event){
         outPut.innerHTML = `<img src="${src}" width="120px" height="120px">`
     }
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     switch (sportType) {
-//         case "basketball":
-//             handleBaskeball()
-//             break;
-//         case "football":
-//             handleFootball()
-//             break;
-//         case "tennis":
-//             handleTennis()
-//             break;
-//         default:
-//             console.log('defualt')
-//     } 
-
-// }
-// function handleBaskeball(){
-//     let root:HTMLElement = document.querySelector("#root")
-//     root.innerHTML+=`<select name="basketball" id="" multiple onchange="handleDisplayBB(event)">`
-//     root.innerHTML+=`<option value = "" disabled selected>Choose a player to display</option>`
-//     basketballArray.forEach((Element)=>{
-//     root.innerHTML+=`<option value="${Element}">${Element}</option>`  
-
-//     })
-//     root.innerHTML+=`</select>`
-// }
-
-// function handleFootball(){
-//     let root:HTMLElement = document.querySelector("#root")
-//     root.innerHTML+=`<select name="football" id="" multiple onchange="handleDisplayFB(event)">`
-//     root.innerHTML+=`<option value = "" disabled selected>Choose a player to display</option>`
-//     basketballArray.forEach((Element)=>{
-//     root.innerHTML+=`<option value="${Element}">${Element}</option>`   
-
-// })
-// root.innerHTML+=`</select>`
-// }
-
-// function handleTennis(){
-//     let root:HTMLElement = document.querySelector("#root")
-//     root.innerHTML+=`<select name="tennis" id="" multiple onchange="handleDisplayTn(event)">`
-//     root.innerHTML+=`<option value = "" disabled selected>Choose a player to display</option>`
-//     basketballArray.forEach((Element)=>{
-//     root.innerHTML+=`<option value="${Element}">${Element}</option>`   
-// })
-// root.innerHTML+=`</select>`
-// }    
-
-
-// <select name="sports" id="" multiple onchange="handleSelectsports(eve)">
-//             <option value="" disabled selected>Choose sports type</option>
-//             <option value="football">Football</option>
-//             <option value="basketball">Basketball</option>
-//             <option value="tennis">Tennis</option>
-//         </select>
