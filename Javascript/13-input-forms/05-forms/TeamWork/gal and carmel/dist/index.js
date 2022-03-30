1d4dde0a1147d2d96aad32bc39101229e7b154a1
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-03-30 20:31:50 +0300

    dsfgdfg

--- Javascript/13-input-forms/05-forms/TeamWork/gal and carmel/dist/index.js
+++ Javascript/13-input-forms/05-forms/TeamWork/gal and carmel/dist/index.js
@@ -1,12 +1,44 @@
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
+var signUp = document.querySelector(".signUp");
+var logIn = document.querySelector(".logIn");
+var welc = document.querySelector(".welcome");
+var consol = {};
+var consolCheck = {};
 function handleSend(ev) {
     ev.preventDefault();
-    var inputelem = ev.target.elements;
-    var result = {};
-    // for (let i = 0; i < inputelem.length; i++)
-    // {
-    //     if (inputelem[i].name === 'username'){
-    //     }
-    // }
+    var userDet = ev.target.elements;
+    for (var i = 0; i < userDet.length; i++) {
+        if ((userDet[i].username) && (userDet[i].password)) {
+            console.log(userDet);
+        }
+        else if (userDet[i].name && userDet[i].value) {
+            consol[userDet[i].name] = userDet[i].value;
+        }
+    }
+    logIn.style.display = "inline";
+    signUp.style.display = "none";
+    console.log(consol);
 }
-function handleCheck(event) {
+function handleCheck(ev) {
+    ev.preventDefault();
+    var userElm = ev.target.elements;
+    for (var i = 0; i < userElm.length; i++) {
+        if ((userElm[i].username) && (userElm[i].password)) {
+            console.log(userElm);
+        }
+        else if (userElm[i].name && userElm[i].value) {
+            consolCheck[userElm[i].name] = userElm[i].value;
+            if (consol[i] === consolCheck[i]) {
+                alert(__makeTemplateObject(["you are in!"], ["you are in!"
+                    // welc.innerText = `welcome user ${consol[i]}`
+                    // welc.style.display = "inline"
+                ]));
+                // welc.innerText = `welcome user ${consol[i]}`
+                // welc.style.display = "inline"
+            }
+        }
+    }
 }
