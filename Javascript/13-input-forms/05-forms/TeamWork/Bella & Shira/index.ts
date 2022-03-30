cbbe63c3e625ff69087378ee9661281bb5222edf
Shira1401 <shiraamar44@gmail.com>
2022-03-30 20:51:40 +0300

    sign up form

--- Javascript/13-input-forms/05-forms/TeamWork/Bella & Shira/index.ts
+++ Javascript/13-input-forms/05-forms/TeamWork/Bella & Shira/index.ts
@@ -1,39 +1,31 @@
 
 function handleSend(ev: any) {
+    try {
     ev.preventDefault();
     console.log(ev);
-    const firstInput:any = ev.target.elements.firstInput.value;
-    const secondInput:any = ev.target.elements.secondInput.value;
+    const firstInput: any = ev.target.elements.firstInput.value;
+    const secondInput: any = ev.target.elements.secondInput.value;
     ev.target.elements.firstInput.value = '';
     ev.target.elements.secondInput.value = '';
 
+    console.log(Math.floor(firstInput / secondInput))
 
-    // return{
+    let result: any = document.querySelector('.result');
     
-    // 
-
-    console.log(Math.floor(firstInput/secondInput))
-
-    let result:any = document.querySelector('.result');
-    try{
-        const value = Math.floor(firstInput/secondInput)
+        const value = Math.floor(firstInput / secondInput)
         result.innerHTML = value
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
-    
-    // console.log(firstInput, secondInput)
 }
 
-// function result(event){
-//     console.dir(event)
-    // let result:HTMLDivElement = document.querySelector('.result');
-    // try{
-    //     const value = event.target.value;
-    //     result.innerHTML = value
-    // }
-    // catch(err){
-    //     console.log(err);
-    // }
-// }
+function handleLogin(ev){
+
+    let image: HTMLImageElement = document.querySelector('.img');
+    
+    const imgValue = ev.target.elements.image.value;
+    image.innerHTML = `<img src=${imgValue}>`
+
+    console.log(imgValue); 
+}   
