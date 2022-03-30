1d80a91d2722af0ac70ea5f68b71fc7630778356
adamepel <adamepel11@gmail.com>
2022-03-30 18:37:22 +0300

    111

--- Javascript/13-input-forms/05-forms/Exercises/Adam/dist/index.js
+++ Javascript/13-input-forms/05-forms/Exercises/Adam/dist/index.js
@@ -1,8 +1,7 @@
 function handleSend(ev) {
     ev.preventDefault();
     console.log(ev);
-    var name = ev.target.elements.name.value;
-    var username = ev.target.elements.username.value;
+    var username = ev.target.elements.UserName.value;
     var password = ev.target.elements.password.value;
-    console.log(name, username, password);
+    console.log(username, password);
 }
--- Javascript/13-input-forms/05-forms/Exercises/Adam/index.ts
+++ Javascript/13-input-forms/05-forms/Exercises/Adam/index.ts
@@ -2,11 +2,11 @@
 function handleSend(ev: any) {
     ev.preventDefault();
     console.log(ev);
-    const name = ev.target.elements.name.value;
-    const username = ev.target.elements.username.value;
+    
+    const username = ev.target.elements.UserName.value;
     const password= ev.target.elements.password.value;
     
   
-    console.log(name, username, password);
+    console.log( username, password);
 }
 
