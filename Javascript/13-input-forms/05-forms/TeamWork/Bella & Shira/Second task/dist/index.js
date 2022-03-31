7392e68df4899647dd08c224532cd533cd83015f
Bella-UI <bellabrailovsky@gmail.com>
2022-03-31 18:26:56 +0300

    31/03/22

---
+++ Javascript/13-input-forms/05-forms/TeamWork/Bella & Shira/Second task/dist/index.js
@@ -0,0 +1,56 @@
+// function handleSend(ev: any) {
+//     try {
+//         ev.preventDefault();
+//         console.log(ev);
+//         const firstInput: any = ev.target.elements.firstInput.value;
+//         const secondInput: any = ev.target.elements.secondInput.value;
+//         ev.target.elements.firstInput.value = '';
+//         ev.target.elements.secondInput.value = '';
+//         console.log(Math.floor(firstInput / secondInput))
+//         let result: any = document.querySelector('.result');
+//         const value = Math.abs(firstInput / secondInput)
+//         result.innerHTML = value
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+function handleSignUp(ev) {
+    try {
+        ev.preventDefault();
+        var userName = ev.target.elements.userName.value;
+        var password = ev.target.elements.password.value;
+        var repeatPassword = ev.target.elements.repeatPassword.value;
+        var image = document.querySelector('.img');
+        var imgValue = ev.target.elements.image.value;
+        image.innerHTML = "<img src=" + imgValue + ">";
+        var person = {
+            personUserName: "" + userName,
+            personPassword: "" + password,
+            personRepeatPassword: "" + repeatPassword
+        };
+        console.log(person);
+        // compare passwords
+        if (repeatPassword != password) {
+            alert('Your passwords does not match, try again!');
+            console.log('false password');
+            return false;
+        }
+        else {
+            console.log(password);
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+// challange:
+// medium + create on the same page a login form do it it dinamicly (after submit). 
+// and then chack if password and username are matching theses from register
+//SIGN UP - הרשמה
+// sign up datails are saved
+//Login - כניסה
+//enter user datails
+//compare if the datails are match
+//if userName !== loginusername{
+//     return alert("false")
+// }
--- Javascript/13-input-forms/05-forms/TeamWork/Bella & Shira/dist/new.js
+++
@@ -1,9 +0,0 @@
-function handleSend(ev) {
-    ev.preventDefault();
-    console.log(ev);
-    var userName = ev.target.elements.userName.value;
-    var password = ev.target.elements.password.value;
-    ev.target.elements.userName.value = '';
-    ev.target.elements.password.value = '';
-    console.log(userName, password);
-}
