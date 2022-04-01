f35b28782c57e2c2046e0a074cb72a592fff776c
doritgy <63942750+doritgy@users.noreply.github.com>
2022-04-01 16:19:27 +0300

    fix homework

--- Javascript/13-input-forms/05-forms/TeamWork/dorit - hanoch/dist/index.js
+++ Javascript/13-input-forms/05-forms/TeamWork/dorit - hanoch/dist/index.js
@@ -1,25 +1,24 @@
 var root = document.querySelector("#root");
+var signUpPassword = "";
 var result = {};
 function handleSend(ev) {
-    console.log(ev);
+    //console.log(ev)
     ev.preventDefault();
     var elements = ev.target.elements;
     //console.log(elements)()
-    console.log('first for');
+    //console.log('first for')
     for (var i = 0; i < elements.length; i++) {
         if (elements[i].name && elements[i].value) {
             result[elements[i].name] = elements[i].value;
         }
     }
-    var img = document.querySelector("#root");
     var value = result['img'];
-    img.innerHTML = "<img src=\"" + value + "\" width=\"100px\" height=\"100px\">";
-    if (result['pass'] == result['repass']) {
-        alert('ok');
+    root.innerHTML = "<img src=\"" + value + "\" width=\"100px\" height=\"100px\">";
+    if (result['pass'] === result['repass']) {
+        signUpPassword = result['pass'];
+        alert('ok,  now please enter signin Form');
+        handleLoginForm();
     }
-    var login = document.querySelector(".login");
-    login.innerHTML = "please login";
-    login.innerHTML = "<form onsubmit=\"handleCheck(event)\"> <input type=\"text\" name=\"loginName\" placeholder=\"name\"> <input type=\"password\" name=\"loginPass\" placeholder=\"password\"></form>";
 }
 function handleLoginForm() {
     var html = "<form onsubmit=\"handleSendSignIn(event)\">\n        <input type=\"text\" name=\"name\" id=\"name\" placeholder=\"name\">\n        <input type=\"password\" name=\"pass\" placeholder=\"password\">\n        <button type=\"submit\">SEND</button>\n        </form>";
@@ -30,13 +29,13 @@ function handleSendSignIn(ev) {
     ev.preventDefault();
     var resultNew = {};
     var elementsNew = ev.target.elements;
-    console.log(elementsNew);
+    //console.log(elementsNew)
     for (var i = 0; i < elementsNew.length; i++) {
         if (elementsNew[i].name && elementsNew[i].value) {
             resultNew[elementsNew[i].name] = elementsNew[i].value;
         }
     }
-    console.log(elementsNew);
+    //console.log(elementsNew)
     if (resultNew['pass'] === signUpPassword) {
         alert('Hi, you were authorised');
     }
--- Javascript/13-input-forms/05-forms/TeamWork/dorit - hanoch/index.ts
+++ Javascript/13-input-forms/05-forms/TeamWork/dorit - hanoch/index.ts
@@ -1,29 +1,29 @@
-let root= document.querySelector("#root")
+let root:HTMLElement= document.querySelector("#root")
+let signUpPassword:string = ""
 const result = {};
 function handleSend(ev){
-    console.log(ev)
+    //console.log(ev)
     ev.preventDefault();
     const elements = ev.target.elements;
    
     //console.log(elements)()
-  console.log('first for')
+  //console.log('first for')
   for (let i = 0; i < elements.length; i++) {
       if (elements[i].name && elements[i].value) {
       result[elements[i].name] = elements[i].value;
     }
     
 }
-let img = document.querySelector("#root")
-let value = result['img']
-img.innerHTML = `<img src="${value}" width="100px" height="100px">`
-if (result['pass'] == result['repass']){
-    alert('ok')
-}
-const login = document.querySelector(".login")
-login.innerHTML = "please login"
-login.innerHTML = `<form onsubmit="handleCheck(event)"> <input type="text" name="loginName" placeholder="name"> <input type="password" name="loginPass" placeholder="password"></form>`
 
+let value = result['img']
+root.innerHTML = `<img src="${value}" width="100px" height="100px">`
+if (result['pass'] === result['repass']){
+    signUpPassword = result['pass']
+    alert('ok,  now please enter signin Form')
+    handleLoginForm()
 }
+}
+
 function handleLoginForm(){
         let html = `<form onsubmit="handleSendSignIn(event)">
         <input type="text" name="name" id="name" placeholder="name">
@@ -38,13 +38,13 @@ function handleSendSignIn(ev){
     ev.preventDefault();
     let resultNew = {}
     const elementsNew = ev.target.elements
-    console.log(elementsNew)
+    //console.log(elementsNew)
     for (let i = 0; i < elementsNew.length; i++) {
         if (elementsNew[i].name && elementsNew[i].value) {
           resultNew[elementsNew[i].name] = elementsNew[i].value;
           }
       } 
-      console.log(elementsNew)
+      //console.log(elementsNew)
       if (resultNew['pass'] === signUpPassword){
           alert('Hi, you were authorised')
         
