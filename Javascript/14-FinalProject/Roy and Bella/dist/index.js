74777fd7fd35a4e453f52dfb6a1a696f593afb36
Bella-UI <bellabrailovsky@gmail.com>
2022-04-03 20:26:40 +0300

    ...

--- Javascript/14-FinalProject/Roy and Bella/dist/index.js
+++ Javascript/14-FinalProject/Roy and Bella/dist/index.js
@@ -1,54 +1,71 @@
-var users = []; // Creating Fake Data Base To Hold All new Users
-function handleSignUp(ev) {
-    try {
-        ev.preventDefault();
-        var userName = ev.target.elements.userName.value;
-        var password = ev.target.elements.password.value;
-        var repeatPassword = ev.target.elements.repeatPassword.value;
-        var image = document.querySelector('.img');
-        var imgValue = ev.target.elements.image.value;
-        image.innerHTML = "<img src=" + imgValue + ">";
-        var person = {
-            personUserName: "" + userName,
-            personPassword: "" + password
-        };
-        console.log(person);
-        // compare passwords
-        if (repeatPassword != password) {
-            alert('Your passwords does not match, try again!');
-            console.log('false password');
-            return false;
+var tasks = []; // Creating Fake Data Base To Hold All new Users
+function handleTask(event) {
+    event.preventDefault();
+    // console.log(event);
+    // const heading: any = event.target.elements.heading.value;
+    // const description: any = event.target.elements.description.value;
+    // const startTime: any = event.target.elements.startTime.value;
+    // const endTime: any = event.target.elements.endTime.value;
+    // console.log(heading, description, startTime, endTime);
+    var elements = event.target.elements;
+    var result = {};
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].type === "checkbox") {
+            result[elements[i].name] = elements[i].checked;
         }
-        else {
-            users.push(person); // Pushing new user information to fake data base 
-            console.log(password);
+        else if (elements[i].name && elements[i].value) {
+            result[elements[i].name] = elements[i].value;
         }
     }
-    catch (err) {
-        console.log(err);
-    }
+    console.log(result);
 }
-function handleLogin(ev) {
-    try {
-        ev.preventDefault();
-        var loginUserName_1 = ev.target.elements.loginUserName.value;
-        var loginPassword_1 = ev.target.elements.loginPassword.value;
-        var isConnected_1 = false;
-        console.log(loginUserName_1, loginPassword_1);
-        // Checking if username and password exsixt in data base
-        users.forEach(function (userInfo) {
-            if (userInfo['personUserName'] == loginUserName_1) {
-                if (userInfo['personPassword'] == loginPassword_1) {
-                    isConnected_1 = true;
-                }
-            }
-        });
-        isConnected_1 ? alert('Connected') : alert('Username or password were inccorent');
-    }
-    catch (err) {
-        console.log(err);
-    }
-}
+// function handleSignUp(event: any) {
+//     try {
+//         event.prevententDefault();
+//         const userName: any = event.target.elements.userName.value;
+//         const password: any = event.target.elements.password.value;
+//         const repeatPassword: any = event.target.elements.repeatPassword.value;
+//         const image: HTMLImageElement = document.querySelector('.img');
+//         const imgValue = event.target.elements.image.value;
+//         image.innerHTML = `<img src=${imgValue}>`
+//         const person = {
+//             personUserName: `${userName}`,
+//             personPassword: `${password}`,
+//         };
+//         console.log(person);
+//         // compare passwords
+//         if (repeatPassword != password) {
+//             alert('Your passwords does not match, try again!')
+//             console.log('false password')
+//             return false;
+//         } else {
+//             users.push(person); // Pushing new user information to fake data base 
+//             console.log(password);
+//         }
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+// function handleLogin(ev: any) {
+//     try {
+//         ev.preventDefault();
+//         const loginUserName: any = ev.target.elements.loginUserName.value;
+//         const loginPassword: any = ev.target.elements.loginPassword.value;
+//         let isConnected: boolean = false;
+//         console.log(loginUserName, loginPassword);
+//         // Checking if username and password exsixt in data base
+//         users.forEach(userInfo => {
+//             if (userInfo['personUserName'] == loginUserName) {
+//                 if (userInfo['personPassword'] == loginPassword) {
+//                     isConnected = true; 
+//                 }
+//             }
+//         })
+//         isConnected ? alert('Connected') : alert('Username or password were inccorent');
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
 // challange:
 // medium + create on the same page a login form do it it dinamicly (after submit).
 // and then chack if password and username are matching theses from register
