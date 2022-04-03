1f53d3c3055a22f15545023012f72a55c47345f6
LitalCohen479 <litalcohen479@gmail.com>
2022-04-03 17:26:54 +0300

    inop

--- Javascript/13-input-forms/01-inputs/Exercise-Form/Lital Cohen/index.ts
+++ Javascript/13-input-forms/01-inputs/Exercise-Form/Lital Cohen/index.ts
@@ -39,15 +39,11 @@ function loadImg(event) {
 //checking (press login btn) if username and password matched as in siging up if yes the dom turn green and if not turns red//
 
 function checkThePassword(event) {
+  event.preventDefault();
   let valueOfUser: HTMLElement = document.getElementById("userName");
   let valueOfPass: HTMLElement = document.getElementById("password");
   let valueOfUserLog: HTMLElement = document.getElementById("userNameLogin");
   let valueOfPassLog: HTMLElement = document.getElementById("passwordLogin");
-
-  // console.log(valueOfUser.innerHTML);
-  // console.log(valueOfPass.innerHTML);
-  // console.log(valueOfUserLog.innerHTML);
-  // console.log(valueOfPassLog.innerHTML);
 
   if (
     document.getElementById("userName").innerHTML ===
@@ -55,13 +51,30 @@ function checkThePassword(event) {
     document.getElementById("password").innerHTML ===
       document.getElementById("passwordLogin").innerHTML
   ) {
-    console.log("user name + ps yes");
+    // ifPsMatch(valueOfPass)
+    // console.log("user name + ps yes");
     document.body.style.backgroundColor = "green";
+    let outPutOfPassRe: HTMLElement=document.getElementById("passwordLogin");
+    outPutOfPassRe.innerHTML =(`${valueOfPass.innerHTML} + ${valueOfPassLog.innerHTML}`)
+
   } else {
     console.log("no no");
     document.body.style.backgroundColor = "red";
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 //uploading file from the local pc and display on dom//
 
 function uploadFile(event) {
--- Javascript/13-input-forms/03-select/classTask/Lital + Adam/complex/index.ts
+++ Javascript/13-input-forms/03-select/classTask/Lital + Adam/complex/index.ts
@@ -1,11 +1,20 @@
 
  function showPlayerImg(event:any){
      console.log(event)
-     let myPlayer=document.getElementById('selectFootball');
+    //  let myPlayer=document.getElementById('selectFootball');
      if(event.target.value=="messi"){
-     let valueOpactiy=event.target.style;
-      valueOpactiy.opacity=0;
+         console.log("hey")
+     let valueOpactiy=event.target.value;
+      let outPutOfdiv: HTMLElement=document.getElementById("messiid");
+      outPutOfdiv.innerHTML =`<img src="./dist/messi.jpg">`;
+
+    
      }
     }
 
 
+    // let outPutOfdiv1: HTMLElement=document.getElementById("federerid");
+    // outPutOfdiv.innerHTML =`<img src="./dist/federer.jpg">`;
+
+    // let outPutOfdiv2: HTMLElement=document.getElementById("mjid");
+    // outPutOfdiv.innerHTML =`<img src="./dist/mj.jpg">`;
