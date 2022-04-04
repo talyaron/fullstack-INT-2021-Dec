c3cec3970811016efc872c0c5eedef6d8b05c234
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-04-04 21:19:32 +0300

    1

--- תרגול/2- JavaScript/04.04 - function + filter + sort + search/index.ts
+++ תרגול/2- JavaScript/04.04 - function + filter + sort + search/index.ts
@@ -2,6 +2,21 @@ const uid = function () {
     return Date.now().toString(36) + Math.random().toString(36).substr(2);
 };
 
+function fun() {
+    let x = 1;
+
+    fun2(x)
+
+    return fun2(x)
+}
+
+function fun2(x) {
+    let y = x + x
+    return y
+}
+
+console.log(fun())
+
 
 /*
  Write a JavaScript program to construct the following pattern, using a nested for loop.
@@ -10,7 +25,7 @@ const uid = function () {
  * * *
  * * * *
  * * * * *
- 
+
  let string = '*';
  let i;
  let j;
