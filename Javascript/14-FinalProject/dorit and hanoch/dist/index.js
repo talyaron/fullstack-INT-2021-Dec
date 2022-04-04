425d7fe52b95933921c367e2f510d59998294a15
hanochshachar <goralot550@gmail.com>
2022-04-04 13:11:40 +0300

    gg

--- Javascript/14-FinalProject/dorit and hanoch/dist/index.js
+++ Javascript/14-FinalProject/dorit and hanoch/dist/index.js
@@ -1,26 +1,51 @@
 var result = {};
 var user, _a = void 0;
 var manager = {
-    name: "Tal Yaron",
+    name: "tal Yaron",
     username: "tal",
     password: "1234"
 };
 function handleUser(ev) {
     ev.preventDefault();
     var details = ev.target.elements;
-    //console.dir(ev)
     for (var i = 0; i < details.length; i++) {
         if (details[i].name && details[i].value) {
             result[details[i].name] = details[i].value;
         }
     }
-    // console.log(`name from form ${result['name']}`)
-    // console.log(`name from array ${manager['name']}`)
-    // console.log(`password from form ${result['pass']}`)
-    // console.log(`password from arry ${manager['password']}`)
-    if (result['name'] === manager["name"] && result['pass'] === manager['password']) {
-        //console.log('its ok')
-        root = document.querySelector("#root");
-        window.location.href = "director.html";
+    console.log(result['name']);
+    if (result['name'] === manager['username'] && result['pass'] === manager['password']) {
+        window.location.href = "./index1.html";
     }
+}
+var handleNew = function (ev) {
+    var root = document.querySelector("#root");
+    root.innerHTML = "<div id=\"newitem\">\n                                <form action=\"\" >\n                                    <input type=\"text\" name=\"typeName\" placeholder=\"enter type of item\" onchange=\"handleItem1(event)\" >\n                                    <input type=\"file\" onchange=\"loadImg(event)\" id=\"file\" style=\"display: none;\">\n                                    <p><img id=\"output\" width=\"200\" /></p>\n                                    <p><label for=\"file\" style=\"cursor: pointer;\" onclick=\"handleHidden(event)\">Upload Image</label></p>\n                                    <input type=\"number\" name=\"price\" placeholder=\"inter a price\" onchange=\"handleItem0(event)\" >\n\n                                </form>\n                                \n                                <div id=\"addItemName\"></div>\n                                <div id=\"addItemPrice\"></div>\n                                </div>\n                                <button onclick=\"handleDelete(event)\"> delete item</button>";
+};
+var loadImg = function (event) {
+    var image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
+function handleHidden(ev) {
+    ev.target.hidden = true;
+}
+function handleItem1(ev) {
+    console.log(ev);
+    var content = ev.target.value;
+    var div = document.querySelector("#addItemName");
+    div.innerHTML = "<p>" + content + "</p>";
+    ev.target.hidden = true;
+}
+function handleItem0(ev) {
+    console.log(ev);
+    var content = ev.target.value;
+    var div = document.querySelector("#addItemPrice");
+    div.innerHTML = "<p>" + content + " \u05E9\"\u05D7</p>";
+    ev.target.hidden = true;
+}
+function handleDelete(ev) {
+    var catchItem = document.querySelector("#newitem");
+    console.log(catchItem);
+    catchItem.innerHTML = "";
+    ev.target.hidden = true;
 }
--- Javascript/14-FinalProject/dorit and hanoch/index.ts
+++ Javascript/14-FinalProject/dorit and hanoch/index.ts
@@ -13,7 +13,7 @@ let user:users{
 
 }
 const manager:users ={
-    name:"Tal Yaron",
+    name:"tal Yaron",
     username:"tal",
     password:"1234"
 
@@ -23,19 +23,63 @@ const manager:users ={
 function handleUser(ev){
     ev.preventDefault();
     const details = ev.target.elements
-    //console.dir(ev)
     for (let i = 0; i < details.length; i++) {
         if (details[i].name && details[i].value) {
         result[details[i].name] = details[i].value;
-        }
-    }
-    // console.log(`name from form ${result['name']}`)
-    // console.log(`name from array ${manager['name']}`)
-    // console.log(`password from form ${result['pass']}`)
-    // console.log(`password from arry ${manager['password']}`)
-    if (result['name'] === manager["name"] && result['pass'] === manager['password']){
-        //console.log('its ok')
-        root=document.querySelector("#root")
-        window.location.href = "director.html"
-    }
+  
+      }
+}
+console.log(result['name']) 
+if (result['name'] === manager['username'] && result['pass'] === manager['password']){
+    window.location.href="./index1.html"
+     
+}
+}
+const handleNew = function(ev){
+    const root = document.querySelector("#root")
+    root.innerHTML = `<div id="newitem">
+                                <form action="" >
+                                    <input type="text" name="typeName" placeholder="enter type of item" onchange="handleItem1(event)" >
+                                    <input type="file" onchange="loadImg(event)" id="file" style="display: none;">
+                                    <p><img id="output" width="200" /></p>
+                                    <p><label for="file" style="cursor: pointer;" onclick="handleHidden(event)">Upload Image</label></p>
+                                    <input type="number" name="price" placeholder="inter a price" onchange="handleItem0(event)" >
+
+                                </form>
+                                
+                                <div id="addItemName"></div>
+                                <div id="addItemPrice"></div>
+                                </div>
+                                <button onclick="handleDelete(event)"> delete item</button>`
+
+}
+var loadImg = function(event) {
+	var image:any = document.getElementById('output');
+	image.src = URL.createObjectURL(event.target.files[0]);
+}
+function handleHidden(ev){
+    ev.target.hidden = true
+}
+function handleItem1(ev){
+    console.log(ev)
+    const content = ev.target.value
+    const div = document.querySelector("#addItemName")
+     div.innerHTML = `<p>${content}</p>`
+     ev.target.hidden = true
+    
+}
+function handleItem0(ev){
+    console.log(ev)
+    const content = ev.target.value
+     const div = document.querySelector("#addItemPrice")
+    div.innerHTML = `<p>${content} ש"ח</p>`
+    ev.target.hidden = true
+
+    
+}
+function handleDelete(ev ){
+    const catchItem = document.querySelector("#newitem")
+    console.log(catchItem)
+    catchItem.innerHTML = ""
+    ev.target.hidden = true
 }
