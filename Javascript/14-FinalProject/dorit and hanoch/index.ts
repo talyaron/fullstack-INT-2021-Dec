40f4bce4685a439df3c6c3d6718f34c011813a72
doritgy <63942750+doritgy@users.noreply.github.com>
2022-04-04 10:08:05 +0300

    reidrect to another html

--- Javascript/14-FinalProject/dorit and hanoch/index.ts
+++ Javascript/14-FinalProject/dorit and hanoch/index.ts
@@ -23,19 +23,19 @@ const manager:users ={
 function handleUser(ev){
     ev.preventDefault();
     const details = ev.target.elements
-    console.dir(ev)
+    //console.dir(ev)
     for (let i = 0; i < details.length; i++) {
         if (details[i].name && details[i].value) {
         result[details[i].name] = details[i].value;
         }
     }
-    console.log(`name from form ${result['name']}`)
-    console.log(`name from array ${manager['name']}`)
-    console.log(`password from form ${result['pass']}`)
-    console.log(`password from arry ${manager['password']}`)
+    // console.log(`name from form ${result['name']}`)
+    // console.log(`name from array ${manager['name']}`)
+    // console.log(`password from form ${result['pass']}`)
+    // console.log(`password from arry ${manager['password']}`)
     if (result['name'] === manager["name"] && result['pass'] === manager['password']){
-        console.log('its ok')
+        //console.log('its ok')
         root=document.querySelector("#root")
-        root.innerHTML=`<a src="director.html"></a>`
+        window.location.href = "director.html"
     }
 }
