b74cf48efe9c15639ac1ee575111afbdaec64086
hanan <jigaart0@gmail.com>
2022-04-06 17:41:24 +0300

    גדש

---
+++ תרגול/Proj Hanan Hodaya Juda/app.ts
@@ -0,0 +1,29 @@
+const products = document.querySelector(`.products`)
+
+
+function refreshPro(){
+    let menu = [
+        {
+        "type": "pargit",
+         "price":  30,
+          "img": new Image
+          img.src='imgs/pargit.jpg',
+        },
+        {
+        "type": "ceves",
+        "price": 35
+        "img": 
+        },
+         {
+          
+            
+
+         },
+      ]
+ 
+
+
+
+
+    console.log(`check one two three`)
+}
