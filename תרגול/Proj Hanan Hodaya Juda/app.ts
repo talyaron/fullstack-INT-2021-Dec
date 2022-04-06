5128f7968f31c6dedfa8872e78a6a2775c55b097
hanan <jigaart0@gmail.com>
2022-04-06 20:17:01 +0300

    dsa

--- תרגול/Proj Hanan Hodaya Juda/app.ts
+++ תרגול/Proj Hanan Hodaya Juda/app.ts
@@ -2,22 +2,22 @@ const foodMenu = [
 
   {
 
-    type: "pargit",
+    type: "פרגית",
     price: 30,
     img: `imgs/pargit.jpg`,
   },
   {
-    type: "ceves",
+    type: "כבש",
     price: 35,
     img: 'imgs/ceves.jpg',
   },
   {
-    type: "chips",
+    type: "צ'יפס",
     price: 15,
     img: 'imgs/fries.jpg',
   },
   {
-    type: "anavim",
+    type: "ענבים",
     price: 30,
     img: `imgs/grape-juice-the-israelis.jpg`,
   },
@@ -26,29 +26,20 @@ const products: any = document.querySelector(".products")
 
 function refreshPro() {
   let html =
-    `<h2>product: ${foodMenu[0].type}<h2>
-  <h2>price: ${foodMenu[0].price}<h2>
+    `<h2>${foodMenu[0].type}<h2>
+  <h2>מחיר: ${foodMenu[0].price}<h2>
   <img src=${foodMenu[0].img}>
-  <h2>product: ${foodMenu[1].type}<h2>
-  <h2>price: ${foodMenu[1].price}<h2>
+  <h2>${foodMenu[1].type}<h2>
+  <h2>מחיר: ${foodMenu[1].price}<h2>
   <img src=${foodMenu[1].img}>
-  <h2>product: ${foodMenu[2].type}<h2>
-  <h2>price: ${foodMenu[2].price}<h2>
+  <h2> ${foodMenu[2].type}<h2>
+  <h2>מחיר: ${foodMenu[2].price}<h2>
   <img src=${foodMenu[2].img}>
-  <h2>product: ${foodMenu[3].type}<h2>
-  <h2>price: ${foodMenu[3].price}<h2>
+  <h2>${foodMenu[3].type}<h2>
+  <h2>מחיר: ${foodMenu[3].price}<h2>
   <img src=${foodMenu[3].img}>`;
   products.innerHTML = html
   
+  
 };
 
-function styleTheWeb(){
-
-
-
-
-
-
-
-
-};
--- תרגול/Proj Hanan Hodaya Juda/dist/app.js
+++ תרגול/Proj Hanan Hodaya Juda/dist/app.js
@@ -1,31 +1,28 @@
 var foodMenu = [
     {
-        type: "pargit",
+        type: "פרגית",
         price: 30,
         img: "imgs/pargit.jpg"
     },
     {
-        type: "ceves",
+        type: "כבש",
         price: 35,
         img: 'imgs/ceves.jpg'
     },
     {
-        type: "chips",
+        type: "צ'יפס",
         price: 15,
         img: 'imgs/fries.jpg'
     },
     {
-        type: "anavim",
+        type: "ענבים",
         price: 30,
         img: "imgs/grape-juice-the-israelis.jpg"
     },
 ];
 var products = document.querySelector(".products");
 function refreshPro() {
-    var html = "<h2>product: " + foodMenu[0].type + "<h2>\n  <h2>price: " + foodMenu[0].price + "<h2>\n  <img src=" + foodMenu[0].img + ">\n  <h2>product: " + foodMenu[1].type + "<h2>\n  <h2>price: " + foodMenu[1].price + "<h2>\n  <img src=" + foodMenu[1].img + ">\n  <h2>product: " + foodMenu[2].type + "<h2>\n  <h2>price: " + foodMenu[2].price + "<h2>\n  <img src=" + foodMenu[2].img + ">\n  <h2>product: " + foodMenu[3].type + "<h2>\n  <h2>price: " + foodMenu[3].price + "<h2>\n  <img src=" + foodMenu[3].img + ">";
+    var html = "<h2>" + foodMenu[0].type + "<h2>\n  <h2>\u05DE\u05D7\u05D9\u05E8: " + foodMenu[0].price + "<h2>\n  <img src=" + foodMenu[0].img + ">\n  <h2>" + foodMenu[1].type + "<h2>\n  <h2>\u05DE\u05D7\u05D9\u05E8: " + foodMenu[1].price + "<h2>\n  <img src=" + foodMenu[1].img + ">\n  <h2> " + foodMenu[2].type + "<h2>\n  <h2>\u05DE\u05D7\u05D9\u05E8: " + foodMenu[2].price + "<h2>\n  <img src=" + foodMenu[2].img + ">\n  <h2>" + foodMenu[3].type + "<h2>\n  <h2>\u05DE\u05D7\u05D9\u05E8: " + foodMenu[3].price + "<h2>\n  <img src=" + foodMenu[3].img + ">";
     products.innerHTML = html;
-}
-;
-function styleTheWeb() {
 }
 ;
