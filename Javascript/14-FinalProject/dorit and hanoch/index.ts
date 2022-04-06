cd4d8d99cda169a52dac51e59d6dc5957a5be89d
doritgy <63942750+doritgy@users.noreply.github.com>
2022-04-06 05:07:12 +0300

    new feature

--- Javascript/14-FinalProject/dorit and hanoch/index.ts
+++ Javascript/14-FinalProject/dorit and hanoch/index.ts
@@ -19,13 +19,8 @@ let result = {};
 let productsArr:Array<product> = []
 let output:HTMLImageElement=document.querySelector("#output")
 let fileName:string = ''
-
-
-
-
-// let user:users={
-
-// }
+let root: HTMLElement = document.querySelector("#root")
+let contOrBack:HTMLDivElement=document.querySelector("#contOrBack")
 const manager: users = {
     name: "Tal Yaron",
     username: "tal",
@@ -47,7 +42,7 @@ function handleUser(ev) {
 
 function handleNew(ev) {
 
-    const root: HTMLElement = document.querySelector("#root")
+    
     const myButton: HTMLElement = document.querySelector("#button2")
     myButton.style.backgroundColor = "gray"
     root.innerHTML += `<div id="newitem">
@@ -65,7 +60,6 @@ function handleNew(ev) {
                         </div>`
                         
 
-    //image.src = URL.createObjectURL(ev.target.files[0]);
     let newitem: HTMLDivElement = document.querySelector("#newitem")
     newitem.style.display = "flex"
     newitem.style.flexDirection = "column"
@@ -76,22 +70,16 @@ function handleNew(ev) {
     output.style.position="absolute"
     output.style.top = "50px"
     output.style.left="350px"
-    //newitem.style.border = "1px solid black"
-    //root.style.border="1px solid black"
-    //<p><label for="file" style="cursor: pointer;" onclick="handleHidden(event)">Upload Image</label></p>
 }
 
 function handleNewProduct(ev) {
     console.log("we are here")
     ev.preventDefault();
     const details = ev.target.elements
-    //console.dir(details)
     for (let i = 0; i < details.length; i++) {
         if (details[i].name && details[i].value) {
             if (details[i].name == "imageFile") {
                 result['imageFile'] = details['imageFile'].files[0]
-               // console.log(`typeof ${result['imageFile']}`)
-               //console.log(`result of imagefile:${result['imageFile']}`)
             }
             else {
                 result[details[i].name] = details[i].value;
@@ -99,17 +87,10 @@ function handleNewProduct(ev) {
         }
       
     }
-    //output = document.getElementById('#output');
+
     let fileinput:string = URL.createObjectURL(result["imageFile"])
     output.src = fileinput
-    console.log(typeof fileinput)
-    // output.src = URL.createObjectURL(result["imageFile"]);
-    // let fileInput:File = result["imageFile"];
-    // console.log(`fileInput: ${fileInput}`)
-    // let reader = new FileReader();
-    // let fileName = reader.readAsDataURL(fileInput);
-    // console.log(`typeof fileName ${typeof fileName}`)
-	// console.log(`reader.result:${reader.result}`);//base64encoded string
+    //console.log(typeof fileinput)
     let newProduct: product = {
         name: result['typeName'],
         serialNo: result['serialNo'],
@@ -118,15 +99,36 @@ function handleNewProduct(ev) {
         currency: result['currency'],
         pImage : fileinput
     }
-    console.log(`newProduct of typeName: ${newProduct['name']}`)
-    console.log(`newProduct of serialNo: ${newProduct['serialNo']}`)
-    console.log(`newProduct of description: ${newProduct['description']}`)
-    console.log(`newProduct of price: ${newProduct['price']}`)
-    console.log(`newProduct of currency: ${newProduct['currency']}`)
-    console.log(`newProduct of imgFile: ${newProduct['pImage']}`)
+    // console.log(`newProduct of typeName: ${newProduct['name']}`)
+    // console.log(`newProduct of serialNo: ${newProduct['serialNo']}`)
+    // console.log(`newProduct of description: ${newProduct['description']}`)
+    // console.log(`newProduct of price: ${newProduct['price']}`)
+    // console.log(`newProduct of currency: ${newProduct['currency']}`)
+    // console.log(`newProduct of imgFile: ${newProduct['pImage']}`)
     //console.log(`newProduct ${newProduct}`)
     productsArr.push(newProduct)
+    handleDirection('managerAddProduct')
+ 
+}
+function handleDirection(action:string){
+    if (action=='managerAddProduct'){
+        root.innerHTML+=
+        `<div id="contOrBack">
+        <h2>new product created</h2>
+        <button id="back" onclick =window.location.href = "index1.html">back to manager tasks</button>
+        <button id="cont" onclick="handleNew(event)">Add another Items</button> 
+        </div>`
+        contOrBack = document.querySelector("#contOrBack")
+        contOrBack.style.top = "600px"
+        contOrBack.style.left = "400px"
+        contOrBack.style.fontSize="10px"
 
+        // background-color: rgb(172, 143, 161);
+        // border: 1px solid black;
+        // border-radius: 10px;
+        // padding: 15px;
+        // margin-top: 15px;
+    }
 }
 
 // <p><img id="output" width="200" /></p>
