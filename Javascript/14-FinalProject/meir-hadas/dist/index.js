39dcbfd2c413dfca73cca8836c2060ff1410f2c4
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-04-06 23:54:32 +0300

    pre

--- Javascript/14-FinalProject/meir-hadas/dist/index.js
+++ Javascript/14-FinalProject/meir-hadas/dist/index.js
@@ -1,3 +1,24 @@
-function handleNote(event) {
-    event.preventDefault();
+var root = document.querySelector('.root');
+function handleNote(ev) {
+    ev.preventDefault();
+    var elements = ev.target.elements;
+    var result = {};
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].type === "text") {
+            result[elements[i].name] = elements[i].note;
+        }
+        else if (elements[i].name && elements[i].valueAsString) {
+            result[elements[i].name] = elements[i].valueAsString;
+        }
+        console.log(elements[i].name, elements[i].value);
+    }
+    console.log(result);
+    console.log(elements);
 }
+form.noteText.value === '';
+form.addButton.addEventListener(function (elm) {
+    elm.preventDefault();
+    if (form.noteText.value !== '') {
+        addNote();
+    }
+});
--- Javascript/14-FinalProject/meir-hadas/index.ts
+++ Javascript/14-FinalProject/meir-hadas/index.ts
@@ -1,5 +1,31 @@
-function handleNote(event:any){
-    event.preventDefault();
+const root = document.querySelector('.root')
 
+function handleNote(ev: any) {
+    ev.preventDefault();
+    
+  
+    const elements = ev.target.elements;
+    const result = {};
+    for (let i = 0; i < elements.length; i++) {
+      if (elements[i].type === "text") {
+        result[elements[i].name] = elements[i].note;
+      } else if (elements[i].name && elements[i].valueAsString) {
+        result[elements[i].name] = elements[i].valueAsString;
+      }
+      console.log(elements[i].name, elements[i].value)
+    }
+    console.log(result);
+    console.log(elements)
+    
+}    
 
-}
+form.noteText.value === '';
+
+form.addButton.addEventListener(elm => {
+    elm.preventDefault();  
+    if (form.noteText.value !== '' {
+      addNote();
+    }
+  })
+
+  
