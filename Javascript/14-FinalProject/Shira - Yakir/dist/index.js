d82fadd7e18b9588bc22c5f3a5f613f86a3b92c1
Shira1401 <shiraamar44@gmail.com>
2022-04-06 18:26:30 +0300

    start

--- Javascript/14-FinalProject/Shira - Yakir/dist/index.js
+++ Javascript/14-FinalProject/Shira - Yakir/dist/index.js
@@ -1,1 +1,40 @@
-var a = 2;
+var userName;
+var password;
+var formUserName = document.querySelector("#UserName");
+var formPassword = document.querySelector("#password");
+var formconfirmPassword = document.querySelector("#repeatPassword");
+var formSignUpMsg = document.querySelector("#signUpMsg");
+var formLoginMsg = document.querySelector("#loginMsg");
+function handleSignUp(ev) {
+    ev.preventDefault();
+    if (formPassword.value === formconfirmPassword.value) {
+        userName = formUserName.value;
+        password = formPassword.value;
+        var elements = ev.target.elements;
+        formSignUpMsg.innerHTML = "sign up success";
+    }
+    else {
+        alert("confirm password dosent match");
+    }
+}
+function handleLogin(ev) {
+    ev.preventDefault();
+    var LoginUserName = document.querySelector("#LoginUserName");
+    var LoginPassword = document.querySelector("#LoginPassword");
+    var htmlBody = document.querySelector("body");
+    var loginStatus = checkLogin(LoginUserName.value, LoginPassword.value);
+    if (loginStatus) {
+        htmlBody.style.background = "green";
+        formLoginMsg.innerHTML = "success Login";
+    }
+    else {
+        htmlBody.style.background = "red";
+        formLoginMsg.innerHTML = "Login Failed";
+    }
+}
+function checkLogin(check_username, check_password) {
+    if (check_username === userName && check_password === password)
+        return true;
+    else
+        return false;
+}
--- Javascript/14-FinalProject/Shira - Yakir/index.ts
+++ Javascript/14-FinalProject/Shira - Yakir/index.ts
@@ -1,1 +1,45 @@
-const a = 2;
+let userName;
+let password;
+const formUserName: HTMLInputElement = document.querySelector("#UserName");
+const formPassword: HTMLInputElement = document.querySelector("#password");
+const formconfirmPassword: HTMLInputElement = document.querySelector("#repeatPassword");
+const formSignUpMsg: HTMLElement = document.querySelector("#signUpMsg");
+const formLoginMsg: HTMLElement = document.querySelector("#loginMsg");
+
+function handleSignUp(ev) {
+    ev.preventDefault();
+    if (formPassword.value === formconfirmPassword.value) {
+        userName = formUserName.value;
+        password = formPassword.value;
+        const elements = ev.target.elements;
+        formSignUpMsg.innerHTML = `sign up success`;
+    } else {
+        alert("confirm password dosent match");
+    }
+}
+
+function handleLogin(ev) {
+    ev.preventDefault();
+    const LoginUserName: HTMLInputElement =
+        document.querySelector("#LoginUserName");
+    const LoginPassword: HTMLInputElement =
+        document.querySelector("#LoginPassword");
+    const htmlBody: HTMLElement = document.querySelector("body");
+
+    const loginStatus = checkLogin(LoginUserName.value, LoginPassword.value);
+    if (loginStatus) {
+        htmlBody.style.background = "green";
+        formLoginMsg.innerHTML = `success Login`;
+
+
+
+    } else {
+        htmlBody.style.background = "red";
+        formLoginMsg.innerHTML = `Login Failed`;
+    }
+}
+
+function checkLogin(check_username: String, check_password: string): Boolean {
+    if (check_username === userName && check_password === password) return true;
+    else return false;
+}
