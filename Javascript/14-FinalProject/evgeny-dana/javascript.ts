bed68ca270036fe36ab1d7358482702c43c1b378
Tal Yaron <tal.yaron@gmail.com>
2022-04-06 17:52:53 +0300

    cont

--- Javascript/14-FinalProject/evgeny-dana/javascript.ts
+++ Javascript/14-FinalProject/evgeny-dana/javascript.ts
@@ -1,63 +1,53 @@
+const uid = function () {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+};
 
-const server_wrapper = document.querySelector('.Server_wrapper')
+const products = [];
 
+const handleAddProduct = (ev: any) => {
+  const server_wrapper = document.querySelector(".Server_wrapper");
+  ev.preventDefault();
 
-function handleBtn(ev: any) {
-    ev.preventDefault();
+  const image = ev.target.elements.image.files[0];
+  const nameinput = ev.target.elements.name.value;
+  const priceinput = ev.target.elements.price.value;
 
-    const image = ev.target.elements.image.files[0]
-    const nameinput = ev.target.elements.name.value
-    const priceinput = ev.target.elements.price.value
+  console.log(image, nameinput, priceinput);
+  const id = uid();
 
-    console.log(image, nameinput, priceinput)
+  const item = { id, image, nameinput, priceinput };
+  products.push(item);
 
-    const item = { image, nameinput, priceinput }
-    console.log(item.image)
+  //   const imgSrc = URL.createObjectURL(item.image);
 
-    const imgSrc = URL.createObjectURL(item.image)
+  //   const display_item = document.createElement("div");
+  //   server_wrapper.appendChild(display_item);
+  //   display_item.setAttribute("class", "display_card");
+
+  //   const detail_list = document.createElement("div");
+  //   display_item.appendChild(detail_list);
+  //   detail_list.setAttribute("class", "detail_list");
+
+  //   const image_output = document.createElement("div");
+  //   display_item.appendChild(image_output);
+  //   image_output.setAttribute("id", "image_output");
+  //   image_output.innerHTML = `<img src="${imgSrc}" alt="" id="image-preview"> </div>`;
+  //   const imagePreview: HTMLImageElement =
+  //     document.querySelector("#image-preview");
+
+  //   imagePreview.src = imgSrc;
+
+  //   const name_output = document.createElement("div");
+  //   detail_list.appendChild(name_output);
+  //   name_output.setAttribute("id", "name_output");
+  //   name_output.innerHTML = `<div>item name:${nameinput}</div>`;
+
+  //   const price_output = document.createElement("div");
+  //   detail_list.appendChild(price_output);
+  //   price_output.setAttribute("id", "price_output");
+  //   price_output.innerHTML = `<div>item price:${priceinput}</div>`;
+};
+
+function renderProducts(){
     
-     const display_item = document.createElement('div')
-     server_wrapper.appendChild(display_item)
-     display_item.setAttribute('class', 'display_card')
-    
-    const detail_list = document.createElement('div')
-    display_item.appendChild(detail_list)
-    detail_list.setAttribute('class', 'detail_list')
-
-    const image_output = document.createElement('div')
-    display_item.appendChild(image_output)
-    image_output.setAttribute('id', 'image_output')
-    image_output.innerHTML = `<img src="${imgSrc}" alt="" id="image-preview"> </div>`
-    const imagePreview = document.querySelector('#image-preview')
-    imagePreview.src = imgSrc
-    
-    const name_output =document.createElement('div')
-    detail_list.appendChild(name_output)
-    name_output.setAttribute('id', 'name_output')
-    name_output.innerHTML = `<div>item name:${nameinput}</div>`
-    
-    const price_output = document.createElement('div')
-    detail_list.appendChild(price_output)
-    price_output.setAttribute('id', 'price_output')
-    price_output.innerHTML = `<div>item price:${priceinput}</div>`
-    
-    
-    
-    
-    
-    
-
-
-
-   
 }
-
-
-
-
-
-
-
-
-
-
