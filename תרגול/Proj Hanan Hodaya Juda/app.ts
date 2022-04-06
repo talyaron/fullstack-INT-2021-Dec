4fc23cd42b14012fc683cd6163125a3c74952b81
hanan <jigaart0@gmail.com>
2022-04-06 19:04:40 +0300

    dsa

--- תרגול/Proj Hanan Hodaya Juda/app.ts
+++ תרגול/Proj Hanan Hodaya Juda/app.ts
@@ -1,29 +1,32 @@
-const products = document.querySelector(`.products`)
+let shw = [
+ 
+    {
+     
+      type : "pargit",
+       price : 30,
+        img: ("תרגול\Proj Hanan Hodaya Juda\imgs\pargit.jpg"),
+    },
+    {
+      type : "ceves",
+      price : 35,
+       img : ("תרגול\Proj Hanan Hodaya Juda\imgs\ceves.jpg"),
+    },
+    {
+      type: "chips",
+      price: 15,
+       img: ("תרגול\Proj Hanan Hodaya Juda\imgs\fries.jpg"),
+    },
+    {
+      type: "anavim",
+      price: 30,
+       img: ("תרגול\Proj Hanan Hodaya Juda\imgs\grape juice the israelis.jpg"),
+    },
+];
 
+function rolrol(){
+ 
+   
+  
 
-function refreshPro(){
-    let menu = [
-        {
-        "type": "pargit",
-         "price":  30,
-          "img": new Image
-          img.src='imgs/pargit.jpg',
-        },
-        {
-        "type": "ceves",
-        "price": 35
-        "img": 
-        },
-         {
-          
-            
-
-         },
-      ]
-
-
-
-
-
-    console.log(`check one two three`)
 }
+
--- תרגול/Proj Hanan Hodaya Juda/dist/app.js
+++ תרגול/Proj Hanan Hodaya Juda/dist/app.js
@@ -1,19 +1,24 @@
-var products = document.querySelector(".products");
-function refreshPro() {
-    var menu = [
-        {
-            "type": "pargit",
-            "price": 30,
-            "img": new Image,
-            img: img,
-            : .src = 'imgs/pargit.jpg'
-        },
-        {
-            "type": "ceves",
-            "price": 35,
-            "img": 
-        },
-        {},
-    ];
-    console.log("check one two three");
+var shw = [
+    {
+        type: "pargit",
+        price: 30,
+        img: ("תרגול\Proj Hanan Hodaya Juda\imgs\pargit.jpg")
+    },
+    {
+        type: "ceves",
+        price: 35,
+        img: ("תרגול\Proj Hanan Hodaya Juda\imgs\ceves.jpg")
+    },
+    {
+        type: "chips",
+        price: 15,
+        img: ("תרגול\Proj Hanan Hodaya Juda\imgs\fries.jpg")
+    },
+    {
+        type: "anavim",
+        price: 30,
+        img: ("תרגול\Proj Hanan Hodaya Juda\imgs\grape juice the israelis.jpg")
+    },
+];
+function rolrol() {
 }
