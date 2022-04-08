6e5629f252a9a5668d042dfc5cf18826bda23b48
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-04-08 13:04:07 +0300

    innerHtml

--- Javascript/14-FinalProject/meir-hadas/dist/index.js
+++ Javascript/14-FinalProject/meir-hadas/dist/index.js
@@ -1,17 +1,20 @@
-var roots = document.querySelector('.root');
+var root = document.querySelector('.root');
 function handleNote(ev) {
     ev.preventDefault();
     var elements = ev.target.elements;
     var result = {};
     for (var i = 0; i < elements.length; i++) {
         if (elements[i].type === "text") {
-            result[elements[i].name] = elements[i].note;
+            result[elements[i].name] = elements[i].value;
         }
-        else if (elements[i].name && elements[i].valueAsString) {
-            result[elements[i].name] = elements[i].valueAsString;
+        else if (elements[i].name && elements[i].value) {
+            result[elements[i].name] = elements[i].value;
         }
         //   console.log(elements[i].name, elements[i].value)
     }
     console.log(result);
-    console.log(elements);
+    // console.log(elements)
+    // let html = result.map(el=>{
+    // })
+    root.innerHTML += "<div>" + result.note + "</div>";
 }
