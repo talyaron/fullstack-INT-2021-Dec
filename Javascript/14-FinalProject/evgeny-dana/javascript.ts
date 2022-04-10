1dcd8e1c0c61a03b3150ffce797752a0336f74c3
Evgeny <zakman336@gmail.com>
2022-04-10 20:00:39 +0300

    hw

--- Javascript/14-FinalProject/evgeny-dana/javascript.ts
+++ Javascript/14-FinalProject/evgeny-dana/javascript.ts
@@ -1,38 +1,42 @@
+
+//this gives a unique id
 const uid = function () {
   return Date.now().toString(36) + Math.random().toString(36).substr(2);
 };
-
+//this is an interface to catch bugs and rats
 interface Product {
   id: string;
   image: string;
   price: string;
   name: string;
-  imgSrc: string;
+  // imgSrc: string;
 }
-
+////////////////////////////////////////////////////////////////////////////////////
+//this is a temporary array for storing the new product cards
 const products: Array<Product> = [];
 
-
-
+//////////////////////////////////////////////////////////////////////////////////////
+// this handles the button for adding a product
 const handleAddProduct = (ev: any) => {
 
   ev.preventDefault();
 
-  const image = ev.target.elements.image.files[0];
+  const image = ev.target.elements.image.value;
   const name = ev.target.elements.name.value;
   const price = ev.target.elements.price.value;
 
   const id = uid();
-  const imgSrc = URL.createObjectURL(image);
+  // const imgSrc = URL.createObjectURL(image);
 
-  const item: Product = { id, image, imgSrc, name, price };
+  const item: Product = { id, image,  name, price };
   products.push(item);
 
   renderProducts();
   ev.target.reset()
 
 };
-
+////////////////////////////////////////////////////////////////////////////
+//this renders products 
 function renderProducts() {
   const root = document.querySelector("#root");
 
@@ -41,20 +45,20 @@ function renderProducts() {
     html +=
       `<div class="megaTest" >
       <div class="test" >
-            <img src=${product.imgSrc}>
+            <img src=${product.image}>
             <div class="test2" >
                   <div>name:${product.name}</div>
                   <div>Price: ${product.price}</div>
                   <button onclick="handleRemoveProduct('${product.id}')" class="btnDelete">delete</button>
-                  <button" class="btnUpdate">update</button>
+                  <button onclick="showUpdateMenu(event)" class="btnUpdate">update</button>
                   <p>in/out stock</p>
                   <input type="checkbox"> 
             </div>
         </div>
-        <form onsubmit="handleUpdateProduct(event, '${product.id}')">
+        <form class="updateProduct" onsubmit="handleUpdateProduct(event, '${product.id}')">
         <input type="text" name="productName" value="${product.name}">
-        <input type="file">
-        <input type="text" value="${product.price}">
+        <input type="text" name="newUrl" placeholder="NEW Item Image url">
+        <input type="text" name="NewPrice" value="${product.price}">
         <button type="submit">send</button>
     </form>
      </div>`
@@ -62,9 +66,9 @@ function renderProducts() {
 
   root.innerHTML = html;
 }
-
+////////////////////////////////////////////////////////////////////////
+// this removes the product card
 function handleRemoveProduct(productId: string) {
-  // products.indexOf()
   const index = products.findIndex(object => {
     return object.id === productId;
 
@@ -74,24 +78,40 @@ function handleRemoveProduct(productId: string) {
   console.log(products)
   renderProducts()
 }
-
-
+///////////////////////////////////////////////////////////////////////
+// this updates the product card
 function handleUpdateProduct(ev: any, productId: string) {
   ev.preventDefault();
-
   console.log(productId)
-
+  
   const index = products.findIndex(object => {
     return object.id === productId;
   });
-
-  console.log(index)
-
+  
+  
   if (index !== -1) {
     products[index].name = ev.target.elements.productName.value
   }
-
+  if (index !== -1) {
+    products[index].price = ev.target.elements.NewPrice.value
+  }
+  if (index !== -1) {
+    products[index].image = ev.target.elements.newUrl.value
+  }
   console.log(products)
-
+  
   renderProducts()
+}
+//////////////////////////////////////////////////////////////////////////////
+function showUpdateMenu(event){
+  
+  
+  const showUpdatePanal =document.querySelector(".updateProduct").style.display
+  if(showUpdatePanal == "none"){
+  
+    showUpdatePanal == "block"
+  }
+  
+  console.dir(showUpdatePanal)
+
 }
