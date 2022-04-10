37f369836f13b2f4725bd51d97d1b3269b26a2e4
hanan <jigaart0@gmail.com>
2022-04-10 20:42:31 +0300

    dsadsa

--- Javascript/14-FinalProject/Hodaya Juda Hanan/app.ts
+++ Javascript/14-FinalProject/Hodaya Juda Hanan/app.ts
@@ -1,34 +1,40 @@
 const foodMenu: Array<any> = [
-
+ 
   {
 
     type: "Pargit",
     price: 30,
     img: 'imgs/pargit.jpg',
     count: 0,
+    sub: 0,
   },
   {
     type: "Ceves",
     price: 35,
     img: 'imgs/ceves.jpg',
     count: 0,
+    sub: 0,
   },
   {
     type: "Chips",
     price: 15,
     img: 'imgs/fries.jpg',
     count: 0,
+    sub: 0,
   },
   {
     type: "Grapes",
     price: 10,
     img: 'imgs/grapeJuice.jpg',
     count: 0,
+    sub: 0,
   },
 ];
+
 const products: HTMLDivElement = document.querySelector(".products")
 
 const cart: HTMLDivElement = document.querySelector(".cart");
+let total:number = 0
 
 function refreshPro() {
 
@@ -51,22 +57,45 @@ function clickAdd(ev) {
   console.log(ev)
 
   foodMenu[ev.target.id].count += 1
+  foodMenu[ev.target.id].sub = foodMenu[ev.target.id].count*foodMenu[ev.target.id].price
   console.log(foodMenu)
   cart.innerText = ""
-order();
-
+  total=0
+ 
+  
+  totalSum()
+  order();
 }
+
 
 
 
 function order() {
 
-
   for (let i = 0; i < foodMenu.length; i++) {
-   cart.innerText += `\n ${foodMenu[i].type} : ${foodMenu[i].count}`
+   
+    if (foodMenu[i].count != 0) {
+      cart.innerText += ` ${foodMenu[i].type} : ${foodMenu[i].count}  ${foodMenu[i].price*foodMenu[i].count} ₪\n ${total} `
+        
+
+    }
+    else {
+
+    }
   }
 
 }
 
+
 order()
 
+function totalSum(){
+  for (let i = 0; i < foodMenu.length; i++) {
+  total += foodMenu[i].sub 
+  
+  
+  
+
+  }
+  
+}
--- Javascript/14-FinalProject/Hodaya Juda Hanan/dist/app.js
+++ Javascript/14-FinalProject/Hodaya Juda Hanan/dist/app.js
@@ -3,29 +3,34 @@ var foodMenu = [
         type: "Pargit",
         price: 30,
         img: 'imgs/pargit.jpg',
-        count: 0
+        count: 0,
+        sub: 0
     },
     {
         type: "Ceves",
         price: 35,
         img: 'imgs/ceves.jpg',
-        count: 0
+        count: 0,
+        sub: 0
     },
     {
         type: "Chips",
         price: 15,
         img: 'imgs/fries.jpg',
-        count: 0
+        count: 0,
+        sub: 0
     },
     {
         type: "Grapes",
         price: 10,
         img: 'imgs/grapeJuice.jpg',
-        count: 0
+        count: 0,
+        sub: 0
     },
 ];
 var products = document.querySelector(".products");
 var cart = document.querySelector(".cart");
+var total = 0;
 function refreshPro() {
     for (var i = 0; i < foodMenu.length; i++) {
         products.innerHTML += "\n    <div class=\"cube\">\n    " + foodMenu[i].type + "\n    <img src=\"" + foodMenu[i].img + "\" alt=\"\">\n    " + foodMenu[i].price + " \u20AA\n    <button id=\"" + i + "\" onclick=\"clickAdd(event)\">Panek Oti</button>\n    </div>\n\n    ";
@@ -34,13 +39,25 @@ function refreshPro() {
 function clickAdd(ev) {
     console.log(ev);
     foodMenu[ev.target.id].count += 1;
+    foodMenu[ev.target.id].sub = foodMenu[ev.target.id].count * foodMenu[ev.target.id].price;
     console.log(foodMenu);
     cart.innerText = "";
+    total = 0;
+    totalSum();
     order();
 }
 function order() {
     for (var i = 0; i < foodMenu.length; i++) {
-        cart.innerText += "\n " + foodMenu[i].type + " : " + foodMenu[i].count;
+        if (foodMenu[i].count != 0) {
+            cart.innerText += " " + foodMenu[i].type + " : " + foodMenu[i].count + "  " + foodMenu[i].price * foodMenu[i].count + " \u20AA\n " + total + " ";
+        }
+        else {
+        }
     }
 }
 order();
+function totalSum() {
+    for (var i = 0; i < foodMenu.length; i++) {
+        total += foodMenu[i].sub;
+    }
+}
