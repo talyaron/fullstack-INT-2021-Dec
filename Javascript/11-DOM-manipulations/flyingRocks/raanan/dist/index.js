971864a4c4952d7c4e871bb04ea6b7f5d16a31af
Raanan M <yraanan1@gmail.com>
2022-04-11 16:02:36 +0300

    a

--- Javascript/11-DOM-manipulations/flyingRocks/raanan/dist/index.js
+++ Javascript/11-DOM-manipulations/flyingRocks/raanan/dist/index.js
@@ -1,4 +1,6 @@
 var positions = {};
+var astroidWidth = 50;
+var astroidHieght = 50;
 window.onload = function () {
     setInterval(function () {
         moveAstroid('astroid1');
@@ -9,15 +11,51 @@ window.onload = function () {
         console.log(positions);
     }, 1000);
 };
+//positioning of astriod
 function moveAstroid(id) {
     var astroid = document.getElementById(id);
-    var x = getRandomNumber(1, 100);
-    var y = getRandomNumber(1, 100);
+    var x = getRandomPosition(1, 100);
+    var y = getRandomPosition(1, 100);
     positions[id + "x"] = x;
     positions[id + "y"] = y;
     astroid.style.top = x + "vh";
     astroid.style.left = y + "vw";
 }
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+function getRandomPosition(min, max) {
+    var position = 0;
+    position = Math.floor(Math.random() * (max - min + 1) + min);
+    return position;
+}
+//geting astroid's position points
+var leftP1 = 0;
+var topP1 = 0;
+var leftP2 = 0;
+var topP2 = 0;
+//creating a function that checks for collisions between all 4 points
+function checkCollisions(elem1, elem2) {
+    //left position calculations
+    var left1 = elem1.style.left;
+    left1 = left1.slice(0, left1.length - 2);
+    leftP1 = parseInt(left1);
+    var left2 = elem2.style.left;
+    left2 = left2.slice(0, left2.length - 2);
+    leftP2 = parseInt(left2);
+    //top position calculations
+    var top1 = elem1.style.top;
+    top1 = top1.slice(0, top1.length - 2);
+    topP1 = parseInt(top1);
+    var top2 = elem2.style.top;
+    top2 = top2.slice(0, top2.length - 2);
+    topP2 = parseInt(top2);
+    //right and bottom calculations
+    //right calculations
+    var leftRight1 = leftP1 + astroidWidth;
+    var leftRight2 = leftP2 + astroidWidth;
+    //bottom calculations
+    var bottomLeft1 = topP1 + astroidHieght;
+    var bottomLeft2 = topP2 + astroidHieght;
+    //if colision is enabled make elem disappear
+    if (leftP1 < leftRight2 && leftP1 > leftP2 && topP1 < bottomLeft2 && topP1 > topP2) {
+        console.log('colision');
+    }
 }
--- Javascript/11-DOM-manipulations/flyingRocks/raanan/index.ts
+++ Javascript/11-DOM-manipulations/flyingRocks/raanan/index.ts
@@ -1,4 +1,6 @@
 let positions = {}
+let astroidWidth = 50
+let astroidHieght = 50
 
 window.onload = () => {
     setInterval(() => {
@@ -12,18 +14,59 @@ window.onload = () => {
     }, 1000)
 }
 
-
+//positioning of astriod
 function moveAstroid(id: string) {
     let astroid = document.getElementById(id);
-    let x:number= getRandomNumber(1, 100);
-    let y:number= getRandomNumber(1, 100);
+    let x:number= getRandomPosition(1, 100);
+    let y:number= getRandomPosition(1, 100);
     positions[id+"x"] = x;
     positions[id+"y"] = y;
     astroid.style.top = `${x}vh`;
     astroid.style.left = `${y}vw`;
 }
 
+function getRandomPosition(min: number, max: number) {
+    let position:number =0;
+    position = Math.floor(Math.random() * (max - min + 1) + min)
+    return position;
+}
 
-function getRandomNumber(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+//geting astroid's position points
+let leftP1:number = 0;
+let topP1:number = 0;
+let leftP2:number = 0;
+let topP2:number = 0;
+
+//creating a function that checks for collisions between all 4 points
+function checkCollisions(elem1:number, elem2:number){
+    //left position calculations
+    let left1:string = elem1.style.left
+    left1 = left1.slice(0,left1.length-2)
+    leftP1=parseInt(left1)
+
+    let left2:string = elem2.style.left
+    left2 = left2.slice(0,left2.length-2)
+    leftP2=parseInt(left2)
+
+    //top position calculations
+    let top1:string = elem1.style.top
+    top1 = top1.slice(0,top1.length-2)
+    topP1=parseInt(top1)
+
+    let top2:string = elem2.style.top
+    top2 = top2.slice(0,top2.length-2)
+    topP2=parseInt(top2)
+
+    //right and bottom calculations
+    //right calculations
+    let leftRight1:any = leftP1 + astroidWidth
+    let leftRight2:any = leftP2 + astroidWidth
+    //bottom calculations
+    let bottomLeft1:any = topP1 + astroidHieght
+    let bottomLeft2:any = topP2 + astroidHieght
+
+    //if colision is enabled make elem disappear
+    if (leftP1<leftRight2 && leftP1>leftP2 && topP1<bottomLeft2 && topP1>topP2){
+        console.log('colision')
+    }
 }
