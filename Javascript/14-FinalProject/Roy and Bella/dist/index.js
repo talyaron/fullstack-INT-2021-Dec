6310ba7cb31f71275df23bf6a6ae923fb0f675b5
Bella-UI <bellabrailovsky@gmail.com>
2022-04-11 10:28:22 +0300

    11/04/22

--- Javascript/14-FinalProject/Roy and Bella/dist/index.js
+++ Javascript/14-FinalProject/Roy and Bella/dist/index.js
@@ -1,37 +1,25 @@
-var tasks = []; // Creating "Data Base" To Hold All the tasks.
-// Add new task form.
-function handOpenForm(event) {
+var tasks = [];
+// Add task form
+function handleAddTask(event) {
     event.preventDefault();
-    var heading = event.target.elements.heading.value;
-    var description = event.target.elements.description.value;
-    var startTime = event.target.elements.startTime.value;
-    var endTime = event.target.elements.endTime.value;
     var elements = event.target.elements;
     var result = {};
+    var heading = elements.heading.value;
+    var description = elements.description.value;
+    var setReminderDate = elements.setReminderDate.value;
     for (var i = 0; i < elements.length; i++) {
-        if (elements[i].type === "checkbox") {
-            result[elements[i].name] = elements[i].checked;
-        }
-        else if (elements[i].name && elements[i].value) {
+        if (elements[i].name && elements[i].value) {
             result[elements[i].name] = elements[i].value;
         }
-        // const task: any = result;
     }
-    // Creating new task div each time a task added from the form.
+    // Create new task
     var taskContainer = document.querySelector('.tasksContainer');
     var task = document.createElement('div');
-    // const newtask: object = Object.create(result);
-    // creating checkbox element
-    var checkbox = document.createElement('input');
-    // Assigning the attributes
-    // to created checkbox
-    checkbox.type = "checkbox";
-    task.append(checkbox);
     task.classList.add('task');
-    // Appending new tasks into the tasks container.
-    task.innerHTML = "<div class='heading'>" + heading + "</div>\n    <div class='description'>" + description + "</div>\n    <div class='startTime'>Scheduled to: " + startTime + "</div> \n   <div class='deletesvg'><img src=./images/delete.svg onclick=removeTask(event) ></div>\n   <input type='checkbox' class='checkbox'></input>";
+    // Append a new task to the tasks container
+    task.innerHTML = "<div class='heading'>" + heading + "</div>\n    <div class='description'>" + description + "</div>\n    <div class='setReminderDate'>Scheduled to: " + setReminderDate + "</div> \n   <div class='deletesvg'><img src=./images/delete.svg onclick=removeTask(event) ></div>";
     taskContainer.append(task);
-    // Pushing the task into a tasks array ("data base").
+    // Push a new task to the tasks array ("data base").
     tasks.push(result);
     console.log(tasks);
 }
@@ -45,62 +33,34 @@ function removeTask(event) {
     div.removeChild(array);
 }
 // Add new task (using 'handleTask' form).
-function handleAddTask(event) {
+function handleOpenForm(event) {
     event.preventDefault();
     var addBtn = document.querySelector('.addBtn');
     var slide = document.querySelector('.slide');
     var saveBtn = document.querySelector('.saveBtn');
     var cancelBtn = document.querySelector('.cancelBtn');
-    // Pressing the 'add' button.
-    addBtn.addEventListener('click', toggleSliderUp, false);
-    // Slide up
+    // Form slides up
+    addBtn.addEventListener('click', toggleSliderUp);
     function toggleSliderUp() {
         slide.classList.contains('slide-up');
         {
             slide.classList.add('slide-up');
         }
     }
-    // Pressing the 'save task' button.
-    saveBtn.addEventListener('click', toggleSliderDown, false);
-    // Slide down
+    saveBtn.addEventListener('click', toggleSliderDown);
+    // Form slides down
     function toggleSliderDown() {
         slide.classList.contains('slide');
         {
             slide.classList.remove('slide-up');
         }
     }
+    cancelBtn.addEventListener('click', handleCancelTask);
+    // Form submition canceled
+    function handleCancelTask() {
+        slide.classList.contains('slide');
+        {
+            slide.classList.remove('slide-up');
+        }
+    }
 }
-// Drafts -------------------------------------------------------------------------------------------------------------------------
-// const tasks: Array<object> = []; 
-// function handleTask(event: any) {
-//   event.preventDefault();
-//   const elements = event.target.elements;
-//   console.log(event.target.elements);
-//   console.log(elements.length);
-//   const result = {};
-//   for (let i = 0; i < elements.length; i++) {
-//     if (elements[i].type === "input") {
-//       result[elements[i].name] = elements[i].checked;
-//     } else if (elements[i].name && elements[i].value) {
-//       result[elements[i].name] = elements[i].value;
-//     }
-// const task: any = result;
-// tasks.push(task);
-// }
-// const task: any = result;
-// tasks.push(task);
-// console.log(tasks);
-// console.log(result);
-// let taskContainer: any = document.querySelector('.tasksContainer');
-// console.log(taskContainer);
-// for (const property in tasks) {
-//   console.log(tasks[property]);
-//   taskContainer.innerHTML = tasks[property];
-//   taskContainer.append(tasks);
-//   console.log(tasks);
-// }
-// }
-//   <div class='task'>
-//   <h3 class='title'>Task name</h3>
-//   <p class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit.quam autem est.</p>
-// </div>
--- Javascript/14-FinalProject/Roy and Bella/index.ts
+++ Javascript/14-FinalProject/Roy and Bella/index.ts
@@ -1,61 +1,39 @@
-const tasks: Array < object > = []; // Creating "Data Base" To Hold All the tasks.
 
+const tasks: Array < object > = [];
 
-
-// Add new task form.
-function handOpenForm(event: any) {
+// Add task form
+function handleAddTask(event: any) {
   event.preventDefault();
-  const heading: HTMLInputElement = event.target.elements.heading.value;
-  const description: HTMLInputElement = event.target.elements.description.value;
-  const startTime: HTMLInputElement = event.target.elements.startTime.value;
-  const endTime: HTMLInputElement = event.target.elements.endTime.value;
 
   const elements = event.target.elements;
-
   const result = {};
+  const heading: HTMLInputElement = elements.heading.value;
+  const description: HTMLInputElement =  elements.description.value;
+  const setReminderDate: HTMLInputElement =  elements.setReminderDate.value;
+
   for (let i = 0; i < elements.length; i++) {
-    if (elements[i].type === "checkbox") {
-      result[elements[i].name] = elements[i].checked;
-    } else if (elements[i].name && elements[i].value) {
+    if (elements[i].name && elements[i].value) {
       result[elements[i].name] = elements[i].value;
     }
-    // const task: any = result;
   }
 
-
-  // Creating new task div each time a task added from the form.
+  // Create new task
   const taskContainer: any = document.querySelector('.tasksContainer');
   let task = document.createElement('div');
-  // const newtask: object = Object.create(result);
-
-  // creating checkbox element
-  const checkbox = document.createElement('input'); 
-  
-  // Assigning the attributes
-  // to created checkbox
-  checkbox.type = "checkbox";
-
-
-
-  task.append(checkbox);
-
   task.classList.add('task');
 
-  // Appending new tasks into the tasks container.
+  // Append a new task to the tasks container
   task.innerHTML = `<div class='heading'>${heading}</div>
     <div class='description'>${description}</div>
-    <div class='startTime'>Scheduled to: ${startTime}</div> 
-   <div class='deletesvg'><img src=./images/delete.svg onclick=removeTask(event) ></div>
-   <input type='checkbox' class='checkbox'></input>`;
-
+    <div class='setReminderDate'>Scheduled to: ${setReminderDate}</div> 
+   <div class='deletesvg'><img src=./images/delete.svg onclick=removeTask(event) ></div>`;
   taskContainer.append(task);
 
-
-  // Pushing the task into a tasks array ("data base").
+  // Push a new task to the tasks array ("data base").
   tasks.push(result);
   console.log(tasks);
-}
 
+}
 
 // Remove tasks.
 function removeTask(event) {
@@ -68,93 +46,35 @@ function removeTask(event) {
   div.removeChild(array);
 }
 
-
 // Add new task (using 'handleTask' form).
-function handleAddTask(event) {
+function handleOpenForm(event) {
   event.preventDefault();
   const addBtn = document.querySelector('.addBtn');
   const slide = document.querySelector('.slide');
   const saveBtn = document.querySelector('.saveBtn');
   const cancelBtn = document.querySelector('.cancelBtn');
 
-  // Pressing the 'add' button.
-  addBtn.addEventListener('click', toggleSliderUp, false);
-  // Slide up
+  // Form slides up
+  addBtn.addEventListener('click', toggleSliderUp);
   function toggleSliderUp() {
     slide.classList.contains('slide-up'); {
       slide.classList.add('slide-up')
     }
   }
 
-  // Pressing the 'save task' button.
-  saveBtn.addEventListener('click', toggleSliderDown, false);
-  // Slide down
+  saveBtn.addEventListener('click', toggleSliderDown);
+  // Form slides down
   function toggleSliderDown() {
     slide.classList.contains('slide'); {
       slide.classList.remove('slide-up');
     }
   }
+
+  cancelBtn.addEventListener('click', handleCancelTask);
+  // Form submition canceled
+  function handleCancelTask() {
+    slide.classList.contains('slide'); {
+      slide.classList.remove('slide-up');
+    }
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Drafts -------------------------------------------------------------------------------------------------------------------------
-
-
-// const tasks: Array<object> = []; 
-
-// function handleTask(event: any) {
-//   event.preventDefault();
-
-//   const elements = event.target.elements;
-//   console.log(event.target.elements);
-//   console.log(elements.length);
-
-//   const result = {};
-//   for (let i = 0; i < elements.length; i++) {
-//     if (elements[i].type === "input") {
-//       result[elements[i].name] = elements[i].checked;
-//     } else if (elements[i].name && elements[i].value) {
-//       result[elements[i].name] = elements[i].value;
-//     }
-// const task: any = result;
-// tasks.push(task);
-// }
-// const task: any = result;
-// tasks.push(task);
-// console.log(tasks);
-// console.log(result);
-
-
-// let taskContainer: any = document.querySelector('.tasksContainer');
-// console.log(taskContainer);
-
-
-// for (const property in tasks) {
-//   console.log(tasks[property]);
-//   taskContainer.innerHTML = tasks[property];
-//   taskContainer.append(tasks);
-
-//   console.log(tasks);
-// }
-// }
-//   <div class='task'>
-//   <h3 class='title'>Task name</h3>
-//   <p class="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit.quam autem est.</p>
-// </div>
