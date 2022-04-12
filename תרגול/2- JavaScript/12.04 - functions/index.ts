342f74808d45a99a0f1dc6fc3fad06d729f41614
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-04-12 19:34:16 +0300

    index

---
+++ תרגול/2- JavaScript/12.04 - functions/index.ts
@@ -0,0 +1,17 @@
+// const arr = [
+//     { id: 1, name: "Dorit" },
+//     { id: 2, name: "Dana" },
+//     { id: 3, name: "Yevgeniy" },
+//     { id: 4, name: "Yehuda" },
+// ]
+
+// function getIndex(id) {
+//     const index = arr.findIndex(object => object.id === id);
+
+//     console.log(index)
+
+//     console.log(arr[index])
+// }
+
+// getIndex(3)
+
