d09ebdc9b11da7a02109a820f96cd5444e15e95c
JudaGolan <yehuda6993@gmail.com>
2022-04-12 16:14:01 +0300

    juda hodaya hanan update

--- Javascript/14-FinalProject/Hodaya Juda Hanan/app.ts
+++ Javascript/14-FinalProject/Hodaya Juda Hanan/app.ts
@@ -1,40 +1,42 @@
 const foodMenu: Array<any> = [
- 
+
   {
 
     type: "Pargit",
     price: 30,
     img: 'imgs/pargit.jpg',
     count: 0,
-    sub: 0,
+
   },
   {
     type: "Ceves",
     price: 35,
     img: 'imgs/ceves.jpg',
     count: 0,
-    sub: 0,
+
   },
   {
     type: "Chips",
     price: 15,
     img: 'imgs/fries.jpg',
     count: 0,
-    sub: 0,
+
   },
   {
     type: "Grapes",
     price: 10,
     img: 'imgs/grapeJuice.jpg',
     count: 0,
-    sub: 0,
+
   },
 ];
 
 const products: HTMLDivElement = document.querySelector(".products")
 
 const cart: HTMLDivElement = document.querySelector(".cart");
-let total:number = 0
+const totalDiv: HTMLDivElement = document.querySelector(".totalDiv")
+
+let totalPrice: number = 0;
 
 function refreshPro() {
 
@@ -57,14 +59,13 @@ function clickAdd(ev) {
   console.log(ev)
 
   foodMenu[ev.target.id].count += 1
-  foodMenu[ev.target.id].sub = foodMenu[ev.target.id].count*foodMenu[ev.target.id].price
+  foodMenu[ev.target.id].sub = foodMenu[ev.target.id].count * foodMenu[ev.target.id].price
   console.log(foodMenu)
   cart.innerText = ""
-  total=0
- 
-  
-  totalSum()
+  totalPrice = 0;
   order();
+  totalSum();
+
 }
 
 
@@ -73,10 +74,10 @@ function clickAdd(ev) {
 function order() {
 
   for (let i = 0; i < foodMenu.length; i++) {
-   
+
     if (foodMenu[i].count != 0) {
-      cart.innerText += ` ${foodMenu[i].type} : ${foodMenu[i].count}  ${foodMenu[i].price*foodMenu[i].count} ₪\n ${total} `
-        
+      cart.innerHTML += 
+       `<div class="orderLine">${foodMenu[i].type} : ${foodMenu[i].price}₪  <div class="circleCount">${foodMenu[i].count}</div> </div>`
 
     }
     else {
@@ -87,15 +88,9 @@ function order() {
 }
 
 
-order()
-
-function totalSum(){
+function totalSum() {
   for (let i = 0; i < foodMenu.length; i++) {
-  total += foodMenu[i].sub 
-  
-  
-  
-
+    totalPrice += foodMenu[i].price * foodMenu[i].count
   }
-  
+  totalDiv.innerText = `Total : ${totalPrice}₪`
 }
--- Javascript/14-FinalProject/Hodaya Juda Hanan/dist/app.js
+++ Javascript/14-FinalProject/Hodaya Juda Hanan/dist/app.js
@@ -3,34 +3,31 @@ var foodMenu = [
         type: "Pargit",
         price: 30,
         img: 'imgs/pargit.jpg',
-        count: 0,
-        sub: 0
+        count: 0
     },
     {
         type: "Ceves",
         price: 35,
         img: 'imgs/ceves.jpg',
-        count: 0,
-        sub: 0
+        count: 0
     },
     {
         type: "Chips",
         price: 15,
         img: 'imgs/fries.jpg',
-        count: 0,
-        sub: 0
+        count: 0
     },
     {
         type: "Grapes",
         price: 10,
         img: 'imgs/grapeJuice.jpg',
-        count: 0,
-        sub: 0
+        count: 0
     },
 ];
 var products = document.querySelector(".products");
 var cart = document.querySelector(".cart");
-var total = 0;
+var totalDiv = document.querySelector(".totalDiv");
+var totalPrice = 0;
 function refreshPro() {
     for (var i = 0; i < foodMenu.length; i++) {
         products.innerHTML += "\n    <div class=\"cube\">\n    " + foodMenu[i].type + "\n    <img src=\"" + foodMenu[i].img + "\" alt=\"\">\n    " + foodMenu[i].price + " \u20AA\n    <button id=\"" + i + "\" onclick=\"clickAdd(event)\">Panek Oti</button>\n    </div>\n\n    ";
@@ -42,22 +39,23 @@ function clickAdd(ev) {
     foodMenu[ev.target.id].sub = foodMenu[ev.target.id].count * foodMenu[ev.target.id].price;
     console.log(foodMenu);
     cart.innerText = "";
-    total = 0;
-    totalSum();
+    totalPrice = 0;
     order();
+    totalSum();
 }
 function order() {
     for (var i = 0; i < foodMenu.length; i++) {
         if (foodMenu[i].count != 0) {
-            cart.innerText += " " + foodMenu[i].type + " : " + foodMenu[i].count + "  " + foodMenu[i].price * foodMenu[i].count + " \u20AA\n " + total + " ";
+            cart.innerHTML +=
+                "<div class=\"orderLine\">" + foodMenu[i].type + " : " + foodMenu[i].price + "\u20AA  <div class=\"circleCount\">" + foodMenu[i].count + "</div> </div>";
         }
         else {
         }
     }
 }
-order();
 function totalSum() {
     for (var i = 0; i < foodMenu.length; i++) {
-        total += foodMenu[i].sub;
+        totalPrice += foodMenu[i].price * foodMenu[i].count;
     }
+    totalDiv.innerText = "Total : " + totalPrice + "\u20AA";
 }
