e9347d4d99202e5b043a3b7a7e9b3194294d31f9
adamepel <adamepel11@gmail.com>
2022-04-13 18:47:34 +0300

    finish!

--- Javascript/14-FinalProject/Adam&Hadeer/dist/index.js
+++ Javascript/14-FinalProject/Adam&Hadeer/dist/index.js
@@ -13,11 +13,12 @@ calculatorButtons.forEach(function (button) {
     addEventListener("click", function (ev) {
         ev.stopPropagation();
         console.log(ev.target.type);
-        if (ev.target.className !== 'wrapper' && ev.target.className !== 'calculatorButtons' && ev.target.nodeName !== 'BODY') {
+        if (ev.target.className !== 'wrapper' && ev.target.className !== 'calculatorButtons' && ev.target.nodeName !== 'BODY' && ev.target.nodeName !== 'HTML' && ev.target.id !== 'display') {
             console.log("clicked"); //Any button that been clicked pop a `clicked` messege
             console.log(ev);
             console.dir(ev.target);
             console.log(ev.target.type);
+            console.dir(display);
             switch (ev.target.innerText) {
                 case 'AC':
                     display.innerText = '';
@@ -34,10 +35,16 @@ calculatorButtons.forEach(function (button) {
                 case '=':
                     try {
                         display.innerText = eval(display.innerText);
+                        if (display.innerText === 'undefined') {
+                            display.innerText = '';
+                            alert('NAN');
+                        }
+                        console.dir(display);
                         break;
                     }
                     catch (_a) {
                         display.innerText = '';
+                        alert('NAN');
                         break;
                     }
                 default:
