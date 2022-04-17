c537b1cafa70b6fcaad663bc92310af5b410e7d2
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-04-17 15:54:57 +0300

    notes go on

--- Javascript/14-FinalProject/meir-hadas/dist/index.js
+++ Javascript/14-FinalProject/meir-hadas/dist/index.js
@@ -1,17 +1,56 @@
-var root = document.querySelector('.root');
-////Functions
-function handleNote(ev) {
-    ev.preventDefault();
-    var elements = ev.target.elements;
-    var result = {};
-    for (var i = 0; i < elements.length; i++) {
-        if (elements[i].type === "text") {
-            result[elements[i].name] = elements[i].value;
-        }
-        else if (elements[i].name && elements[i].value) {
-            result[elements[i].name] = elements[i].value;
-        }
-    }
-    console.log(result);
-    root.innerHTML += "<div>" + result.note + "</div>";
+////// const root = document.querySelector('.root')
+// ////Functions
+////// function handleNote(ev: any) {
+//////   ev.preventDefault();
+//////   const elements = ev.target.elements;
+//////   const result = {};
+//////   for (let i = 0; i < elements.length; i++) {
+///////       if (elements[i].type === "text") {
+///////        result[elements[i].name] = elements[i].value;
+///////       } else if (elements[i].name && elements[i].value) {
+///////       result[elements[i].name] = elements[i].value;
+//////   }
+////// }
+//////   console.log(result);
+//////   root.innerHTML += `<div>${result.note}</div>`
+////// }
+// Form reference
+var form = {};
+form.noteText = document.querySelector('#formNoteText');
+form.addButton = document.querySelector('#formAddButton');
+form.color = document.querySelector('#formColor');
+var notes = document.querySelector('#notes');
+form.noteText.focus();
+// Functions
+function addNote() {
+    var text = form.noteText.value;
+    var note = document.createElement('div');
+    var deleteButton = document.createElement('span');
+    note.classList.add('note');
+    note.classList.add(form.color.value);
+    note.innerHTML = "<div class='note-text'>" + text + "</div>";
+    deleteButton.classList.add('note-delete');
+    deleteButton.innerHTML = '&times;';
+    note.appendChild(deleteButton);
+    notes.appendChild(note);
+    form.noteText.value = '';
+    form.noteText.focus();
+    addListenerDeleteButton(deleteButton);
 }
+function addListenerDeleteButton(deleteButton) {
+    deleteButton.addEventListener('click', function (e) {
+        e.stopPropagation();
+        deleteNote(e);
+    });
+}
+function deleteNote(e) {
+    var eventNote = e.target.parentNode;
+    eventNote.parentNode.removeChild(eventNote);
+}
+// Event 
+form.addButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (form.noteText.value != '') {
+        addNote();
+    }
+});
--- Javascript/14-FinalProject/meir-hadas/index.ts
+++ Javascript/14-FinalProject/meir-hadas/index.ts
@@ -1,23 +1,78 @@
-const root = document.querySelector('.root')
+////// const root = document.querySelector('.root')
 
-////Functions
+// ////Functions
 
-function handleNote(ev: any) {
-  ev.preventDefault();
+////// function handleNote(ev: any) {
+//////   ev.preventDefault();
 
 
-  const elements = ev.target.elements;
-  const result = {};
-  for (let i = 0; i < elements.length; i++) {
-      if (elements[i].type === "text") {
-       result[elements[i].name] = elements[i].value;
-      } else if (elements[i].name && elements[i].value) {
-      result[elements[i].name] = elements[i].value;
-  }
+//////   const elements = ev.target.elements;
+//////   const result = {};
+//////   for (let i = 0; i < elements.length; i++) {
+///////       if (elements[i].type === "text") {
+///////        result[elements[i].name] = elements[i].value;
+///////       } else if (elements[i].name && elements[i].value) {
+///////       result[elements[i].name] = elements[i].value;
+//////   }
     
-}
-  console.log(result);
+////// }
+//////   console.log(result);
   
 
-  root.innerHTML += `<div>${result.note}</div>`
+//////   root.innerHTML += `<div>${result.note}</div>`
+////// }
+
+
+// Form reference
+const form = {}
+form.noteText = document.querySelector('#formNoteText');
+form.addButton = document.querySelector('#formAddButton');
+form.color = document.querySelector('#formColor');
+
+const notes = document.querySelector('#notes');
+
+form.noteText.focus();
+
+// Functions
+function addNote() {
+  let text = form.noteText.value;
+  let note = document.createElement('div');
+  let deleteButton = document.createElement('span');
+
+  note.classList.add('note');
+  note.classList.add(form.color.value);
+  note.innerHTML = `<div class='note-text'>${text}</div>`;
+  deleteButton.classList.add('note-delete');
+  deleteButton.innerHTML = '&times;';
+
+  note.appendChild(deleteButton);  
+  notes.appendChild(note);
+
+  form.noteText.value = '';
+  form.noteText.focus();
+
+  addListenerDeleteButton(deleteButton);
 }
+
+function addListenerDeleteButton(deleteButton) {
+  deleteButton.addEventListener('click', function (e) {
+    e.stopPropagation();      
+    deleteNote(e);
+  });
+}
+
+function deleteNote(e) {
+  let eventNote = e.target.parentNode;
+  eventNote.parentNode.removeChild(eventNote);
+}
+
+
+
+// Event 
+form.addButton.addEventListener('click', function (e) {
+  e.preventDefault();  
+  if (form.noteText.value != '') {
+    addNote();
+  }
+})
+
