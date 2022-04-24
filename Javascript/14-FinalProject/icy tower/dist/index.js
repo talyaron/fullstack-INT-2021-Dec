01a2d2b5ff0a24485b4036d3492fc3ed159edeee
carmacookies <carmacookies@gmail.com>
2022-04-24 17:36:32 +0300

    carmel bar

--- Javascript/14-FinalProject/icy tower/dist/index.js
+++ Javascript/14-FinalProject/icy tower/dist/index.js
@@ -1,6 +1,8 @@
 var body = document.querySelector('body');
 var rapper = document.querySelector('.rapper');
 var player = document.querySelector('.player');
+var floor = document.querySelector('.floor');
+var allSteps = [];
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
@@ -13,46 +15,73 @@ function createStep() {
     if (leftPlace > 60) {
         leftPlace = leftPlace - stepWidth;
     }
-    if (stepWidth < 20) {
-        var steptwo_1 = document.createElement('div');
-        steptwo_1.classList.add('step');
-        var stepWidthTwo = getRandomArbitrary(10, 20);
-        steptwo_1.style.width = stepWidthTwo + "vw";
-        var leftPlaceTwo = getRandomArbitrary(1, 100);
-        if (leftPlace > 50) {
-            leftPlaceTwo = leftPlace - 48;
-        }
-        else {
-            leftPlaceTwo = leftPlace + 48;
-        }
-        steptwo_1.style.left = leftPlaceTwo + "vw";
-        steptwo_1.style.transition = "all 50s";
-        rapper.appendChild(steptwo_1);
-        console.log(steptwo_1);
-        var _loop_1 = function (i) {
-            setTimeout(function () {
-                steptwo_1.style.top = i * 5 + "vh";
-            }, 2000);
-        };
-        for (var i = 0; i < 200; i++) {
-            _loop_1(i);
-        }
-    }
+    // if(stepWidth <20){
+    //     const steptwo = document.createElement('div')
+    //     steptwo.classList.add('step') 
+    //     const stepWidthTwo = getRandomArbitrary(10, 20)
+    //     steptwo.style.width = `${stepWidthTwo}vw`
+    //     let leftPlaceTwo = getRandomArbitrary(1,100)
+    //     if(leftPlace > 50)
+    //     {
+    //         leftPlaceTwo = leftPlace - 48
+    //     }
+    //     else{
+    //         leftPlaceTwo = leftPlace + 48
+    //     }
+    //     steptwo.style.left = `${leftPlaceTwo}vw`
+    //     allSteps.push(steptwo)
+    //     steptwo.style.transition = `all 60s`
+    //     rapper.appendChild(steptwo)
+    //     for (let i=0; i<200; i++){
+    //         setTimeout(() => {
+    //             steptwo.style.top = `${i*5}vh`
+    //         }, 2000)
+    //     }
+    // }
     step.style.left = leftPlace + "vw";
-    step.style.transition = "all 50s";
+    allSteps.push(step);
+    step.style.transition = "all 60s";
     rapper.appendChild(step);
-    var _loop_2 = function (i) {
+    var _loop_1 = function (i) {
         setTimeout(function () {
             step.style.top = i * 5 + "vh";
         }, 2000);
     };
     for (var i = 0; i < 200; i++) {
-        _loop_2(i);
+        _loop_1(i);
     }
 }
 function handleClick(ev) {
+    player.style.transition = "all 800ms";
     player.style.left = ev.x + "px";
     player.style.top = ev.y + "px";
+    var loc = player.getBoundingClientRect();
+    setTimeout(function () {
+        for (var i = 0; i < 150; i++) {
+            player.style.transition = "all 25s";
+            player.style.top = loc.top - 2 + "vh";
+        }
+    }, 500);
 }
 rapper.addEventListener("click", handleClick);
-setInterval(createStep, 2000);
+function checkForHeadBump() {
+    player.style.transition = "all 800ms";
+    var loc = player.getBoundingClientRect();
+    for (var i = 0; i < allSteps.length; i++) {
+        var stepHeight = allSteps[i].getBoundingClientRect();
+        var floorPlace = floor.getBoundingClientRect();
+        if (stepHeight.top + 50 > floorPlace.top) {
+            allSteps.shift();
+        }
+        if (stepHeight.top + 50 > loc.top) {
+            if (((stepHeight.left <= loc.left) && (loc.left <= stepHeight.right)) ||
+                ((stepHeight.left - 70 <= loc.left) && (loc.left <= stepHeight.right))) {
+                for (var i_1 = 0; i_1 < 150; i_1++) {
+                    player.style.top = loc.top - 2 + "vh";
+                }
+            }
+        }
+    }
+}
+setInterval(createStep, 3000);
+setInterval(checkForHeadBump, 200);
