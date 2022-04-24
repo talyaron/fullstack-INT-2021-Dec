17d2a0e4ff39f0dbf6236cabf78724e189df34f8
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-04-24 18:55:44 +0300

    g

---
+++ Node/01-basic/Folowup/gal/dist/server.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log('hi');
+
+function multi(a, b) {
+  return Math.sqrt(a * b);
+}
+
+var result = multi(3, 5);
+console.log(result);
---
+++ Node/01-basic/Folowup/gal/server.js
@@ -0,0 +1,8 @@
+console.log('hi');
+
+function multi(a,b){
+    return Math.sqrt(a*b)
+}
+
+let result= multi (3,5);
+console.log(result);
