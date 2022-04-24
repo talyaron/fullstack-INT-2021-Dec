60b4a83c7d8bda66c030d5efba25cdf73cba343e
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-04-24 19:57:10 +0300

    e

--- Node/01-basic/Folowup/gal/server.js
+++ Node/01-basic/Folowup/gal/server.js
@@ -1,3 +1,15 @@
+var express = require('express')
+var app = express()
+const port = 3000;
+
+app.get('/', function (req, res) => {
+    res.send('OK')
+  });
+  
+  app.listen(port, function () => {
+    console.log('Ready')
+  });
+
 console.log('hi');
 
 function multi(a,b){
@@ -6,3 +18,6 @@ function multi(a,b){
 
 let result= multi (3,5);
 console.log(result);
+
+
+const 
