7643e69a290afd6ec69c5986926ddf90a98c3ea5
Rowy92 <roeyshpringer@gmail.com>
2022-04-25 18:46:28 +0300

    roey shp - fp

--- Javascript/14-FinalProject/Roey Shpringer/index.ts
+++ Javascript/14-FinalProject/Roey Shpringer/index.ts
@@ -10,33 +10,52 @@ const neon = document.querySelector(".agentNeon")
 
 const sage = document.querySelector(".agentSage")
 
-const wrapper = document.querySelector(".profileWrapper")
+const profileWrapper = document.querySelector(".profileWrapper")
 
 const agentName = document.querySelector(".agentName")
 
 const agentImg = document.querySelector(".agentImg")
+
+const gameWrapper = document.querySelector(".gameWrapper")
+
+const line = document.querySelector(".line")
+
+const startButton = document.querySelector(".startButton")
 
 
 function handleClick(ev) {
     if (ev.target.className == 'agentJett') {
         ul.remove()
         chooseUrAgent.remove()
-        wrapper.style.display = 'block'
+        profileWrapper.style.display = 'block'
+        gameWrapper.style.display = 'block'
         agentName.innerHTML = 'Agent name: Jett'
         agentImg.src = 'images/jettProfile.png'
     } else if (ev.target.className == 'agentNeon') {
         ul.remove()
         chooseUrAgent.remove()
-        wrapper.style.display = 'block'
+        profileWrapper.style.display = 'block'
+        gameWrapper.style.display = 'block'
         agentName.innerHTML = 'Agent name: Neon'
         agentImg.src = 'images/neonProfile.png'
     } else if (ev.target.className == 'agentSage') {
         ul.remove()
         chooseUrAgent.remove()
-        wrapper.style.display = 'block'
+        profileWrapper.style.display = 'block'
+        gameWrapper.style.display = 'block'
         agentName.innerHTML = 'Agent name: Sage'
         agentImg.src = 'images/sageProfile.png'
     }
     console.log(ev.target.value)
+}
+
+function handleStart(ev) {
+    startButton.innerHTML = `<div class ="pauseButton"><button onclick="handlePause(event)">Pause</button></div>`
+    console.log(ev);
+    let linePosition = line.getBoundingClientRect()
+    console.log(linePosition)
+    if (ev.isTrusted = true) {
+        line.style.left = `99.30%`
+    }
 }
 
