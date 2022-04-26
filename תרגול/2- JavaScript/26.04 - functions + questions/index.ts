82962fedc1327f4c3d305c171746eb8b304b6efd
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-04-26 20:01:38 +0300

    main

--- תרגול/2- JavaScript/26.04 - functions + questions/index.ts
+++ תרגול/2- JavaScript/26.04 - functions + questions/index.ts
@@ -39,3 +39,83 @@
 // console.log(RGB[30]) //GREEN
 
 ///////// 6 /////////
+// "use strict"
+
+//Defines that JavaScript code should be executed in "strict mode".With strict mode, you can not, for example, use undeclared variables.
+//You can use strict mode in all your programs. It helps you to write cleaner code.
+// x = 30;
+// console.log(x)
+
+////////// 7 ////////
+// console.log(undefined == false) //false
+// console.log(null == undefined) //true
+// console.log(null == false) //false
+
+//null = nothing
+//undefined = לא נמצא
+// false = not true
+
+///////// 8 ////////
+// console.log("This is the outer level");
+// console.group();
+// console.log("Level 2");
+// console.group();
+// console.log("Level 3");
+// console.warn("More of level 3");
+// console.groupEnd();
+// console.log("Back to level 2");
+// console.groupEnd();
+// console.log("Back to the outer level");
+
+// const arr11 = [
+//     { ok: true, name: "gal" },
+//     { ok: true, name: "gal" },
+//     { ok: true, name: "gal" },
+//     { ok: true, name: "gal" },
+//     { ok: true, name: "gal" },
+// ]
+
+// console.log(arr11)
+// console.table(arr11)
+
+///////9//////
+//4. variable value exchange
+// let fire = '1';
+// let friut = '2';
+
+// [friut, fire] = [fire, friut];
+// console.log(fire, friut)
+
+////////10////////
+// function foo1() {
+//     return {
+//         bar: "hello"
+//     };
+// }
+// console.log(foo1())
+
+// function foo2() {
+//     return
+//     {
+//         bar: "hello"
+//     };
+// }
+
+// console.log(foo2())
+
+///////11////////
+// alert('hello')
+// const hi = +prompt()
+// console.log(hi)
+
+// const confirmClient = confirm('meir')
+// console.log(confirmClient)
+
+// if (confirmClient == true) {
+//     console.log('gal haya po')
+// }
+
+// CSS
+// http://www.layerstyles.org/builder.html
+// https://codepen.io/Anna_Batura/pen/dypeoxj
+
