cbf1c3c01e92c8c20b06afbab518ef85af8e9d8d
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-04-26 18:45:14 +0300

    question

---
+++ תרגול/2- JavaScript/26.04 - functions + questions/index.ts
@@ -0,0 +1,41 @@
+////////// 1 //////////
+// console.log(1 + "2" + "2"); //122 // 122
+// console.log(1 + +"2" + "2"); //122 //1+22 //32
+// console.log(1 + -"1" + "2"); //1-12 //02
+// console.log(+"1" + "1" + "2"); //112 //112
+// console.log("A" - "B" + "2"); //AB2 //NaN = not a number = NaN2
+// console.log("A" - "B" + 2); //AB+2 //NaN
+
+// // +prompt('num')
+
+/////////// 2 /////////
+// console.log(false == '0')  //true
+// console.log(false === '0')  //false
+
+////////// 3 /////////
+
+// console.log(typeof 4.55.toFixed(1)) //we get string
+// console.log(4.55.toFixed(1) + 1) //string + number - give string
+
+
+///////////4 //////////
+// const arr = [1, 3, , 7]
+
+// arr.forEach((element) => {
+//     console.log(element)
+// }) //1,3,7
+
+///////// 5 //////////
+// enum RGB {
+//     YELLOW,
+//     GREY,
+//     Maam = 17,
+//     RED = 30,
+//     GREEN = 60,
+//     BLUE
+// }
+
+// console.log(RGB.Maam) //GREEN
+// console.log(RGB[30]) //GREEN
+
+///////// 6 /////////
