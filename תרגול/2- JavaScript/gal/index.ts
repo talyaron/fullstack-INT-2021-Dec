cb3a1a50bd5e0bcb02264973d9b59aad936d118e
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-04-26 19:09:14 +0300

    ds

---
+++ תרגול/2- JavaScript/gal/index.ts
@@ -0,0 +1,21 @@
+function daysOfMonth(nYear, nMonth) {
+    switch (nMonth) {
+        case 0||2||4||6||7||9||11: //january
+            return 31; break;
+            
+        case 1: //february
+            if ((nYear % 4) == 0  && (0 != nYear % 100) || (0 == nYear % 400))  {
+                return 29;
+            }
+            else {
+                return 28;
+            };
+            break;
+            
+        case 3||5||8||10: //april
+            return 30; break;
+        default: //months
+         return console.log(daysOfMonth);break;   
+    }
+}
+
