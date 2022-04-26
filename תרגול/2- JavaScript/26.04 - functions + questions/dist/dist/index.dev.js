224a15c707a830f3a44b18f259358c86cb2936c7
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-04-26 20:03:36 +0300

    ques

---
+++ תרגול/2- JavaScript/26.04 - functions + questions/dist/dist/index.dev.js
@@ -0,0 +1,13 @@
+////////// 1 //////////
+// console.log(1 + "2" + "2"); //122 // 122
+// console.log(1 + +"2" + "2"); //122 //1+22 //32
+// console.log(1 + -"1" + "2"); //1-12 //02
+// console.log(+"1" + "1" + "2"); //112 //112
+// console.log("A" - "B" + "2"); //AB2 //NaN = not a number = NaN2
+// console.log("A" - "B" + 2); //AB+2 //NaN
+// // +prompt('num')
+/////////// 2 /////////
+// console.log(false == '0')  //true
+// console.log(false === '0')  //false
+////////// 3 /////////
+"use strict";
