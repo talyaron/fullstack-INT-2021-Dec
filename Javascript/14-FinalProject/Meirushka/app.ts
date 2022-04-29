bf5bb1616bb4d7bf72457b6f34c966d61e5cb54f
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-04-29 10:24:29 +0300

    matjil

--- Javascript/14-FinalProject/Meirushka/app.ts
+++ Javascript/14-FinalProject/Meirushka/app.ts
@@ -1,67 +1,39 @@
-// selectors
-const root = document.querySelector('.root');
-// const note = document.querySelector('.note')
-// Functions
 
-function handleNote(ev: any) {
-  ev.preventDefault();
-  const elements = ev.target.elements;
-  const result = {};
-  for (let i = 0; i < elements.length; i++) {
-    if (elements[i].type === "text") {
-      result[elements[i].name] = elements[i].value;
-      }   else if (elements[i].name && elements[i].value) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // selectors
+// const root = document.querySelector('.root');
+// // const note = document.querySelector('.note')
+// // Functions
+
+// function handleNote(ev: any) {
+//   ev.preventDefault();
+//   const elements = ev.target.elements;
+//   const result = {};
+//   for (let i = 0; i < elements.length; i++) {
+//     if (elements[i].type === "text") {
+//       result[elements[i].name] = elements[i].value;
+//       }   else if (elements[i].name && elements[i].value) {
       
-      result[elements[i].name] = elements[i].value;
-    } 
-  }
+//       result[elements[i].name] = elements[i].value;
+//     } 
+//   }
     
   
-  console.log(result);
-  root.innerHTML += `<div>${result.note}</div>`
-}  
+//   console.log(result);
+//   root.innerHTML += `<div>${result.note}</div>`
+// }  
 
-let todosArr: Array<any> = []
-
-const todoList:HTMLElement = document.querySelector('#todolist');
-const noOfTodos:HTMLElement = document.querySelector('#noOfTodos');
-
-const createTodo = (arr) => {
-  
-  todoList.innerHTML = "";
-  
-  arr.forEach((todo, id) => {
-    let list = document.createElement('li');
-    let spanWithValue = document.createElement('span');
-    let spanWithBtn = document.createElement('button')
-    spanWithValue.innerText = todo.value;
-    spanWithBtn.innerText = "X";
-    spanWithBtn.style.color = 'red';
-    
-    
-  
-  
-    spanWithBtn.addEventlistener("click", () => {
-      deleteTodo(id);
-    });
-    
-    list.appendChild(spanWithValue);
-    list.appendChild(spanWithBtn);
-  
-    todoList.appendChild(list);
-    
-    
-  }); 
-  
-  #noOfTodos.innerText = todosArr.length;
-  
-};
-
-cosnt deleteTodo = (index) =>{
-  todosArr.splice(index, 1);
-  createTodo(todosArr);
-};
-
-window.addEventListener ("load", () => {
-  createTodo(todosArr)
-});
--- Javascript/14-FinalProject/Meirushka/dist/app.js
+++ Javascript/14-FinalProject/Meirushka/dist/app.js
@@ -1,48 +1,18 @@
-// selectors
-var root = document.querySelector('.root');
-// const note = document.querySelector('.note')
-// Functions
-function handleNote(ev) {
-    ev.preventDefault();
-    var elements = ev.target.elements;
-    var result = {};
-    for (var i = 0; i < elements.length; i++) {
-        if (elements[i].type === "text") {
-            result[elements[i].name] = elements[i].value;
-        }
-        else if (elements[i].name && elements[i].value) {
-            result[elements[i].name] = elements[i].value;
-        }
-    }
-    console.log(result);
-    root.innerHTML += "<div>" + result.note + "</div>";
-}
-var todosArr = [];
-var todoList = document.querySelector('#todolist');
-var noOfTodos = document.querySelector('#noOfTodos');
-var createTodo = function (arr) {
-    todoList.innerHTML = "";
-    arr.forEach(function (todo, id) {
-        var list = document.createElement('li');
-        var spanWithValue = document.createElement('span');
-        var spanWithBtn = document.createElement('button');
-        spanWithValue.innerText = todo.value;
-        spanWithBtn.innerText = "X";
-        spanWithBtn.style.color = 'red';
-        spanWithBtn.addEventlistener("click", function () {
-            deleteTodo(id);
-        });
-        list.appendChild(spanWithValue);
-        list.appendChild(spanWithBtn);
-        todoList.appendChild(list);
-    });
-    #noOfTodos.innerText = todosArr.length;
-};
-cosnt;
-deleteTodo = function (index) {
-    todosArr.splice(index, 1);
-    createTodo(todosArr);
-};
-window.addEventListener("load", function () {
-    createTodo(todosArr);
-});
+// // selectors
+// const root = document.querySelector('.root');
+// // const note = document.querySelector('.note')
+// // Functions
+// function handleNote(ev: any) {
+//   ev.preventDefault();
+//   const elements = ev.target.elements;
+//   const result = {};
+//   for (let i = 0; i < elements.length; i++) {
+//     if (elements[i].type === "text") {
+//       result[elements[i].name] = elements[i].value;
+//       }   else if (elements[i].name && elements[i].value) {
+//       result[elements[i].name] = elements[i].value;
+//     } 
+//   }
+//   console.log(result);
+//   root.innerHTML += `<div>${result.note}</div>`
+// }  
