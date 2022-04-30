9c327e210195649532c74e71ccf5d3382fa60a74
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-04-30 23:58:54 +0300

    SDF

--- Javascript/14-FinalProject/Gal - Noy - Lital New/client.ts
+++ Javascript/14-FinalProject/Gal - Noy - Lital New/client.ts
@@ -20,14 +20,14 @@ interface cartProduct {
 let milk: Product = {
   name: "milk",
   price: 6,
-  description: "3% milk",
+  description: "3% Milk",
   image: "https://www.tnuva.co.il/uploads/f_606ee43fa87cf_1617880127.jpg",
   id: "milk",
 };
 let bread: Product = {
   name: "bread",
   price: 9,
-  description: "white bread",
+  description: "White Bread",
   image:
     "https://www.einbar.co.il/wp-content/uploads/2020/08/%D7%9C%D7%97%D7%9D-%D7%99%D7%9C%D7%93%D7%99%D7%9D-%D7%9C%D7%90%D7%AA%D7%A8.jpg",
 
@@ -36,7 +36,7 @@ let bread: Product = {
 let butter: Product = {
   name: "butter",
   price: 9,
-  description: "the butter",
+  description: "Butter",
   image:
     "https://ynet-images1.yit.co.il/picserver5/crop_images/2020/10/04/BJF90wPID/BJF90wPID_0_530_2560_1441_0_x-large.jpg",
   id: "butter",
@@ -44,7 +44,7 @@ let butter: Product = {
 let eggs: Product = {
   name: "eggs",
   price: 21,
-  description: "the eggs",
+  description: "Eggs Pacage",
   image:
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6bII5tIeCOVPE92gwVSLi1v_-WieT9qhIsg&usqp=CAU",
   id: "eggs",
@@ -52,7 +52,7 @@ let eggs: Product = {
 let tomato: Product = {
   name: "tomato",
   price: 3,
-  description: "1kg tomato",
+  description: "1kg Tomato",
   image: "https://s3-us-west-2.amazonaws.com/melingoimages/Images/98055.jpg",
   id: "tomato",
 };
@@ -60,7 +60,7 @@ let tomato: Product = {
 let cabbage: Product = {
   name: "cabbage",
   price: 5,
-  description: "1kg cabbage",
+  description: "1kg Cabbage",
   image:
     "https://media.istockphoto.com/photos/green-cabbage-isolated-on-white-picture-id673162168?k=20&m=673162168&s=612x612&w=0&h=3QKF6zzzCAUL3pKxW6kVbZ7lUt1JUY_SchOUMyOHwhs=",
   id: "cabbage",
@@ -68,7 +68,7 @@ let cabbage: Product = {
 let tea: Product = {
   name: "tea",
   price: 12,
-  description: "tea",
+  description: "Tea",
   image:
     "https://www.wallashops.co.il/on/demandware.static/-/Sites-wallashops-m-catalog/default/dwd19f9a45/productImages/images_from_feed/hury/9KA989DE9D/openfile_aspx_id_148684&type_png.jpg",
   id: "tea",
@@ -76,7 +76,7 @@ let tea: Product = {
 let coffee: Product = {
   name: "coffee",
   price: 15,
-  description: "coffee",
+  description: "Coffee",
   image:
     "https://superpharmstorage.blob.core.windows.net/hybris/products/mobile/medium/7290000176062.jpg",
   id: "coffee",
@@ -84,7 +84,7 @@ let coffee: Product = {
 let toiletPaper: Product = {
   name: "toilet paper",
   price: 35,
-  description: "toilet paper",
+  description: "Toilet Paper",
   image:
     "https://h7z8m6j8.stackpathcdn.com/wp-content/uploads/2019/04/90-%D7%9E%D7%9E%D7%97%D7%98%D7%95%D7%AA-%D7%A4%D7%A0%D7%99%D7%9D-%D7%A7%D7%9C%D7%99%D7%A0%D7%A7%D7%A1-%D7%91%D7%95%D7%98%D7%99%D7%A7-%D7%91%D7%A7%D7%95%D7%A4%D7%A1%D7%90.jpg",
   id: "toilet-paper",
