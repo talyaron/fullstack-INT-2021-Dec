7bc8a21d7784b1b490a1263d9f76962bf5cc38e5
Shira1401 <shiraamar44@gmail.com>
2022-05-08 22:21:11 +0300

    task

---
+++ Node/02-routes-async/Shira_h.w/dist/server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+app.use(express.static('public'));
+app.listen(port, function () {
+    console.log("Server listening on port " + port);
+});
