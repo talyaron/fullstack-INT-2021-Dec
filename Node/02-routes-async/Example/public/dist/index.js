63e0c32f6807b9e37c8d000b9ab2d37cf8e57909
Tal Yaron <tal.yaron@gmail.com>
2022-05-09 08:02:33 +0300

    cont

--- Node/02-routes-async/Example/public/dist/index.js
+++ Node/02-routes-async/Example/public/dist/index.js
@@ -37,6 +37,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 function handleGetUser1() {
     try {
         console.log("get user 12");
+        renderLoader();
         axios
             .get("/api/user1")
             .then(function (_a) {
@@ -46,6 +47,7 @@ function handleGetUser1() {
             if (error)
                 throw new Error(error);
             console.log(user);
+            renderLoader();
             renderUser(user);
         })["catch"](function (err) { return console.error(err); });
     }
@@ -62,9 +64,11 @@ function handleGetUser2() {
                     _a.trys.push([0, 2, , 3]);
                     console.log("get user (1)");
                     console.log("get user After fetch (2)");
+                    renderLoader();
                     return [4 /*yield*/, axios.get("/api/user2")];
                 case 1:
                     data = (_a.sent()).data;
+                    renderLoader();
                     console.log(data);
                     console.log("get user After fetch (2.5)");
                     user = data.user, error = data.error;
@@ -84,6 +88,7 @@ function handleGetUser2() {
 }
 function handleGetUser3() {
     try {
+        renderLoader();
         axios.get("/api/user3").then(function (_a) {
             var data = _a.data;
             console.log(data);
@@ -91,6 +96,7 @@ function handleGetUser3() {
             if (error)
                 throw new Error(error);
             renderUser(user);
+            renderLoader();
         });
     }
     catch (error) {
@@ -100,4 +106,15 @@ function handleGetUser3() {
 function renderUser(user) {
     var root = document.querySelector("#root");
     root.innerText = "user " + user.name + " is " + user.age + " years old";
+}
+function renderLoader() {
+    var loader = document.querySelector('#loader');
+    if (!loader.classList.contains('lds-dual-ring')) {
+        loader.classList.add('lds-dual-ring');
+        console.log('add');
+    }
+    else {
+        loader.classList.remove('lds-dual-ring');
+        console.log('remove');
+    }
 }
--- Node/02-routes-async/Example/public/index.ts
+++ Node/02-routes-async/Example/public/index.ts
@@ -1,6 +1,7 @@
 function handleGetUser1() {
   try {
     console.log("get user 12");
+    renderLoader()
     axios
       .get("/api/user1")
       .then(({ data }) => {
@@ -8,6 +9,7 @@ function handleGetUser1() {
         const { user, error } = data;
         if (error) throw new Error(error);
         console.log(user);
+        renderLoader()
         renderUser(user);
       })
       .catch((err) => console.error(err));
@@ -20,8 +22,9 @@ async function handleGetUser2() {
   try {
     console.log("get user (1)");
     console.log("get user After fetch (2)");
-    
+    renderLoader()
     const { data } = await axios.get("/api/user2");
+    renderLoader()
     console.log(data)
     console.log("get user After fetch (2.5)");
     const { user, error } = data;
@@ -37,11 +40,13 @@ async function handleGetUser2() {
 
 function handleGetUser3() {
   try {
+    renderLoader()
     axios.get("/api/user3").then(({ data }) => {
       console.log(data);
       const { user, error } = data;
       if (error) throw new Error(error);
       renderUser(user);
+      renderLoader()
     });
   } catch (error) {
     console.error(error);
@@ -52,4 +57,15 @@ function renderUser(user: User) {
   const root: HTMLElement = document.querySelector("#root");
 
   root.innerText = `user ${user.name} is ${user.age} years old`;
+}
+
+function renderLoader(){
+    const loader: HTMLElement = document.querySelector('#loader')
+    if(!loader.classList.contains('lds-dual-ring')){
+        loader.classList.add('lds-dual-ring');
+        console.log('add')
+    } else {
+        loader.classList.remove('lds-dual-ring');
+        console.log('remove')
+    }
 }
