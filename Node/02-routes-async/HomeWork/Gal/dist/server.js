1f34783a30c6ca3149c3702c6ddf400babb2faba
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-05-10 20:05:15 +0300

    fcdgh

--- Node/02-routes-async/HomeWork/Gal/dist/server.js
+++ Node/02-routes-async/HomeWork/Gal/dist/server.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var Interface = require('readline').Interface;
 var app = express();
 var port = process.env.PORT || 4000;
 app.use(express.static('public'));
@@ -11,19 +10,23 @@ var imageArray = [
     {
         name: 'woman',
         src: 'https://img.freepik.com/free-photo/indoor-shot-beautiful-happy-african-american-woman-smiling-cheerfully-keeping-her-arms-folded-relaxing-indoors-after-morning-lectures-university_273609-1270.jpg?size=626&ext=jpg&ga=GA1.1.2011762371.1644019200'
+    },
+    {
+        name: 'kid',
+        src: 'https://nationaltoday.com/wp-content/uploads/2021/04/Every-Kid-Healthy.jpg'
     }
 ];
-app.listen(port, function () {
-    console.log("server is listening on port " + port);
-});
-function getRandomArbitrary() {
+function getRandom() {
     return Math.round(Math.random());
 }
 app.get('/api/randomImage', function (req, res) {
     try {
-        res.send({ picture: imageArray[getRandomArbitrary()] });
+        res.send({ picture: imageArray[getRandom()] });
     }
     catch (error) {
         res.send({ error: error.message });
     }
+});
+app.listen(port, function () {
+    console.log("server is listening on port " + port);
 });
