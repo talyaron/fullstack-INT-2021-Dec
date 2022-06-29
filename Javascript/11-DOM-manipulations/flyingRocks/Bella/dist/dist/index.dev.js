165b85f0b8c9c3a5c0290fa9cf2037c8c5ed6b1e
Bella Brail <bellabrailovsky@gmail.com>
2022-06-29 14:06:38 +0300

    .

---
+++ Javascript/11-DOM-manipulations/flyingRocks/Bella/dist/dist/index.dev.js
@@ -0,0 +1,67 @@
+"use strict";
+
+var wrapper1 = document.querySelectorAll('.wrapper1');
+console.dir(wrapper1);
+
+function meteor1() {
+  wrapper1 = document.querySelectorAll('.wrapper1');
+  setInterval(function () {
+    wrapper1.forEach(function (element) {
+      //console.log(listRandomNumber())
+      element.style.top = listRandomNumber();
+      element.style.left = listRandomNumber();
+      element.style.transform = "translate(" + listRandomNumber() + ", " + listRandomNumber() + ")"; //isOverlap()
+    });
+    wrapper1.forEach(function (elem) {
+      elem.onmouseover = mouseInv;
+      window.onclick = mouseHop; // elem.innerHTML += `<div class="wrapper1"> <img src="png_meteor_6537 (1).png" alt=""> </div>`;
+    });
+    isOverlap();
+  }, 2000); //setTimeout (meteor1,5000)
+}
+
+var x1 = 0;
+var y1 = 0;
+var right1 = 0;
+var bottom1 = 0;
+var x2 = 0;
+var y2 = 0;
+var right2 = 0;
+var bottom2 = 0;
+
+function isOverlap() {
+  for (var i = 0; i < wrapper1.length; i++) {
+    x1 = wrapper1[i].getBoundingClientRect().x;
+    right1 = wrapper1[i].getBoundingClientRect().right;
+    bottom1 = wrapper1[i].getBoundingClientRect().bottom;
+    y1 = wrapper1[i].getBoundingClientRect().y;
+
+    for (var j = i + 1; j < wrapper1.length; j++) {
+      x2 = wrapper1[j].getBoundingClientRect().x;
+      right2 = wrapper1[j].getBoundingClientRect().right;
+      bottom2 = wrapper1[j].getBoundingClientRect().bottom;
+      y2 = wrapper1[j].getBoundingClientRect().y;
+
+      if (x1 > x2 && x1 < right2 && y1 > y2 && y1 < bottom2) {
+        console.log("overlap");
+      }
+    }
+  }
+}
+
+function listRandomNumber() {
+  var randomPX = Math.floor(Math.random() * 250);
+  return randomPX + "px";
+}
+
+meteor1();
+
+function mouseInv(ev) {
+  ev.target.style.opacity = 0;
+  meteor1();
+}
+
+function mouseHop(ev) {
+  document.body.innerHTML += "<div class=\"wrapper1\"><img src=\"png_meteor_6537 (1).png\" alt=\"\"></div>";
+  meteor1();
+}
--- Javascript/11-DOM-manipulations/flyingRocks/Bella/dist/index.js
+++ Javascript/11-DOM-manipulations/flyingRocks/Bella/dist/index.js
@@ -1,33 +1,59 @@
-// function moveObj() {
-//     const $span = $(".spaceRock");
-//     $span.animate([
-//         // keyframes
-//         { transform: 'translateY(100vh)' },
-//         { transform: 'translateX(100vw)' }
-//       ], function() {
-//         let maxLeft = $(window).width() - $span.width();
-//         let maxTop = $(window).height() - $span.height();
-//         let leftPos = Math.floor(Math.random() * (maxLeft + 10))
-//         let topPos = Math.floor(Math.random() * (maxTop + 10))
-//         $span.css({ left: leftPos, top: topPos });
-//     });
-// };
-// moveObj();
-// setInterval(moveObj,100);
-var spaceRocks = document.querySelectorAll('.spaceRock');
-spaceRocks.forEach(function (element) {
-    element.style.left = getRandomPosition();
-    element.style.top = getRandomPosition();
-});
-function getRandomPosition() {
-    var element = (((Math.random() * 800) + 60).toFixed()) + 'px';
-    console.dir(element);
-    return element;
+var wrapper1 = document.querySelectorAll('.wrapper1');
+console.dir(wrapper1);
+function meteor1() {
+    wrapper1 = document.querySelectorAll('.wrapper1');
+    setInterval(function () {
+        wrapper1.forEach(function (element) {
+            //console.log(listRandomNumber())
+            element.style.top = listRandomNumber();
+            element.style.left = listRandomNumber();
+            element.style.transform = "translate(" + listRandomNumber() + ", " + listRandomNumber() + ")";
+            //isOverlap()
+        });
+        wrapper1.forEach(function (elem) {
+            elem.onmouseover = mouseInv;
+            window.onclick = mouseHop;
+            // elem.innerHTML += `<div class="wrapper1"> <img src="png_meteor_6537 (1).png" alt=""> </div>`;
+        });
+        isOverlap();
+    }, 2000);
+    //setTimeout (meteor1,5000)
 }
-getRandomPosition();
-setInterval(function () {
-    for (var i = 0; i < spaceRocks.length; i++) {
-        spaceRocks.style.top = getRandomPosition() + "px";
-        spaceRocks.style.left = getRandomPosition() + "px";
+var x1 = 0;
+var y1 = 0;
+var right1 = 0;
+var bottom1 = 0;
+var x2 = 0;
+var y2 = 0;
+var right2 = 0;
+var bottom2 = 0;
+function isOverlap() {
+    for (var i = 0; i < wrapper1.length; i++) {
+        x1 = wrapper1[i].getBoundingClientRect().x;
+        right1 = wrapper1[i].getBoundingClientRect().right;
+        bottom1 = wrapper1[i].getBoundingClientRect().bottom;
+        y1 = wrapper1[i].getBoundingClientRect().y;
+        for (var j = i + 1; j < wrapper1.length; j++) {
+            x2 = wrapper1[j].getBoundingClientRect().x;
+            right2 = wrapper1[j].getBoundingClientRect().right;
+            bottom2 = wrapper1[j].getBoundingClientRect().bottom;
+            y2 = wrapper1[j].getBoundingClientRect().y;
+            if (x1 > x2 && x1 < right2 && y1 > y2 && y1 < bottom2) {
+                console.log("overlap");
+            }
+        }
     }
-}, 1000);
+}
+function listRandomNumber() {
+    var randomPX = Math.floor(Math.random() * 250);
+    return randomPX + "px";
+}
+meteor1();
+function mouseInv(ev) {
+    ev.target.style.opacity = 0;
+    meteor1();
+}
+function mouseHop(ev) {
+    document.body.innerHTML += "<div class=\"wrapper1\"><img src=\"png_meteor_6537 (1).png\" alt=\"\"></div>";
+    meteor1();
+}
